ba9420d324f2804c68b30036938409dd1215f9a0
Chris Laurel <claurel@gmail.com>
2001-02-26 17:05:01 +0000

    Added misc. txt files and resources.

---
+++ res/resource.h
@@ -0,0 +1,56 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by celestia.rc
+//
+#define IDR_MENU1                       101
+#define IDD_ABOUT                       102
+#define IDI_CELESTIA_ICON               103
+#define IDD_FINDOBJECT                  104
+#define IDR_MAIN_MENU                   105
+#define IDR_ACCELERATORS                106
+#define IDD_LICENSE                     108
+#define IDD_SETTIME                     109
+#define IDC_FINDOBJECT_EDIT             1001
+#define IDC_LICENSE_TEXT                1004
+#define IDC_CALENDAR                    1006
+#define IDC_TIMEPICKER                  1007
+#define IDC_DATEPICKER                  1008
+#define ID_SELECTSOL                    40001
+#define ID_GOTOSELECTION                40002
+#define ID_FOLLOWSELECTION              40003
+#define ID_SPEEDTIME                    40005
+#define ID_SLOWTIME                     40006
+#define ID_STOPTIME                     40007
+#define ID_QUIT                         40008
+#define ID_FILE_EXIT                    40009
+#define ID_HELP_ABOUT                   40010
+#define ID_RENDER_SHOWORBITS            40011
+#define ID_RENDER_SHOWPLANETLABELS      40012
+#define ID_RENDER_SHOWHUDTEXT           40013
+#define ID_RENDER_AMBIENTLIGHT_NONE     40014
+#define ID_RENDER_AMBIENTLIGHT_LOW      40015
+#define ID_RENDER_AMBIENTLIGHT_MEDIUM   40016
+#define ID_NAVIGATION_HOME              40017
+#define ID_NAVIGATION_SELECT            40018
+#define ID_NAVIGATION_GOTO              40019
+#define ID_NAVIGATION_FOLLOW            40020
+#define ID_NAVIGATION_BLAH              40021
+#define ID_TIME_FASTER                  40023
+#define ID_TIME_SLOWER                  40024
+#define ID_TIME_FREEZE                  40025
+#define ID_HELP_LICENSE                 40026
+#define ID_NAVIGATION_CENTER            40027
+#define ID_TIME_REALTIME                40028
+#define ID_RENDER_SHOWSTARLABELS        40029
+#define ID_TIME_SETTIME                 40030
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40031
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
