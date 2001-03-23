74b3a714cc0c4bab2183c653a1c0f9b5d03c4a93
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2001-03-23 02:29:32 +0000

    Core of Celestia's limited scripting system.

---
+++ src/execution.cpp
@@ -0,0 +1,49 @@
+// execution.cpp
+//
+// Copyright (C) 2001 Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#include "execution.h"
+
+using namespace std;
+
+
+Execution::Execution(CommandSequence& cmd, Simulation* s, Renderer* r) :
+    currentCommand(cmd.begin()),
+    finalCommand(cmd.end()),
+    sim(s),
+    renderer(r),
+    commandTime(0.0)
+{
+}
+
+
+bool Execution::tick(double dt)
+{
+    while (dt > 0.0 && currentCommand != finalCommand)
+    {
+        Command* cmd = *currentCommand;
+
+        if (dt >= cmd->getDuration() - commandTime)
+        {
+            cmd->process(sim, renderer, cmd->getDuration());
+            dt -= cmd->getDuration() - commandTime;
+            commandTime = 0.0;
+            currentCommand++;
+        }
+        else
+        {
+            commandTime += dt;
+            cmd->process(sim, renderer, commandTime);
+            dt = 0.0;
+        }
+    }
+
+    return currentCommand == finalCommand;
+}
+
+
