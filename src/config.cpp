42b00da6104d5f6a6a0b83cdbcd0844f3d3597b4
Chris Laurel <claurel@gmail.com>
2001-04-09 22:29:18 +0000

    Added AsterismsFile field

--- src/config.cpp
+++ src/config.cpp
@@ -56,6 +56,7 @@ CelestiaConfig* ReadCelestiaConfig(string filename)
     config->faintestVisible = 6.0f;
     configParams->getNumber("FaintestVisibleMagnitude", config->faintestVisible);
     configParams->getString("FavoritesFile", config->favoritesFile);
+    configParams->getString("AsterismsFile", config->asterismsFile);
     configParams->getString("StarDatabase", config->starDatabaseFile);
     configParams->getString("StarNameDatabase", config->starNamesFile);
 
