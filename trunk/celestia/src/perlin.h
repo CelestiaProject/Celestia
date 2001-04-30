6ebc7c98ce056f8825722dfa1095296245a4c367
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2001-04-30 03:18:57 +0000

    Const-ified reference parameters for fractalsum and turbulence.

--- trunk/celestia/src/perlin.h
+++ trunk/celestia/src/perlin.h
@@ -10,10 +10,10 @@ extern float noise2(float vec[]);
 extern float noise3(float vec[]);
 
 extern float turbulence(float v[], float freq);
-extern float turbulence(Point2f& p, float freq);
-extern float turbulence(Point3f& p, float freq);
+extern float turbulence(const Point2f& p, float freq);
+extern float turbulence(const Point3f& p, float freq);
 extern float fractalsum(float v[], float freq);
-extern float fractalsum(Point2f& p, float freq);
-extern float fractalsum(Point3f& p, float freq);
+extern float fractalsum(const Point2f& p, float freq);
+extern float fractalsum(const Point3f& p, float freq);
 
 #endif // _PERLIN_H_
