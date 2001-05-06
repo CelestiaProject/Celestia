faaaef0636a73e9c16c255c7895d571d5392966c
Chris Laurel <claurel@gmail.com>
2001-05-06 21:54:56 +0000

    Octree-based visibility culling for stars.

---
+++ src/octree.h
@@ -0,0 +1,71 @@
+// octree.h
+//
+// Copyright (C) 2001, Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _OCTREE_H_
+#define _OCTREE_H_
+
+#include <vector>
+#include "star.h"
+
+
+class StarHandler
+{
+ public:
+    StarHandler() {};
+    virtual ~StarHandler() {};
+    
+    virtual void process(const Star&) = 0;
+};
+
+
+class StarOctreeNode
+{
+ public:
+    StarOctreeNode(const Point3f& _center, float _luminosity);
+    ~StarOctreeNode();
+
+    void insertStar(const Star&, float);
+    void processVisibleStars(StarHandler& starHandler,
+                             const Point3f& position,
+                             float limitingMag,
+                             float scale) const;
+    int countChildren() const;
+
+ private:
+    void addStar(const Star&);
+    void split(float);
+    void sortStarsIntoChildNodes();
+
+    int nFaintStars;
+    Point3f center;
+    float luminosity;
+    std::vector<const Star*>* stars;
+    StarOctreeNode** children;
+};
+
+
+class StarOctree
+{
+ public:
+    StarOctree(const Point3f& center, float _scale, float limitingMag);
+    ~StarOctree();
+
+    void insertStar(const Star&);
+    void processVisibleStars(StarHandler& starHandler,
+                             const Point3f& position,
+                             float limitingMag) const;
+    int countNodes() const;
+
+ private:
+    StarOctreeNode* root;
+    float scale;
+    float luminosity;
+};
+
+#endif // _OCTREE_H_
