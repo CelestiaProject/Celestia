c34c013f306f33a6f796f0eee1f9a09bc11d92c8
Chris Laurel <claurel@gmail.com>
2001-05-09 02:40:48 +0000

    Split StarOctree into StarOctree and DynamicStarOctree.  Moved star visibility routines into StarDatabase.

--- src/octree.cpp
+++ src/octree.cpp
@@ -32,70 +32,12 @@ static const int splitThreshold = 100;
 static const float sqrt3 = 1.732050808f;
 
 
-StarOctree::StarOctree(const Point3f& center, float _scale, float limitingMag) :
-    root(NULL), scale(_scale)
-{
-    absMag = astro::appToAbsMag(limitingMag, scale * sqrt3);
-    root = new StarOctreeNode(center, absMag);
-}
-
-StarOctree::~StarOctree()
-{
-    if (root != NULL)
-        delete root;
-}
-
-void StarOctree::insertStar(const Star& star)
-{
-    root->insertStar(star, scale);
-}
-
-void StarOctree::processVisibleStars(StarHandler& starHandler,
-                                     const Point3f& position,
-                                     const Quatf& orientation,
-                                     float fovY,
-                                     float aspectRatio,
-                                     float limitingMag) const
-{
-    // Compute the bounding planes of an infinite view frustum
-    Planef frustumPlanes[5];
-    Vec3f planeNormals[5];
-    Mat3f rot = orientation.toMatrix3();
-    float h = (float) tan(fovY / 2);
-    float w = h * aspectRatio;
-    planeNormals[0] = Vec3f(0, 1, -h);
-    planeNormals[1] = Vec3f(0, -1, -h);
-    planeNormals[2] = Vec3f(1, 0, -w);
-    planeNormals[3] = Vec3f(-1, 0, -w);
-    planeNormals[4] = Vec3f(0, 0, -1);
-    for (int i = 0; i < 5; i++)
-    {
-        planeNormals[i].normalize();
-        planeNormals[i] = planeNormals[i] * rot;
-        frustumPlanes[i] = Planef(planeNormals[i], position);
-    }
-    
-    root->processVisibleStars(starHandler, position, frustumPlanes,
-                              limitingMag, scale);
-}
-
-int StarOctree::countNodes() const
-{
-    return 1 + root->countChildren();
-}
-
-int StarOctree::countStars() const
-{
-    return root->countStars();
-}
-
-
-StarOctreeNode::StarOctreeNode(const Point3f& _center, float _absMag) :
+DynamicStarOctree::DynamicStarOctree(const Point3f& _center, float _absMag) :
     stars(NULL), children(NULL), center(_center), absMag(_absMag)
 {
 }
 
-StarOctreeNode::~StarOctreeNode()
+DynamicStarOctree::~DynamicStarOctree()
 {
     if (children != NULL)
     {
@@ -103,6 +45,7 @@ StarOctreeNode::~StarOctreeNode()
         {
             delete children[i];
         }
+        delete [] children;
     }
 }
 
@@ -120,7 +63,7 @@ static int childIndex(const Star& star, const Point3f& center)
 }
 
 
-void StarOctreeNode::insertStar(const Star& star, float scale)
+void DynamicStarOctree::insertStar(const Star& star, float scale)
 {
     // If the star is brighter than the node's magnitude, insert
     // it here now.
@@ -159,65 +102,7 @@ void StarOctreeNode::insertStar(const Star& star, float scale)
 }
 
 
-void StarOctreeNode::processVisibleStars(StarHandler& starHandler,
-                                         const Point3f& position,
-                                         const Planef* frustumPlanes,
-                                         float limitingMag,
-                                         float scale) const
-{
-    // See if this node lies within the view frustum
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            const Planef* plane = frustumPlanes + i;
-            float r = scale * (abs(plane->normal.x) +
-                               abs(plane->normal.y) +
-                               abs(plane->normal.z));
-            if (plane->normal * Vec3f(center.x, center.y, center.z) - plane->d < -r)
-                return;
-        }
-    }
-
-    // Compute the distance to node; this is equal to the distance to
-    // the center of the node minus the radius of the node, scale * sqrt3.
-    float minDistance = (position - center).length() - scale * sqrt3;
-
-    // Process the stars in this node
-    if (stars != NULL)
-    {
-        float dimmest = minDistance > 0 ? astro::appToAbsMag(limitingMag, minDistance) : 100;
-        for (vector<const Star*>::const_iterator iter = stars->begin();
-             iter != stars->end(); iter++)
-        {
-            if ((*iter)->getAbsoluteMagnitude() < dimmest)
-            {
-                float distance = (position - (*iter)->getPosition()).length();
-                float appMag = astro::absToAppMag((*iter)->getAbsoluteMagnitude(), distance);
-                if (appMag < limitingMag)
-                    starHandler.process(**iter, distance, appMag);
-            }
-        }
-    }
-
-    if (minDistance <= 0 || astro::absToAppMag(absMag, minDistance) <= limitingMag)
-    {
-        // Recurse into the child nodes
-        if (children != NULL)
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                children[i]->processVisibleStars(starHandler,
-                                                 position,
-                                                 frustumPlanes,
-                                                 limitingMag,
-                                                 scale * 0.5f);
-            }
-        }
-    }
-}
-
-
-void StarOctreeNode::addStar(const Star& star)
+void DynamicStarOctree::addStar(const Star& star)
 {
     if (stars == NULL)
         stars = new vector<const Star*>();
@@ -225,16 +110,16 @@ void StarOctreeNode::addStar(const Star& star)
 }
 
 
-void StarOctreeNode::split(float scale)
+void DynamicStarOctree::split(float scale)
 {
-    children = new StarOctreeNode*[8];
+    children = new DynamicStarOctree*[8];
     for (int i = 0; i < 8; i++)
     {
         Point3f p(center);
         p.x += ((i & XPos) != 0) ? scale : -scale;
         p.y += ((i & YPos) != 0) ? scale : -scale;
         p.z += ((i & ZPos) != 0) ? scale : -scale;
-        children[i] = new StarOctreeNode(p, astro::lumToAbsMag(astro::absMagToLum(absMag) / 4.0f));
+        children[i] = new DynamicStarOctree(p, astro::lumToAbsMag(astro::absMagToLum(absMag) / 4.0f));
     }
     sortStarsIntoChildNodes();
 }
@@ -243,7 +128,7 @@ void StarOctreeNode::split(float scale)
 // Sort this node's stars into stars that are bright enough to remain
 // in the node, and stars that should be placed into one of the eight
 // child nodes.
-void StarOctreeNode::sortStarsIntoChildNodes()
+void DynamicStarOctree::sortStarsIntoChildNodes()
 {
     int nBrightStars = 0;
 
@@ -265,7 +150,111 @@ void StarOctreeNode::sortStarsIntoChildNodes()
 }
 
 
-int StarOctreeNode::countChildren() const
+void DynamicStarOctree::rebuildAndSort(StarOctree*& node, Star*& sortedStars)
+{
+    Star* firstStar = sortedStars;
+
+    if (stars != NULL)
+    {
+        for (vector<const Star*>::const_iterator iter = stars->begin();
+             iter != stars->end(); iter++)
+        {
+            *sortedStars++ = **iter;
+        }
+    }
+
+    uint32 nStars = (uint32) (sortedStars - firstStar);
+    node = new StarOctree(center, absMag, firstStar, nStars);
+
+    if (children != NULL)
+    {
+        node->children = new StarOctree*[8];
+        for (int i = 0; i < 8; i++)
+            children[i]->rebuildAndSort(node->children[i], sortedStars);
+    }
+}
+
+
+
+StarOctree::StarOctree(const Point3f& _center,
+                       float _absMag,
+                       Star* _firstStar,
+                       uint32 _nStars) :
+    center(_center),
+    absMag(_absMag),
+    firstStar(_firstStar),
+    nStars(_nStars),
+    children(NULL)
+{
+}
+
+StarOctree::~StarOctree()
+{
+    if (children != NULL)
+    {
+        for (int i = 0; i < 8; i++)
+            delete children[i];
+        delete [] children;
+    }
+}
+
+
+void StarOctree::processVisibleStars(StarHandler& starHandler,
+                                     const Point3f& position,
+                                     const Planef* frustumPlanes,
+                                     float limitingMag,
+                                     float scale) const
+{
+    // See if this node lies within the view frustum
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            const Planef* plane = frustumPlanes + i;
+            float r = scale * (abs(plane->normal.x) +
+                               abs(plane->normal.y) +
+                               abs(plane->normal.z));
+            if (plane->normal * Vec3f(center.x, center.y, center.z) - plane->d < -r)
+                return;
+        }
+    }
+
+    // Compute the distance to node; this is equal to the distance to
+    // the center of the node minus the radius of the node, scale * sqrt3.
+    float minDistance = (position - center).length() - scale * sqrt3;
+
+    // Process the stars in this node
+    float dimmest = minDistance > 0 ? astro::appToAbsMag(limitingMag, minDistance) : 100;
+    for (int i = 0; i < nStars; i++)
+    {
+        Star& star = firstStar[i];
+        if (star.getAbsoluteMagnitude() < dimmest)
+        {
+            float distance = (position - star.getPosition()).length();
+            float appMag = astro::absToAppMag(star.getAbsoluteMagnitude(), distance);
+            if (appMag < limitingMag)
+                starHandler.process(star, distance, appMag);
+        }
+    }
+
+    if (minDistance <= 0 || astro::absToAppMag(absMag, minDistance) <= limitingMag)
+    {
+        // Recurse into the child nodes
+        if (children != NULL)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                children[i]->processVisibleStars(starHandler,
+                                                 position,
+                                                 frustumPlanes,
+                                                 limitingMag,
+                                                 scale * 0.5f);
+            }
+        }
+    }
+}
+
+
+int StarOctree::countChildren() const
 {
     if (children == NULL)
     {
@@ -281,18 +270,15 @@ int StarOctreeNode::countChildren() const
     }
 }
 
-int StarOctreeNode::countStars() const
+int StarOctree::countStars() const
 {
-    int nStars = stars == NULL ? 0 : stars->size();
+    int count = nStars;
 
     if (children != NULL)
     {
         for (int i = 0; i < 8; i++)
-        {
-            nStars += children[i]->countStars();
-        }
-
+            count += children[i]->countStars();
     }
 
-    return nStars;
+    return count;
 }
