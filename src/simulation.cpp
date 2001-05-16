00d01fe2e6ac7f49d5d7d9420261e0e9ec34ca94
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2001-05-16 21:57:11 +0000

    Got rid of bigfix bug related debugging output.

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -357,14 +357,13 @@ void Simulation::update(double dt)
     else if (observerMode == Following)
     {
         Point3d posRelToSun = followInfo.body->getHeliocentricPosition(simTime) + followInfo.offset;
-        cout << "Follow: ";
         double x1 = (double) observer.getPosition().x;
         observer.setPosition(astro::universalPosition(posRelToSun,
                                                       followInfo.sun->getPosition()));
         Point3d blah = astro::heliocentricPosition(observer.getPosition(),
                                                followInfo.sun->getPosition());
         double x2 = (double) observer.getPosition().x;
-        cout << (blah.x - posRelToSun.x) << '\n';
+        cout << "Follow: " << (blah.x - posRelToSun.x) << '\n';
     }
     else
     {
