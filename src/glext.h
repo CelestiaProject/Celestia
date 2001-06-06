ba15b3fd0e79a7d8fda5d8e18b3f5c051b587f95
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2001-06-06 01:23:55 +0000

    Added GLenum casts to make it possible to compile with broken Mesa 3.1 headers.

--- src/glext.h
+++ src/glext.h
@@ -14,7 +14,7 @@
 
 /* EXT_texture_edge_clamp defines from <GL/gl.h> */
 #ifndef GL_EXT_texture_edge_clamp
-#define GL_CLAMP_TO_EDGE_EXT                0x812F
+#define GL_CLAMP_TO_EDGE_EXT                ((GLenum) 0x812F)
 #endif
 
 /* EXT_bgra defines from <GL/gl.h> */
@@ -124,18 +124,18 @@ extern PFNGLCLIENTACTIVETEXTUREARBPROC glClientActiveTextureARB;
 
 /* EXT_texture_cube_map defines from <GL/gl.h> */
 #ifndef GL_EXT_texture_cube_map
-#define GL_NORMAL_MAP_EXT                   0x8511
-#define GL_REFLECTION_MAP_EXT               0x8512
-#define GL_TEXTURE_CUBE_MAP_EXT             0x8513
-#define GL_TEXTURE_BINDING_CUBE_MAP_EXT     0x8514
-#define GL_TEXTURE_CUBE_MAP_POSITIVE_X_EXT  0x8515
-#define GL_TEXTURE_CUBE_MAP_NEGATIVE_X_EXT  0x8516
-#define GL_TEXTURE_CUBE_MAP_POSITIVE_Y_EXT  0x8517
-#define GL_TEXTURE_CUBE_MAP_NEGATIVE_Y_EXT  0x8518
-#define GL_TEXTURE_CUBE_MAP_POSITIVE_Z_EXT  0x8519
-#define GL_TEXTURE_CUBE_MAP_NEGATIVE_Z_EXT  0x851A
-#define GL_PROXY_TEXTURE_CUBE_MAP_EXT       0x851B
-#define GL_MAX_CUBE_MAP_TEXTURE_SIZE_EXT    0x851C
+#define GL_NORMAL_MAP_EXT                   ((GLenum) 0x8511)
+#define GL_REFLECTION_MAP_EXT               ((GLenum) 0x8512)
+#define GL_TEXTURE_CUBE_MAP_EXT             ((GLenum) 0x8513)
+#define GL_TEXTURE_BINDING_CUBE_MAP_EXT     ((GLenum) 0x8514)
+#define GL_TEXTURE_CUBE_MAP_POSITIVE_X_EXT  ((GLenum) 0x8515)
+#define GL_TEXTURE_CUBE_MAP_NEGATIVE_X_EXT  ((GLenum) 0x8516)
+#define GL_TEXTURE_CUBE_MAP_POSITIVE_Y_EXT  ((GLenum) 0x8517)
+#define GL_TEXTURE_CUBE_MAP_NEGATIVE_Y_EXT  ((GLenum) 0x8518)
+#define GL_TEXTURE_CUBE_MAP_POSITIVE_Z_EXT  ((GLenum) 0x8519)
+#define GL_TEXTURE_CUBE_MAP_NEGATIVE_Z_EXT  ((GLenum) 0x851A)
+#define GL_PROXY_TEXTURE_CUBE_MAP_EXT       ((GLenum) 0x851B)
+#define GL_MAX_CUBE_MAP_TEXTURE_SIZE_EXT    ((GLenum) 0x851C)
 #endif
 
 /* EXT_separate_specular_color defines from <GL/gl.h> */
@@ -189,62 +189,62 @@ extern PFNGLCLIENTACTIVETEXTUREARBPROC glClientActiveTextureARB;
 
 /* NV_register_combiners defines and prototypes from <GL/gl.h> */
 #ifndef GL_NV_register_combiners
-#define GL_REGISTER_COMBINERS_NV            0x8522
-#define GL_COMBINER0_NV                     0x8550
-#define GL_COMBINER1_NV                     0x8551
-#define GL_COMBINER2_NV                     0x8552
-#define GL_COMBINER3_NV                     0x8553
-#define GL_COMBINER4_NV                     0x8554
-#define GL_COMBINER5_NV                     0x8555
-#define GL_COMBINER6_NV                     0x8556
-#define GL_COMBINER7_NV                     0x8557
-#define GL_VARIABLE_A_NV                    0x8523
-#define GL_VARIABLE_B_NV                    0x8524
-#define GL_VARIABLE_C_NV                    0x8525
-#define GL_VARIABLE_D_NV                    0x8526
-#define GL_VARIABLE_E_NV                    0x8527
-#define GL_VARIABLE_F_NV                    0x8528
-#define GL_VARIABLE_G_NV                    0x8529
+#define GL_REGISTER_COMBINERS_NV            ((GLenum) 0x8522)
+#define GL_COMBINER0_NV                     ((GLenum) 0x8550)
+#define GL_COMBINER1_NV                     ((GLenum) 0x8551)
+#define GL_COMBINER2_NV                     ((GLenum) 0x8552)
+#define GL_COMBINER3_NV                     ((GLenum) 0x8553)
+#define GL_COMBINER4_NV                     ((GLenum) 0x8554)
+#define GL_COMBINER5_NV                     ((GLenum) 0x8555)
+#define GL_COMBINER6_NV                     ((GLenum) 0x8556)
+#define GL_COMBINER7_NV                     ((GLenum) 0x8557)
+#define GL_VARIABLE_A_NV                    ((GLenum) 0x8523)
+#define GL_VARIABLE_B_NV                    ((GLenum) 0x8524)
+#define GL_VARIABLE_C_NV                    ((GLenum) 0x8525)
+#define GL_VARIABLE_D_NV                    ((GLenum) 0x8526)
+#define GL_VARIABLE_E_NV                    ((GLenum) 0x8527)
+#define GL_VARIABLE_F_NV                    ((GLenum) 0x8528)
+#define GL_VARIABLE_G_NV                    ((GLenum) 0x8529)
 /*      GL_ZERO */
-#define GL_CONSTANT_COLOR0_NV               0x852A
-#define GL_CONSTANT_COLOR1_NV               0x852B
+#define GL_CONSTANT_COLOR0_NV               ((GLenum) 0x852A)
+#define GL_CONSTANT_COLOR1_NV               ((GLenum) 0x852B)
 /*      GL_FOG */
-#define GL_PRIMARY_COLOR_NV                 0x852C
-#define GL_SECONDARY_COLOR_NV               0x852D
-#define GL_SPARE0_NV                        0x852E
-#define GL_SPARE1_NV                        0x852F
+#define GL_PRIMARY_COLOR_NV                 ((GLenum) 0x852C)
+#define GL_SECONDARY_COLOR_NV               ((GLenum) 0x852D)
+#define GL_SPARE0_NV                        ((GLenum) 0x852E)
+#define GL_SPARE1_NV                        ((GLenum) 0x852F)
 /*      GL_TEXTURE0_ARB */
 /*      GL_TEXTURE1_ARB */
-#define GL_UNSIGNED_IDENTITY_NV             0x8536
-#define GL_UNSIGNED_INVERT_NV               0x8537
-#define GL_EXPAND_NORMAL_NV                 0x8538
-#define GL_EXPAND_NEGATE_NV                 0x8539
-#define GL_HALF_BIAS_NORMAL_NV              0x853A
-#define GL_HALF_BIAS_NEGATE_NV              0x853B
-#define GL_SIGNED_IDENTITY_NV               0x853C
-#define GL_SIGNED_NEGATE_NV                 0x853D
-#define GL_E_TIMES_F_NV                     0x8531
-#define GL_SPARE0_PLUS_SECONDARY_COLOR_NV   0x8532
+#define GL_UNSIGNED_IDENTITY_NV             ((GLenum) 0x8536)
+#define GL_UNSIGNED_INVERT_NV               ((GLenum) 0x8537)
+#define GL_EXPAND_NORMAL_NV                 ((GLenum) 0x8538)
+#define GL_EXPAND_NEGATE_NV                 ((GLenum) 0x8539)
+#define GL_HALF_BIAS_NORMAL_NV              ((GLenum) 0x853A)
+#define GL_HALF_BIAS_NEGATE_NV              ((GLenum) 0x853B)
+#define GL_SIGNED_IDENTITY_NV               ((GLenum) 0x853C)
+#define GL_SIGNED_NEGATE_NV                 ((GLenum) 0x853D)
+#define GL_E_TIMES_F_NV                     ((GLenum) 0x8531)
+#define GL_SPARE0_PLUS_SECONDARY_COLOR_NV   ((GLenum) 0x8532)
 /*      GL_NONE */
-#define GL_SCALE_BY_TWO_NV                  0x853E
-#define GL_SCALE_BY_FOUR_NV                 0x853F
-#define GL_SCALE_BY_ONE_HALF_NV             0x8540
-#define GL_BIAS_BY_NEGATIVE_ONE_HALF_NV     0x8541
-#define GL_DISCARD_NV                       0x8530
-#define GL_COMBINER_INPUT_NV                0x8542
-#define GL_COMBINER_MAPPING_NV              0x8543
-#define GL_COMBINER_COMPONENT_USAGE_NV      0x8544
-#define GL_COMBINER_AB_DOT_PRODUCT_NV       0x8545
-#define GL_COMBINER_CD_DOT_PRODUCT_NV       0x8546
-#define GL_COMBINER_MUX_SUM_NV              0x8547
-#define GL_COMBINER_SCALE_NV                0x8548
-#define GL_COMBINER_BIAS_NV                 0x8549
-#define GL_COMBINER_AB_OUTPUT_NV            0x854A
-#define GL_COMBINER_CD_OUTPUT_NV            0x854B
-#define GL_COMBINER_SUM_OUTPUT_NV           0x854C
-#define GL_MAX_GENERAL_COMBINERS_NV         0x854D
-#define GL_NUM_GENERAL_COMBINERS_NV         0x854E
-#define GL_COLOR_SUM_CLAMP_NV               0x854F
+#define GL_SCALE_BY_TWO_NV                  ((GLenum) 0x853E)
+#define GL_SCALE_BY_FOUR_NV                 ((GLenum) 0x853F)
+#define GL_SCALE_BY_ONE_HALF_NV             ((GLenum) 0x8540)
+#define GL_BIAS_BY_NEGATIVE_ONE_HALF_NV     ((GLenum) 0x8541)
+#define GL_DISCARD_NV                       ((GLenum) 0x8530)
+#define GL_COMBINER_INPUT_NV                ((GLenum) 0x8542)
+#define GL_COMBINER_MAPPING_NV              ((GLenum) 0x8543)
+#define GL_COMBINER_COMPONENT_USAGE_NV      ((GLenum) 0x8544)
+#define GL_COMBINER_AB_DOT_PRODUCT_NV       ((GLenum) 0x8545)
+#define GL_COMBINER_CD_DOT_PRODUCT_NV       ((GLenum) 0x8546)
+#define GL_COMBINER_MUX_SUM_NV              ((GLenum) 0x8547)
+#define GL_COMBINER_SCALE_NV                ((GLenum) 0x8548)
+#define GL_COMBINER_BIAS_NV                 ((GLenum) 0x8549)
+#define GL_COMBINER_AB_OUTPUT_NV            ((GLenum) 0x854A)
+#define GL_COMBINER_CD_OUTPUT_NV            ((GLenum) 0x854B)
+#define GL_COMBINER_SUM_OUTPUT_NV           ((GLenum) 0x854C)
+#define GL_MAX_GENERAL_COMBINERS_NV         ((GLenum) 0x854D)
+#define GL_NUM_GENERAL_COMBINERS_NV         ((GLenum) 0x854E)
+#define GL_COLOR_SUM_CLAMP_NV               ((GLenum) 0x854F)
 
 typedef void (APIENTRY * PFNGLCOMBINERPARAMETERFVNVPROC) (GLenum pname, const GLfloat *params);
 typedef void (APIENTRY * PFNGLCOMBINERPARAMETERFNVPROC) (GLenum pname, GLfloat param);
