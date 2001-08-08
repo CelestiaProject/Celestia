a0dc4fc521798941385a7abd79666a84a9270554
Chris Laurel <claurel@gmail.com>
2001-08-08 23:48:22 +0000

    Rewrote all star name handling code to allow more than just one proper name, one designation, and one catalog number per star.

--- src/starname.h
+++ src/starname.h
@@ -9,24 +9,77 @@
 
 #ifndef _STARNAME_H_
 #define _STARNAME_H_
+
 #include <string>
+#include <iostream>
+#include <map>
 #include "basictypes.h"
 #include "constellation.h"
+#include "util.h"
 
-using std::string;
 
-class StarName
+class Greek
 {
-public:
-    StarName(string _name, string _designation, Constellation *_constel);
+ private:
+    Greek();
+    ~Greek();
 
-    string getName();
-    string getDesignation(); // get Bayer or Flamsteed designation
-    Constellation *getConstellation();
+ public:
+    enum Letter {
+        Alpha     =  1,
+        Beta      =  2,
+        Gamma     =  3,
+        Delta     =  4,
+        Epsilon   =  5,
+        Zeta      =  6,
+        Eta       =  7,
+        Theta     =  8,
+        Iota      =  9,
+        Kappa     = 10,
+        Lambda    = 11,
+        Mu        = 12,
+        Nu        = 13,
+        Xi        = 14,
+        Omicron   = 15,
+        Pi        = 16,
+        Rho       = 17,
+        Sigma     = 18,
+        Tau       = 19,
+        Upsilon   = 20,
+        Phi       = 21,
+        Chi       = 22,
+        Psi       = 23,
+        Omega     = 24,
+    };
 
-private:
-    string name;
-    string designation; // Bayer or Flamsteed designation
-    Constellation *constellation;
+    static const std::string& canonicalAbbreviation(const std::string&);
+
+ private:
+    static Greek* instance;
+    int nLetters;
+    std::string* names;
+    std::string* abbrevs;
 };
+
+
+class StarNameDatabase
+{
+ public:
+    StarNameDatabase();
+
+    typedef std::map<std::string, uint32, CompareIgnoringCasePredicate> NameIndex;
+    typedef std::multimap<uint32, std::string> NumberIndex;
+
+    void add(uint32, const std::string&);
+    uint32 findCatalogNumber(const std::string& name);
+    NumberIndex::const_iterator findFirstName(uint32 catalogNumber) const;
+    NumberIndex::const_iterator finalName() const;
+
+    static StarNameDatabase* readNames(std::istream&);
+
+ private:
+    NameIndex nameIndex;
+    NumberIndex numberIndex;
+};
+
 #endif // _STARNAME_H_
