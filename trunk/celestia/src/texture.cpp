137c2eb347e676d19fc55ba1a3e64b5390f50fe3
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2001-09-07 20:59:13 +0000

    Fixed normalMap() method so that it doesn't produce inverted bump maps.

--- trunk/celestia/src/texture.cpp
+++ trunk/celestia/src/texture.cpp
@@ -316,10 +316,8 @@ void CTexture::normalMap(float scale, bool wrap)
             float rmag = 1.0f / mag;
 
             int n = (i * width + j) * 4;
-            // npixels[n]     = (unsigned char) (128 + 127 * dy * rmag);
-            // npixels[n + 1] = (unsigned char) (128 - 127 * dx * rmag);
-            npixels[n]     = (unsigned char) (128 - 127 * dx * rmag);
-            npixels[n + 1] = (unsigned char) (128 + 127 * dy * rmag);
+            npixels[n]     = (unsigned char) (128 + 127 * dy * rmag);
+            npixels[n + 1] = (unsigned char) (128 - 127 * dx * rmag);
             npixels[n + 2] = (unsigned char) (128 + 127 * rmag);
             npixels[n + 3] = 255;
         }
