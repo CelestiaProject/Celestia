5a4204122eee9f0ef23aeafd86523fda2e9024e5
Chris Laurel <claurel@gmail.com>
2001-10-25 19:43:29 +0000

    Planetary atmosphere information.

---
+++ src/atmosphere.h
@@ -0,0 +1,28 @@
+// atmosphere.h
+//
+// Copyright (C) 2001 Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _ATMOSPHERE_H_
+#define _ATMOSPHERE_H_
+
+#include "basictypes.h"
+#include "color.h"
+
+
+class Atmosphere
+{
+ public:
+    Atmosphere() : height(0.0f) {};
+ public:
+    float height;
+    Color lowerColor;
+    Color upperColor;
+};
+
+#endif // _ATMOSPHERE_H_
+
