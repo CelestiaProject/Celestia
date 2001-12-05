238d82ee6ceeb50e3520fafe5b435620f94baaf9
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2001-12-05 02:51:43 +0000

    Added code to solve Kepler's Equation and compute the eccentric anomaly for non-circular orbits.

--- trunk/celestia/src/celengine/orbit.cpp
+++ trunk/celestia/src/celengine/orbit.cpp
@@ -7,9 +7,13 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
+#include <functional>
 #include <cmath>
 #include <celmath/mathlib.h>
+#include <celmath/solve.h>
 #include "orbit.h"
+
+using namespace std;
 
 
 EllipticalOrbit::EllipticalOrbit(double _semiMajorAxis,
@@ -37,12 +41,6 @@ EllipticalOrbit::EllipticalOrbit(double _semiMajorAxis,
 // and in fact the series diverges for e > 0.6627434
 static double solveKeplerSeries(double M, double e)
 {
-#if 0
-    return M + e * (sin(M) +
-                    e * ((0.5 * sin(2 * M)) +
-                         e * ((0.325 * sin(3 * M) - 0.125 * sin(M)) +
-                              e * ((1.0 / 3.0 * sin(4 * M) - 1.0 / 6.0 * sin(2 * M))))));
-#endif    
     return (M +
             e * sin(M) +
             e * e * 0.5 * sin(2 * M) +
@@ -52,6 +50,40 @@ static double solveKeplerSeries(double M, double e)
 }
 
 
+// Standard iteration for solving Kepler's Equation
+struct SolveKeplerFunc1 : public unary_function<double, double>
+{
+    double ecc;
+    double M;
+
+    SolveKeplerFunc1(double _ecc, double _M) : ecc(_ecc), M(_M) {};
+
+    double operator()(double x) const
+    {
+        return M + ecc * sin(x);
+    }
+};
+
+
+// Faster converging iteration for Kepler's Equation; more efficient
+// than above for orbits with eccentricities greater than 0.3.  This
+// is from Jean Meeus's _Astronomical Algorithms_ (2nd ed), p. 199
+struct SolveKeplerFunc2 : public unary_function<double, double>
+{
+    double ecc;
+    double M;
+
+    SolveKeplerFunc2(double _ecc, double _M) : ecc(_ecc), M(_M) {};
+
+    double operator()(double x) const
+    {
+        return x + (M + ecc * sin(x) - x) / (1 - ecc * cos(x));
+    }
+};
+
+
+typedef pair<double, double> Solution;
+
 // Return the offset from the barycenter
 Point3d EllipticalOrbit::positionAtTime(double t) const
 {
@@ -59,10 +91,39 @@ Point3d EllipticalOrbit::positionAtTime(double t) const
     double meanMotion = 2.0 * PI / period;
     double meanAnomaly = meanAnomalyAtEpoch + t * meanMotion;
 
-    // TODO: calculate the *real* eccentric anomaly.  This is a reasonable
-    // approximation only for nearly circular orbits
-    double eccAnomaly = meanAnomaly;
-    // eccAnomaly = solveKeplerSeries(meanAnomaly, eccentricity);
+    // Compute the eccentric anomaly from the mean anomaly
+    double eccAnomaly;
+    if (eccentricity == 0.0)
+    {
+        // Circular orbit
+        eccAnomaly = meanAnomaly;
+    }
+    else if (eccentricity < 0.2)
+    {
+        // Low eccentricity, so use the standard iteration technique
+        Solution sol = solve_iteration_fixed(SolveKeplerFunc1(eccentricity,
+                                                              meanAnomaly),
+                                             meanAnomaly, 5);
+        eccAnomaly = sol.first;
+    }
+    else if (eccentricity < 0.98)
+    {
+        // Higher eccentricity elliptical orbit; use a more complex but much
+        // faster converging iteration.
+        Solution sol = solve_iteration_fixed(SolveKeplerFunc2(eccentricity,
+                                                              meanAnomaly),
+                                             meanAnomaly, 6);
+        eccAnomaly = sol.first;
+
+        // Debugging
+        // printf("ecc: %f, error: %f mas\n",
+        //        eccentricity, radToDeg(sol.second) * 3600000);
+    }
+    else
+    {
+        // Need a technique for handling orbits that are nearly parabolic.
+        eccAnomaly = meanAnomaly;
+    }
 
     double x = semiMajorAxis * (cos(eccAnomaly) - eccentricity);
     double z = semiMajorAxis * sqrt(1 - square(eccentricity)) * -sin(eccAnomaly);
@@ -72,25 +133,6 @@ Point3d EllipticalOrbit::positionAtTime(double t) const
                Mat3d::yrotation(argOfPeriapsis));
 
     return R * Point3d(x, 0, z);
-#if 0
-    t = t - epoch;
-    double meanMotion = 2.0 * PI / period;
-    double meanAnomaly = meanAnomalyAtEpoch + t * meanMotion;
-
-    // TODO: calculate the *real* eccentric anomaly.  This is a reasonable
-    // approximation only for nearly circular orbits
-    double eccAnomaly = meanAnomaly;
-    // eccAnomaly = solveKeplerSeries(meanAnomaly, eccentricity);
-
-    double x = semiMajorAxis * (cos(eccAnomaly) - eccentricity);
-    double z = semiMajorAxis * sqrt(1 - square(eccentricity)) * sin(eccAnomaly);
-
-    Mat3d R = (Mat3d::yrotation(-ascendingNode) *
-               Mat3d::xrotation(inclination) *
-               Mat3d::yrotation(-argOfPeriapsis));
-
-    return R * Point3d(x, 0, z);
-#endif
 }
 
 
