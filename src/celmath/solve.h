f144539b77ca1b8976234c20dde541cbeb4add1c
Chris Laurel <claurel@gmail.com>
2001-12-05 03:46:28 +0000

    Added functions for solving an equation by iteration.

--- src/celmath/solve.h
+++ src/celmath/solve.h
@@ -34,3 +34,43 @@ template<class T, class F> std::pair<T, T> solve_bisection(F f,
 
     return std::make_pair(x, (upper - lower) / 2);
 }
+
+
+// Solve using iteration; terminate when error is below err or the maximum
+// number of iterations is reached.
+template<class T, class F> std::pair<T, T> solve_iteration(F f,
+                                                           T x0,
+                                                           T err,
+                                                           int maxIter = 100)
+{
+    T x = 0;
+    T x2 = x0;
+
+    for (int i = 0; i < maxIter; i++)
+    {
+        x = x2;
+        x2 = f(x);
+        if (abs(x2 - x) < err)
+            return std::make_pair(x2, x2 - x);
+    }
+
+    return std::make_pair(x2, x2 - x);
+}
+
+
+// Solve using iteration method and a fixed number of steps.
+template<class T, class F> std::pair<T, T> solve_iteration_fixed(F f,
+                                                                 T x0,
+                                                                 int maxIter)
+{
+    T x = 0;
+    T x2 = x0;
+
+    for (int i = 0; i < maxIter; i++)
+    {
+        x = x2;
+        x2 = f(x);
+    }
+
+    return std::make_pair(x2, x2 - x);
+}
