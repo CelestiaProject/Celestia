dcda83bf13a1758855162b2e619d5a4a0ab3b462
Clint Weisbrod <cweisbrod@cogeco.ca>
2002-02-28 23:21:27 +0000

    Moved miscellaneous Location menu support functions out of winmain.cpp.

---
+++ src/celestia/winlocations.h
@@ -0,0 +1,29 @@
+// winlocations.h
+// 
+// Copyright (C) 2002, Chris Laurel <claurel@shatters.net>
+//
+// Miscellaneous utilities for Locations UI implementation.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _WINLOCATIONS_H_
+#define _WINLOCATIONS_H_
+
+
+#include <windows.h>
+#include <commctrl.h>
+#include "favorites.h"
+#include "celestiacore.h"
+
+void BuildFavoritesMenu(HMENU, CelestiaCore*);
+HTREEITEM PopulateLocationFolders(HWND, CelestiaCore*, HINSTANCE);
+HTREEITEM PopulateLocationsTree(HWND, CelestiaCore*, HINSTANCE);
+void InsertLocationInFavorites(HWND, char*, CelestiaCore*);
+void DeleteLocationFromFavorites(HWND, CelestiaCore*);
+void RenameLocationInFavorites(HWND, char*, CelestiaCore*);
+//HTREEITEM GetTreeViewItemHandle(HWND, char*, HTREEITEM);
+
+#endif
