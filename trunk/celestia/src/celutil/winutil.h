c6022a6e08c189e3e38ef4b0a5b11005d6549574
cweisbrod <cweisbrod@10d76133-bd44-0410-b00f-95d021917061>
2002-02-28 22:35:34 +0000

    New modules for Windows-specific utility functions.

---
+++ trunk/celestia/src/celutil/winutil.h
@@ -0,0 +1,23 @@
+// winutil.h
+//
+// Copyright (C) 2002, Chris Laurel <claurel@shatters.net>
+//
+// Miscellaneous useful Windows-related functions.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _WINUTIL_H_
+#define _WINUTIL_H_
+
+#include <windows.h>
+#include <commctrl.h>
+
+void SetMouseCursor(LPCTSTR lpCursor);
+void CenterWindow(HWND hParent, HWND hWnd);
+void RemoveButtonDefaultStyle(HWND hWnd);
+void AddButtonDefaultStyle(HWND hWnd);
+
+#endif
