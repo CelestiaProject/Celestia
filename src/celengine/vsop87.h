566776544ff4510e22fe8c85b1b0daa00441b051
Chris Laurel <claurel@gmail.com>
2002-03-01 01:41:14 +0000

    Added support for calculation of planet positions from the VSOP87 theory.

---
+++ src/celengine/vsop87.h
@@ -0,0 +1,18 @@
+// vsop87.cpp
+//
+// Copyright (C) 2002, Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _CELENGINE_VSOP87_H_
+#define _CELENGINE_VSOP87_H_
+
+#include <string>
+#include <celengine/orbit.h>
+
+extern Orbit* CreateVSOP87Orbit(const std::string& name);
+
+#endif // _CELENGINE_VSOP87_H_
