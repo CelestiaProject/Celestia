6a81d9a52e465b56a127666b98e14b87de500641
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2002-08-24 07:58:30 +0000

    Eclipse finder tool for windows; original code by Christopher Andre

---
+++ trunk/celestia/src/celestia/wineclipses.h
@@ -0,0 +1,52 @@
+// wineclipses.h by Kendrix <kendrix@wanadoo.fr>
+// 
+// Copyright (C) 2001, Chris Laurel <claurel@shatters.net>
+//
+// Compute Solar Eclipses for our Solar System planets
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _WINECLIPSES_H_
+#define _WINECLIPSES_H_
+
+#include <vector>
+
+#include "celestiacore.h"
+
+class Eclipse
+{
+public:
+    Eclipse(int Y, int M, int D);
+    Eclipse(double JD);
+
+public:
+    Body* body;
+    std::string planete;
+    std::string sattelite;
+    astro::Date* date;
+    double startTime;
+    double endTime;
+};
+
+class EclipseFinder
+{
+ public:
+    EclipseFinder(HINSTANCE, HWND, CelestiaCore*);
+
+ public:
+    CelestiaCore* appCore;
+    HWND parent;
+    HWND hwnd;
+
+    std::string strPlaneteToFindOn;
+    SYSTEMTIME fromTime, toTime;
+    double TimetoSet_;
+    Body* BodytoSet_;
+    bool bSolar;
+};
+
+#endif // _WINECLIPSES_H_
+
