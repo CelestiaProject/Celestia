dc3ece3608f330a8f5bfb973a32b0720955df151
christey <christey@10d76133-bd44-0410-b00f-95d021917061>
2002-12-09 20:28:29 +0000

    Removed ALT-B shortcut for Add Bookmark.

--- trunk/celestia/src/celestia/kde/kcelbookmarkmenu.cpp
+++ trunk/celestia/src/celestia/kde/kcelbookmarkmenu.cpp
@@ -162,7 +162,7 @@ void KCelBookmarkMenu::addAddBookmark()
 {
   KAction * paAddBookmarks = new KAction( i18n( "&Add Bookmark" ),
                                           "bookmark_add",
-                                          0, //m_bIsRoot ? KStdAccel::addBookmark() : KShortcut(),
+                                          ALT + Key_B, //m_bIsRoot ? KStdAccel::addBookmark() : KShortcut(),
                                           this,
                                           SLOT( slotAddBookmark() ),
                                           m_actionCollection, m_bIsRoot ? "add_bookmark" : 0 );
