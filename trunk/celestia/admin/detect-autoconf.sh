312322babd2214807bba8ae5670bc5a1119949b3
christey <christey@10d76133-bd44-0410-b00f-95d021917061>
2003-04-14 19:15:33 +0000

    New build system

--- trunk/celestia/admin/detect-autoconf.sh
+++ trunk/celestia/admin/detect-autoconf.sh
@@ -5,7 +5,7 @@ AUTOCONF="autoconf"
 AUTOHEADER="autoheader"
 AUTOM4TE="autom4te"
 AUTOMAKE="automake"
-ACLOCAL="aclocal"
+ACLOCAL="aclocal -I macros"
 
 
 # We don't use variable here for remembering the type ... strings.
@@ -15,8 +15,12 @@ checkAutoconf()
 {
   if test -x "`$WHICH autoconf-2.5x`" ; then	
     AUTOCONF="`$WHICH autoconf-2.5x`"
+  elif test -x "`$WHICH autoconf-2.54`" ; then
+    AUTOCONF="`$WHICH autoconf-2.54`"
   elif test -x "`$WHICH autoconf-2.53`" ; then
     AUTOCONF="`$WHICH autoconf-2.53`"
+  elif test -x "`$WHICH autoconf-2.53a`" ; then
+    AUTOCONF="`$WHICH autoconf-2.53a`"
   elif test -x "`$WHICH autoconf-2.52`" ; then
     AUTOCONF="`$WHICH autoconf-2.52`"
   elif test -x "`$WHICH autoconf2.50`" ; then
@@ -29,9 +33,15 @@ checkAutoheader()
   if test -x "`$WHICH autoheader-2.5x`" ; then
     AUTOHEADER="`$WHICH autoheader-2.5x`"
     AUTOM4TE="`$WHICH autom4te-2.5x`"
+  elif test -x "`$WHICH autoheader-2.54`" ; then
+    AUTOHEADER="`$WHICH autoheader-2.54`"
+    AUTOM4TE="`$WHICH autom4te-2.54`"
   elif test -x "`$WHICH autoheader-2.53`" ; then
     AUTOHEADER="`$WHICH autoheader-2.53`"
     AUTOM4TE="`$WHICH autom4te-2.53`"
+  elif test -x "`$WHICH autoheader-2.53a`" ; then
+    AUTOHEADER="`$WHICH autoheader-2.53a`"
+    AUTOM4TE="`$WHICH autom4te-2.53a`"
   elif test -x "`$WHICH autoheader-2.52`" ; then
     AUTOHEADER="`$WHICH autoheader-2.52`"
   elif test -x "`$WHICH autoheader2.50`" ; then
@@ -58,7 +68,7 @@ checkWhich ()
 {
   WHICH=""
   for i in "type -p" "which" "type" ; do
-    T=`$i sh`
+    T=`$i sh 2> /dev/null`
     test -x "$T" && WHICH="$i" && break
   done
 }
