c30736e8de4569a9c5466a10c1fad2fb06bba339
hramsey <hramsey@10d76133-bd44-0410-b00f-95d021917061>
2003-06-18 23:19:46 +0000

    initial version

---
+++ macosx/SetTimeWindowController.h
@@ -0,0 +1,17 @@
+//
+//  SetTimeWindowController.h
+//  celestia
+//
+//  Created by Bob Ippolito on Tue May 28 2002.
+//  Copyright (c) 2002 Chris Laurel. All rights reserved.
+//
+#import <Cocoa/Cocoa.h>
+
+@interface SetTimeWindowController : NSWindowController
+{
+    IBOutlet NSTextField *dateField;
+    IBOutlet NSTextField *timeField;
+}
+- (IBAction)setTime:(id)sender;
+- (IBAction)showWindow:(id)sender;
+@end
