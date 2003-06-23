8b778e26fddd53b2b27682991eb35431caab7946
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2003-06-23 13:02:53 +0000

    Added method to get and set the feature type, and to parse USGS feature codes.

--- trunk/celestia/src/celengine/location.cpp
+++ trunk/celestia/src/celengine/location.cpp
@@ -7,16 +7,62 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
+#include <map>
 #include <celengine/location.h>
 #include <celutil/util.h>
 
 using namespace std;
+
+static map<string, uint32> FeatureNameToFlag;
+static bool featureTableInitialized = false;
+
+struct FeatureNameEntry
+{
+    char* name;
+    uint32 flag;
+};
+
+FeatureNameEntry FeatureNames[] =
+{
+    { "AA", Location::Crater },
+    { "VA", Location::Vallis },
+    { "MO", Location::Mons },
+    { "PM", Location::Planum },
+    { "CM", Location::Chasma },
+    { "PE", Location::Patera },
+    { "ME", Location::Mare },
+    { "RU", Location::Rupes },
+    { "TE", Location::Tessera },
+    { "RE", Location::Regio },
+    { "CH", Location::Chaos },
+    { "TA", Location::Terra },
+    { "AS", Location::Astrum },
+    { "CR", Location::Corona },
+    { "DO", Location::Dorsum },
+    { "FO", Location::Fossa },
+    { "CA", Location::Catena },
+    { "MN", Location::Mensa },
+    { "RI", Location::Rima },
+    { "UN", Location::Undae },
+    { "RT", Location::Reticulum },
+    { "PL", Location::Planitia },
+    { "LI", Location::Linea },
+    { "FL", Location::Fluctus },
+    { "FR", Location::Farrum },
+    { "LF", Location::LandingSite },
+    { "XX", Location::Other },
+    { "City", Location::City },
+    { "Observatory", Location::Observatory },
+    { "Landing Site", Location::LandingSite },
+    { "Crater", Location::Crater },
+};
 
 
 Location::Location() :
     position(0.0f, 0.0f, 0.0f),
     size(0.0f),
     importance(-1.0f),
+    featureType(Other),
     infoURL(NULL)
 {
 }
@@ -87,10 +133,36 @@ void Location::setInfoURL(const string& url)
 }
 
 
-static uint32 parseFeatureType(const string& s)
+uint32 Location::getFeatureType() const
 {
-    if (compareIgnoringCase(s, "city"))
-        return 1;
-    else
-        return 0;
+    return featureType;
 }
+
+
+void Location::setFeatureType(uint32 _featureType)
+{
+    featureType = _featureType;
+}
+
+
+static void initFeatureTypeTable()
+{
+    featureTableInitialized = true;
+
+    for (int i = 0; i < sizeof(FeatureNames) / sizeof(FeatureNames[0]); i++)
+    {
+        FeatureNameToFlag[FeatureNames[i].name] = FeatureNames[i].flag;
+    }
+}
+
+
+uint32 Location::parseFeatureType(const string& s)
+{
+    if (!featureTableInitialized)
+        initFeatureTypeTable();
+
+    int flag = FeatureNameToFlag[s];
+    return flag != 0 ? flag : Other;
+}
+
+
