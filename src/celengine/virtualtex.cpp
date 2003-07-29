10a91645036e3a0114aae422058eb2d7cbdf812c
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2003-07-29 14:59:42 +0000

    Implemented hierarchical tile system for virtual textures.

--- src/celengine/virtualtex.cpp
+++ src/celengine/virtualtex.cpp
@@ -11,27 +11,51 @@
 #include <fstream>
 #include <cmath>
 #include <cassert>
+#include <cstdio>
 #include "celutil/debug.h"
+#include "celutil/directory.h"
 #include "virtualtex.h"
 #include "gl.h"
 #include "parser.h"
 
 using namespace std;
 
+static const int MaxResolutionLevels = 10;
+
+
+// Virtual textures are composed of tiles that are loaded from the hard drive
+// as they become visible.  Hidden tiles may be evicted from graphics memory
+// to make room for other tiles when they become visible.
+//
+// The virtual texture consists of one or more levels of detail.  Each level
+// of detail is twice as wide and twice as high as the previous one, therefore
+// having four times as many tiles.  The height and width of each LOD must be
+// a power of two, with width = 2 * height.  The baseSplit determines the
+// number of tiles at the lowest LOD.  It is the log base 2 of the width in
+// tiles of LOD zero.  Though it's not required
 
 static bool isPow2(int x)
 {
     return ((x & (x - 1)) == 0);
 }
 
+static inline lodOffset(uint lod)
+{
+    return ((1 << (lod << 1)) - 1) & 0xaaaaaaaa;
+}
+
 
 VirtualTexture::VirtualTexture(const string& _tilePath,
                                unsigned int _baseSplit,
-                               unsigned int _tileSize) :
+                               unsigned int _tileSize,
+                               const string& _tileType) :
     Texture(0, 0),
     tilePath(_tilePath),
     baseSplit(_baseSplit),
-    tileSize(_tileSize)
+    tileSize(_tileSize),
+    ticks(0),
+    nResolutionLevels(0),
+    tileLookup(NULL)
 {
     assert(baseSplit > 0);
     assert(tileSize != 0 && isPow2(tileSize));
@@ -45,21 +69,171 @@ VirtualTexture::~VirtualTexture()
 
 const TextureTile VirtualTexture::getTile(int lod, int u, int v)
 {
-    if (lod == 0)
+    tilesRequested++;
+
+    if (lod >= nResolutionLevels || 
+        u < 0 || u >= (2 << lod) ||
+        v < 0 || v >= (1 << lod))
     {
         return TextureTile(0);
     }
     else
     {
-        return TextureTile(0);
+        lod <<= baseSplit;
+        u <<= baseSplit;
+        v <<= baseSplit;
+        TileQuadtreeNode* node = tileTree[u >> baseSplit];
+        Tile* tile = node->tile;
+        int tileLOD = 0;
+        int n = 0;
+
+        while (n < lod)
+        {
+            int child = (((v << 1) | u) >> n) & 3;
+            if (node->children[child] == NULL)
+            {
+                break;
+            }
+            else
+            {
+                if (node->tile != NULL)
+                    tile = node->tile;
+                tileLOD = n;
+            }
+            
+            node = node->children[node];
+            n++;
+        }
+
+        // No tile was found at all--not even the base texture was found
+        if (tile == NULL)
+            return TextureTile(0);
+
+        // Make the tile resident.
+        makeResident(tile, lod >> baseSplit, u >> baseSplit, v >> baseSplit);
+
+        // It's possible that we failed to make the tile resident, either
+        // because the texture file was bad, or there was an unresolvable
+        // out of memory situation.  In that case there is nothing else to
+        // do but return a texture tile with a null texture name.
+        if (node->tile->tex == NULL)
+        {
+            return TextureTile(0);
+        }
+        else
+        {
+            // Set up the texture subrect to be the entire texture
+            float texU = 0.0f;
+            float texV = 0.0f;
+            float texDU = 1.0f;
+            float texDV = 1.0f;
+            
+            return TextureTile(node->tile.tex->getName(),
+                               texU, texV, texDU, texDV);
+        }
     }
 }
 
 
 void VirtualTexture::bind()
 {
-    // Treating a virtual texture like an ordinary one will not work; it's a
+    // Treating a virtual texture like an ordinary one will not work; this is a
     // weakness in the class hierarchy.
+}
+
+
+void VirtualTexture::beginUsage()
+{
+    ticks++;
+    tilesRequested = 0;
+}
+
+
+void VirtualTexture::endUsage()
+{
+}
+
+
+#if 0
+uint VirtualTexture::tileIndex(unsigned int lod,
+                               unsigned int u, unsigned int v)
+{
+    unsigned int lodBase = lodOffset(lod + baseSplit) - lodOffset(baseSplit);
+    return lodBase + (v << (lod << 1)) + u;
+}
+#endif
+
+
+ImageTexture* VirtualTexture::loadTileTexture(uint lod, uint u, uint v)
+{
+    assert(lod < MaxResolutionLevels);
+    
+    char filename[32];
+    sprintf(filename, "level%d/tx_%d_%d", lod, u, v);
+    
+    string pathname = tilePath + filename + tileExt;
+    Image* img = LoadImageFromFile(pathname);
+    if (img == NULL)
+        return NULL;
+
+    ImageTexture* tex = NULL;
+    if (isPow2(img->getWidth()) && isPow2(img->getHeight()))
+        tex = new ImageTexture(*img, EdgeClamp, NoMipMaps);
+    delete img;
+
+    return tex;
+}
+
+
+void VirtualTexture::makeResident(Tile* tile, uint lod, uint u, uint v)
+{
+    // Should never be called for an already resident tile
+    assert(tile->tex == NULL);
+
+    // Possibly evict other tiles in order to make this one fit
+
+    tile->tex = loadTileTexture(lod, u, v);
+}
+
+
+void VirtualTexture::populateTileTree()
+{
+    tileExt = string(".") + _tileType;
+
+    // Count the number of resolution levels present
+    for (int i = 0; i < MaxResolutionLevels; i++)
+    {
+        char filename[32];
+        sprintf(filename, "level%d", i);
+        Directory* dir = OpenDirectory(tilePath + filename);
+        if (dir)
+        {
+            nResolutionLevels = i;
+
+            string filename;
+            while (dir->nextFile(filename))
+            {
+                int u = -1, v = -1;
+                if (sscanf(filename.c_str(), "tx_%d_%d.", &u, &v) == 2)
+                {
+                    if (u >= 0 && v >= 0 && u < (2 << i) && v < (1 << i))
+                    {
+                        // Found a tile, so add it to the quadtree
+                        Tile* tile = new Tile();
+                        addTileToTree(tile, nResolutionLevels,
+                                      (uint) u, (uint) v);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void VirtualTexture::addTileToTree(Tile* tile, unsigned int lod,
+                                   unsigned int u, unsigned int v)
+{
+    
 }
 
 
@@ -75,7 +249,7 @@ static VirtualTexture* CreateVirtualTexture(Hash* texParams,
 
     double baseSplit = 0.0;
     if (!texParams->getNumber("BaseSplit", baseSplit) ||
-        baseSplit < 1.0 || baseSplit != floor(baseSplit))
+        baseSplit < 0.0 || baseSplit != floor(baseSplit))
     {
         DPRINTF(0, "BaseSplit in virtual texture missing or has bad value\n");
         return NULL;
@@ -96,9 +270,13 @@ static VirtualTexture* CreateVirtualTexture(Hash* texParams,
         return NULL;
     }
 
-    return new VirtualTexture(path + "/" + imageDirectory,
+    string tileType = "dds";
+    texParams->getString("TileType", tileType);
+
+    return new VirtualTexture(path + "/" + imageDirectory + "/",
                               (unsigned int) baseSplit,
-                              (unsigned int) tileSize);
+                              (unsigned int) tileSize,
+                              tileType);
 }
 
 
@@ -130,6 +308,8 @@ static VirtualTexture* LoadVirtualTexture(istream& in, const string& path)
 VirtualTexture* LoadVirtualTexture(const string& filename)
 {
     ifstream in(filename.c_str(), ios::in);
+
+    cout << "LoadVirtualTexture: " << filename << '\n';
     
     if (!in.good())
     {
