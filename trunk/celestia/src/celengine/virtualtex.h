546fa53ce0fd1536ddecf183f893e39f3868510f
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2003-07-29 14:59:42 +0000

    Implemented hierarchical tile system for virtual textures.

--- trunk/celestia/src/celengine/virtualtex.h
+++ trunk/celestia/src/celengine/virtualtex.h
@@ -11,6 +11,7 @@
 #define _CELENGINE_VIRTUALTEX_H_
 
 #include <string>
+#include "celutil/basictypes.h"
 #include <celengine/texture.h>
 
 
@@ -19,16 +20,61 @@ class VirtualTexture : public Texture
  public:
     VirtualTexture(const std::string& _tilePath,
                    unsigned int _baseSplit,
-                   unsigned int _tileSize);
+                   unsigned int _tileSize,
+                   const std::string& tileType);
     ~VirtualTexture();
 
     virtual const TextureTile getTile(int lod, int u, int v);
     virtual void bind();
 
+    virtual void beginUsage();
+    virtual void endUsage();
+
+ private:
+    void populateTileTree();
+    void addTileToTree(Tile* tile, uint lod, uint v, uint u);
+    bool makeResident(Tile* tile, uint lod, uint v, uint u);
+    ImageTexture* loadTileTexture(uint lod, uint u, uint v);
+
+    struct Tile
+    {
+        Tile() : lastUsed(0), tex(NULL), loadFailed(false) {};
+        unsigned int lastUsed;
+        ImageTexture* tex;
+        bool loadFailed;
+    };
+
+    struct TileQuadtreeNode
+    {
+        TileQuadtreeNode()
+        {
+            for (int i = 0; i < 4; i++)
+                children[i] = NULL;
+        };
+                
+        Tile* tile;
+        TileQuadtreeNode* children[4];
+    };
+    
+    Tile* tiles;
+    Tile* findTile(unsigned int lod,
+                   unsigned int u, unsigned int v);
+
  private:
     std::string tilePath;
+    std::string tileExt;
     unsigned int baseSplit;
     unsigned int tileSize;
+    unsigned int ticks;
+    unsigned int tilesRequested;
+    unsigned int nResolutionLevels;
+
+    enum {
+        TileNotLoaded  = -1,
+        TileLoadFailed = -2,
+    };
+
+    TileQuadtreeNode tileTree[2];
 };
 
 
