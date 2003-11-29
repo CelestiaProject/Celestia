f69746f3adb0ead79b3781aecdd7c7a625c1e23e
hramsey <hramsey@10d76133-bd44-0410-b00f-95d021917061>
2003-11-29 22:21:12 +0000

    added local variable block

--- trunk/celestia/macosx/FavoritesDrawerController.m
+++ trunk/celestia/macosx/FavoritesDrawerController.m
@@ -109,10 +109,12 @@
     node = [favs addNewFavorite:nil];
 
     // kludge to fix name
+    {
     CelestiaAppCore* appCore = [CelestiaAppCore sharedAppCore];
     NSString* selName = [((CelestiaFavorite*)[node nodeValue]) selectionName];
     NSString* newName  = [ [ [appCore simulation] findObjectFromPath: selName ] briefName]; 
     [((CelestiaFavorite*)[node nodeValue]) setName: newName];
+    }
 
     [[CelestiaFavorites sharedFavorites] synchronize];
 //    [self outlineView:outlineView editItem:node];
