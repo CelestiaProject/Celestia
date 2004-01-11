64d0d9f6ee3820c52d334cd1a523654ef61ff0cd
christey <christey@10d76133-bd44-0410-b00f-95d021917061>
2004-01-11 16:16:14 +0000

    Added contributors list to About dialog.

--- src/celestia/kde/kdemain.cpp
+++ src/celestia/kde/kdemain.cpp
@@ -40,13 +40,25 @@ int main(int argc, char *argv[])
     aboutData.addAuthor("Chris Laurel",0, "claurel@shatters.net");
     aboutData.addAuthor("Clint Weisbrod",0, "cweisbrod@adelphia.net");
     aboutData.addAuthor("Fridger Schrempp",0, "t00fri@mail.desy.de");
-    aboutData.addAuthor("Bob Ippolito",0, "bob@redivi.com");
+    aboutData.addAuthor("Bob Ippolito", "Mac OS X version", "bob@redivi.com");
+    aboutData.addAuthor("Hank Ramsey", "Mac OS X version");
     aboutData.addAuthor("Christophe Teyssier", "KDE interface", "chris@teyssier.org");
 
+    aboutData.addCredit("Frank Gregorio", "Celestia User's Guide");
+    aboutData.addCredit("Hitoshi Suzuki", "Japanese README translation");
+    aboutData.addCredit("Christophe Teyssier", "DocBook and HTML conversion of User's Guide", "chris@teyssier.org");
+    aboutData.addCredit("Diego Rodriguez", "Acrobat conversion of User's Guide");
+
+    aboutData.addCredit("Deon Ramsey", "Unix installer, GTK interface");
+    aboutData.addCredit("Christophe André", "Eclipse finder and rendering of orbits", "kendrix@wanadoo.fr");
+    aboutData.addCredit("Colin Walters", "Endianness fixes");
+    aboutData.addCredit("Peter Chapman", "Orbit path rendering changes");
+    aboutData.addCredit("James Holmes");
+    aboutData.addCredit("Harald Schmidt", "Lua scripting enhancements, bug fixes");
+  
     KCmdLineArgs::init( argc, argv, &aboutData );
     KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
     KUniqueApplication::addCmdLineOptions();
-
 
     KdeUniqueCelestia a;
 
