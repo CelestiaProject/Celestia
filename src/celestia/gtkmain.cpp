b4e764bd367baf36eacf6f914f15bbacfb0a6097
Pat Suwalski <pat@suwalski.net>
2004-02-17 01:08:38 +0000

    Added a right-click context menu.

--- src/celestia/gtkmain.cpp
+++ src/celestia/gtkmain.cpp
@@ -329,13 +329,35 @@ static void menuRunDemo()
     appCore->charEntered('D');
 }
 
+// CALLBACK: Mark the selected object.
+static void menuMark()
+{
+	Simulation* sim = appCore->getSimulation();
+	if (sim->getUniverse() != NULL)
+	{
+		sim->getUniverse()->markObject(sim->getSelection(),
+						10.0f,
+						Color(0.0f, 1.0f, 0.0f, 0.9f),
+						Marker::Diamond,
+						1);
+	}
+}
+
+// CALLBACK: Unmark the selected object.
+static void menuUnMark()
+{
+	Simulation* sim = appCore->getSimulation();
+	if (sim->getUniverse() != NULL)
+		sim->getUniverse()->unmarkObject(sim->getSelection(), 1);
+}
+
 static void menuAbout()
 {
     const gchar* authors[] = {
         "Chris Laurel <claurel@shatters.net>",
         "Deon Ramsey <dramsey@sourceforge.net>",
         "Clint Weisbrod <cweisbrod@adelphia.net>",
-		"Fridger Schrempp <fridger.schrempp@dedy.de>",
+		"Fridger Schrempp <fridger.schrempp@desy.de>",
 		"Pat Suwalski <pat@suwalski.net>",
         NULL
     };
@@ -359,16 +381,20 @@ static void menuAbout()
 				NULL,
 				NULL);
 		#else
+		// Join the author array into a single string.
+		int i = 0;
+		string auth = "Celestia, (c) 2001-2004 Chris Laurel\n\n";
+		while (authors[i] != NULL) {
+			if (i != 0) auth += ",\n";
+			auth += authors[i];
+			i++;
+		}
+		
 		about = gtk_message_dialog_new(GTK_WINDOW(mainWindow),
 		                               GTK_DIALOG_DESTROY_WITH_PARENT,
 									   GTK_MESSAGE_INFO,
 									   GTK_BUTTONS_CLOSE,
-									   "Celestia, (c) 2001-2004 Chris Laurel\n%s,\n%s,\n%s,\n%s,\n%s",
-									   authors[0],
-									   authors[1],
-									   authors[2],
-									   authors[3],
-									   authors[4]);
+		                               (gchar *)auth.c_str());
 		#endif
 		if (about == NULL)
 		    return;
@@ -2814,6 +2840,327 @@ void createMainMenu(GtkWidget* window, GtkWidget** menubar)
 }
 
 
+// FUNCTION: Append a menu item and return pointer. Used for context menu.
+GtkMenuItem* AppendMenu(GtkWidget* parent, GtkSignalFunc callback, const gchar* name, int value) {
+	GtkWidget* menuitem;
+
+	// Check for separator
+	if (name == NULL) {
+		menuitem = gtk_separator_menu_item_new();
+	}
+	else {
+		menuitem = gtk_menu_item_new_with_mnemonic(name);
+	}
+
+	// Add handler
+	if (callback != NULL) {
+		g_signal_connect_swapped (G_OBJECT(menuitem), "activate",
+						G_CALLBACK(callback),
+						(gpointer)value);
+	}
+
+	gtk_menu_shell_append(GTK_MENU_SHELL(parent), menuitem);
+	return GTK_MENU_ITEM(menuitem);
+}
+
+
+// CALLBACK: Handle a planetary selection from the context menu.
+void handleContextPlanet(int value) {
+	// Handle the satellite/child object submenu
+	Selection sel = appCore->getSimulation()->getSelection();
+	switch (sel.getType())
+	{
+		case Selection::Type_Star:
+			appCore->getSimulation()->selectPlanet(value);
+			break;
+
+		case Selection::Type_Body:
+			{
+				PlanetarySystem* satellites = (PlanetarySystem*) sel.body()->getSatellites();
+				appCore->getSimulation()->setSelection(Selection(satellites->getBody(value)));
+				break;
+			}
+
+		case Selection::Type_DeepSky:
+			// Current deep sky object/galaxy implementation does
+			// not have children to select.
+			break;
+
+		case Selection::Type_Location:
+			break;
+
+		default:
+			break;
+	}
+}
+
+
+// CALLBACK: Handle an alternate surface from the context menu.
+void handleContextSurface(int value) {
+	// Handle the alternate surface submenu
+	Selection sel = appCore->getSimulation()->getSelection();
+	if (sel.body() != NULL)
+	{
+		guint index = value - 1;
+		vector<string>* surfNames = sel.body()->getAlternateSurfaceNames();
+		if (surfNames != NULL)
+		{
+			string surfName;
+			if (index < surfNames->size())
+				surfName = surfNames->at(index);
+			appCore->getSimulation()->getActiveObserver()->setDisplayedSurface(surfName);
+			delete surfNames;
+		}
+	}
+}
+
+// Typedefs and structs for sorting objects by name in context menu.
+typedef pair<int,string> IntStrPair;
+typedef vector<IntStrPair> IntStrPairVec;
+
+struct IntStrPairComparePredicate
+{
+    IntStrPairComparePredicate() : dummy(0) {}
+
+	bool operator()(const IntStrPair pair1, const IntStrPair pair2) const
+	{
+		return (pair1.second.compare(pair2.second) < 0);
+	}
+
+	int dummy;
+};
+
+// FUNCTION: Create planetary submenu for context menu, return menu pointer.
+GtkMenu* CreatePlanetarySystemMenu(string parentName, const PlanetarySystem* psys)
+{
+	// Modified from winmain.cpp
+
+    // Use some vectors to categorize and sort the bodies within this PlanetarySystem.
+	// In order to generate sorted menus, we must carry the name and menu index as a
+	// single unit in the sort. One obvious way is to create a vector<pair<int,string>>
+	// and then use a comparison predicate to sort the vector based on the string in
+	// each pair.
+
+	// Declare vector<pair<int,string>> objects for each classification of body
+	vector<IntStrPair> asteroids;
+	vector<IntStrPair> comets;
+	vector<IntStrPair> invisibles;
+	vector<IntStrPair> moons;
+	vector<IntStrPair> planets;
+	vector<IntStrPair> spacecraft;
+
+	// We will use these objects to iterate over all the above vectors
+	vector<IntStrPairVec> objects;
+	vector<string> menuNames;
+
+	// Place each body in the correct vector based on classification
+	GtkWidget* menu = gtk_menu_new();
+	for (int i = 0; i < psys->getSystemSize(); i++)
+	{
+		Body* body = psys->getBody(i);
+		switch(body->getClassification())
+		{
+			case Body::Asteroid:
+				asteroids.push_back(make_pair(i, body->getName()));
+				break;
+			case Body::Comet:
+				comets.push_back(make_pair(i, body->getName()));
+				break;
+			case Body::Invisible:
+				invisibles.push_back(make_pair(i, body->getName()));
+				break;
+			case Body::Moon:
+				moons.push_back(make_pair(i, body->getName()));
+				break;
+			case Body::Planet:
+				planets.push_back(make_pair(i, body->getName()));
+				break;
+			case Body::Spacecraft:
+				spacecraft.push_back(make_pair(i, body->getName()));
+				break;
+		}
+	}
+
+	// Add each vector of PlanetarySystem bodies to a vector to iterate over
+	objects.push_back(asteroids);
+	menuNames.push_back("Asteroids");
+	objects.push_back(comets);
+	menuNames.push_back("Comets");
+	objects.push_back(invisibles);
+	menuNames.push_back("Invisibles");
+	objects.push_back(moons);
+	menuNames.push_back("Moons");
+	objects.push_back(planets);
+	menuNames.push_back("Planets");
+	objects.push_back(spacecraft);
+	menuNames.push_back("Spacecraft");
+
+	// Now sort each vector and generate submenus
+	IntStrPairComparePredicate pred;
+	vector<IntStrPairVec>::iterator obj;
+	vector<IntStrPair>::iterator it;
+	vector<string>::iterator menuName;
+	GtkWidget* subMenu;
+	int numSubMenus;
+
+	// Count how many submenus we need to create
+	numSubMenus = 0;
+	for (obj=objects.begin(); obj != objects.end(); obj++)
+	{
+		if (obj->size() > 0)
+			numSubMenus++;
+	}
+
+	menuName = menuNames.begin();
+	for (obj=objects.begin(); obj != objects.end(); obj++)
+	{
+		// Only generate a submenu if the vector is not empty
+		if (obj->size() > 0)
+		{
+			// Don't create a submenu for a single item
+			if (obj->size() == 1)
+			{
+				it=obj->begin();
+				AppendMenu(menu, GTK_SIGNAL_FUNC(handleContextPlanet), it->second.c_str(), it->first);
+			}
+			else
+			{
+				// Skip sorting if we are dealing with the planets in our own Solar System.
+				if (parentName != "Sol" || *menuName != "Planets")
+					sort(obj->begin(), obj->end(), pred);
+
+				if (numSubMenus > 1)
+				{
+					// Add items to submenu
+					subMenu = gtk_menu_new();
+					//hSubMenu = CreatePopupMenu();
+					for(it=obj->begin(); it != obj->end(); it++)
+						AppendMenu(subMenu, GTK_SIGNAL_FUNC(handleContextPlanet), it->second.c_str(), it->first);
+
+					gtk_menu_item_set_submenu(AppendMenu(menu, NULL, menuName->c_str(), 0), GTK_WIDGET(subMenu));
+				}
+				else
+				{
+					// Just add items to the popup
+					for(it=obj->begin(); it != obj->end(); it++)
+						AppendMenu(menu, GTK_SIGNAL_FUNC(handleContextPlanet), it->second.c_str(), it->first);
+				}
+			}
+		}
+		menuName++;
+	}
+
+	return GTK_MENU(menu);
+}
+
+
+// FUNCTION: Create surface submenu for context menu, return menu pointer.
+GtkMenu* CreateAlternateSurfaceMenu(const vector<string>& surfaces)
+{
+	GtkWidget* menu = gtk_menu_new();
+
+	AppendMenu(menu, GTK_SIGNAL_FUNC(handleContextSurface), "Normal", 0);
+	for (guint i = 0; i < surfaces.size(); i++)
+	{
+		AppendMenu(menu, GTK_SIGNAL_FUNC(handleContextSurface), surfaces[i].c_str(), i+1);
+	}
+
+	return GTK_MENU(menu);
+}
+
+
+// CALLBACK: Context menu (event handled by appCore)
+// Normally, float x and y, but unused, so removed.
+void contextMenu(float, float, Selection sel) {
+	GtkWidget* popup = gtk_menu_new();
+	string name;
+
+	switch (sel.getType())
+	{
+		case Selection::Type_Body:
+		{
+			name = sel.body()->getName();
+			AppendMenu(popup, menuCenter, name.c_str(), 0);
+			AppendMenu(popup, NULL, NULL, 0);
+			AppendMenu(popup, menuGoto, "_Goto", 0);
+			AppendMenu(popup, menuFollow, "_Follow", 0);
+			AppendMenu(popup, menuSync, "S_ync Orbit", 0);
+			AppendMenu(popup, NULL, "_Info", 0);
+
+			const PlanetarySystem* satellites = sel.body()->getSatellites();
+			if (satellites != NULL && satellites->getSystemSize() != 0)
+			{
+				GtkMenu* satMenu = CreatePlanetarySystemMenu(name, satellites);
+				gtk_menu_item_set_submenu(AppendMenu(popup, NULL, "_Satellites", 0), GTK_WIDGET(satMenu));
+			}
+
+			vector<string>* altSurfaces = sel.body()->getAlternateSurfaceNames();
+			if (altSurfaces != NULL)
+			{
+				if (altSurfaces->size() > 0)
+				{
+					GtkMenu* surfMenu = CreateAlternateSurfaceMenu(*altSurfaces);
+					gtk_menu_item_set_submenu(AppendMenu(popup, NULL, "_Alternate Surfaces", 0), GTK_WIDGET(surfMenu));
+					delete altSurfaces;
+				}
+			}
+		}
+		break;
+
+		case Selection::Type_Star:
+		{
+			Simulation* sim = appCore->getSimulation();
+			name = sim->getUniverse()->getStarCatalog()->getStarName(*(sel.star()));
+			AppendMenu(popup, menuCenter, name.c_str(), 0);
+			AppendMenu(popup, NULL, NULL, 0);
+			AppendMenu(popup, menuGoto, "_Goto", 0);
+			// *** Add info eventually
+			// AppendMenu(popup, NULL, "_Info", 0);
+
+			SolarSystemCatalog* solarSystemCatalog = sim->getUniverse()->getSolarSystemCatalog();
+			SolarSystemCatalog::iterator iter = solarSystemCatalog->find(sel.star()->getCatalogNumber());
+			if (iter != solarSystemCatalog->end())
+			{
+				SolarSystem* solarSys = iter->second;
+				GtkMenu* planetsMenu = CreatePlanetarySystemMenu(name, solarSys->getPlanets());
+				if (name == "Sol")
+					gtk_menu_item_set_submenu(AppendMenu(popup, NULL, "Orbiting Bodies", 0), GTK_WIDGET(planetsMenu));
+				else
+					gtk_menu_item_set_submenu(AppendMenu(popup, NULL, "Planets", 0), GTK_WIDGET(planetsMenu));
+			}
+		}
+		break;
+
+		case Selection::Type_DeepSky:
+		{
+			AppendMenu(popup, menuCenter, sel.deepsky()->getName().c_str(), 0);
+			AppendMenu(popup, NULL, NULL, 0);
+			AppendMenu(popup, menuGoto, "_Goto", 0);
+			AppendMenu(popup, menuFollow, "_Follow", 0);
+			// *** Add info eventually
+			// AppendMenu(popup, NULL, "_Info", 0);
+		}
+		break;
+
+		case Selection::Type_Location:
+			break;
+
+		default:
+			break;
+	}
+
+	if (appCore->getSimulation()->getUniverse()->isMarked(sel, 1))
+		AppendMenu(popup, menuUnMark, "_Unmark", 0);
+	else
+		AppendMenu(popup, menuMark, "_Mark", 0);
+
+	appCore->getSimulation()->setSelection(sel);
+
+	gtk_widget_show_all(popup);	
+	gtk_menu_popup(GTK_MENU(popup), NULL, NULL, NULL, NULL, 0, gtk_get_current_event_time());
+}
+
+
 // GL CALLBACKS ---------------------------------------------------------------
 
 // Keeps track of whether GL area is initialized
@@ -2945,6 +3292,7 @@ gint glarea_button_press(GtkWidget*, GdkEventButton* event, gpointer)
 {
     lastX = (int) event->x;
     lastY = (int) event->y;
+
     if (event->button == 1)
         appCore->mouseButtonDown(event->x, event->y, CelestiaCore::LeftButton);
     else if (event->button == 2)
@@ -2959,6 +3307,7 @@ gint glarea_button_release(GtkWidget*, GdkEventButton* event, gpointer)
 {
     lastX = (int) event->x;
     lastY = (int) event->y;
+
     if (event->button == 1)
         appCore->mouseButtonUp(event->x, event->y, CelestiaCore::LeftButton);
     else if (event->button == 2)
@@ -3455,6 +3804,8 @@ int main(int argc, char* argv[])
     g_assert(menuItemFactory);
     resyncAll();
 
+	appCore->setContextMenuCallback(contextMenu);
+	
 	// Call Main GTK Loop
 	gtk_main();
     
