1811442dad6ced173e5a8f7c4adff50e4164cf90
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2004-02-17 04:47:28 +0000

    Added new filetype for Celestia models.

--- trunk/celestia/src/celutil/filetype.h
+++ trunk/celestia/src/celutil/filetype.h
@@ -13,22 +13,23 @@
 #include <string>
 
 enum ContentType {
-    Content_JPEG           = 1,
-    Content_BMP            = 2,
-    Content_GIF            = 3,
-    Content_PNG            = 4,
-    Content_Targa          = 5,
-    Content_CelestiaTexture = 6,
-    Content_3DStudio       = 7,
-    Content_CelestiaMesh   = 8,
-    Content_AVI            = 9,
-    Content_CelestiaCatalog = 10,
-    Content_DDS             = 11,
-    Content_CelestiaStarCatalog = 12,
+    Content_JPEG                   = 1,
+    Content_BMP                    = 2,
+    Content_GIF                    = 3,
+    Content_PNG                    = 4,
+    Content_Targa                  = 5,
+    Content_CelestiaTexture        = 6,
+    Content_3DStudio               = 7,
+    Content_CelestiaMesh           = 8,
+    Content_AVI                    = 9,
+    Content_CelestiaCatalog        = 10,
+    Content_DDS                    = 11,
+    Content_CelestiaStarCatalog    = 12,
     Content_CelestiaDeepSkyCatalog = 13,
     Content_CelestiaScript         = 14,
     Content_CelestiaLegacyScript   = 15,
-    Content_Unknown         = -1,
+    Content_CelestiaModel          = 16,
+    Content_Unknown                = -1,
 };
 
 ContentType DetermineFileType(const std::string& filename);
