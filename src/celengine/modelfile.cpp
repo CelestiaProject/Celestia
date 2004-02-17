57e83cf06320904c2bba1bdbd2a4e0d99c7e566a
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2004-02-17 03:20:58 +0000

    Improved mesh support: - Use more efficient and flexible internal representation - Added code to convert 3DS meshes to new structure - Support new Celestia mesh format (ASCII-only right now)

---
+++ src/celengine/modelfile.cpp
@@ -0,0 +1,555 @@
+// modelfile.cpp
+//
+// Copyright (C) 2004, Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#include "modelfile.h"
+#include <celengine/tokenizer.h>
+
+#include <cstring>
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+
+class BinaryModelLoader : public ModelLoader
+{
+public:
+    BinaryModelLoader(istream& _in);
+    ~BinaryModelLoader();
+
+    virtual Model* load();
+
+private:
+    istream& in;
+};
+
+
+class AsciiModelLoader : public ModelLoader
+{
+public:
+    AsciiModelLoader(istream& _in);
+    ~AsciiModelLoader();
+
+    virtual Model* load();
+
+    Mesh::Material*          loadMaterial();
+    Mesh::VertexDescription* loadVertexDescription();
+    Mesh*                    loadMesh();
+    char*                    loadVertices(const Mesh::VertexDescription& vertexDesc,
+                                          uint32& vertexCount);
+
+private:
+    Tokenizer tok;
+};
+
+
+
+ModelLoader::ModelLoader()
+{
+}
+
+
+ModelLoader::~ModelLoader()
+{
+}
+
+
+void
+ModelLoader::reportError(const string& msg)
+{
+    errorMessage = msg;
+}
+
+
+const string& 
+ModelLoader::getErrorMessage() const
+{
+    return errorMessage;
+}
+
+
+
+Model* LoadModel(istream& in)
+{
+    ModelLoader* loader = NULL;
+    char header[CEL_MODEL_HEADER_LENGTH + 1];
+    memset(header, '\0', sizeof(header));
+
+    in.read(header, CEL_MODEL_HEADER_LENGTH);
+    if (strcmp(header, CEL_MODEL_HEADER_ASCII) == 0)
+    {
+        loader = new AsciiModelLoader(in);
+    }
+    else if (strcmp(header, CEL_MODEL_HEADER_BINARY) == 0)
+    {
+        loader = new BinaryModelLoader(in);
+    }
+    else
+    {
+        cerr << "Model file has invalid header.\n";
+        return NULL;
+    }
+
+    Model* model = loader->load();
+    if (model == NULL)
+        cout << "Loading error: " << loader->getErrorMessage() << '\n';
+
+    delete loader;
+
+    return model;
+}
+
+
+AsciiModelLoader::AsciiModelLoader(istream& _in) :
+    tok(&_in)
+{
+}
+
+
+AsciiModelLoader::~AsciiModelLoader()
+{
+}
+
+
+Mesh::Material*
+AsciiModelLoader::loadMaterial()
+{
+    if (tok.nextToken() != Tokenizer::TokenName ||
+        tok.getNameValue() != "material")
+    {
+        reportError("Material definition expected");
+        return NULL;
+    }
+
+    Mesh::Material* material = new Mesh::Material();
+
+    material->diffuse = Color(0.0f, 0.0f, 0.0f);
+    material->specular = Color(0.0f, 0.0f, 0.0f);
+    material->emissive = Color(0.0f, 0.0f, 0.0f);
+    material->specularPower = 1.0f;
+    material->opacity = 1.0f;
+
+    while (tok.nextToken() == Tokenizer::TokenName &&
+           tok.getNameValue() != "end")
+    {
+        string property = tok.getNameValue();
+        double data[3];
+
+        // All material properties are 3-vectors except specular power and
+        // opacity
+        int nValues = 3;
+        if (property == "specpower" || property == "opacity")
+            nValues = 1;
+
+        for (int i = 0; i < nValues; i++)
+        {
+            if (tok.nextToken() != Tokenizer::TokenNumber)
+            {
+                reportError("Bad property value in material");
+                delete material;
+                return NULL;
+            }
+            data[i] = tok.getNumberValue();
+        }
+
+        Color colorVal;
+        if (nValues == 3)
+            colorVal = Color((float) data[0], (float) data[1], (float) data[2]);
+
+        if (property == "diffuse")
+            material->diffuse = colorVal;
+        else if (property == "specular")
+            material->specular = colorVal;
+        else if (property == "emissive")
+            material->emissive = colorVal;
+        else if (property == "opacity")
+            material->opacity = (float) data[0];
+        else if (property == "specpower")
+            material->specularPower = (float) data[0];
+    }    
+
+    if (tok.getTokenType() != Tokenizer::TokenName)
+    {
+        delete material;
+        return NULL;
+    }
+    else
+    {
+        return material;
+    }
+}
+
+
+Mesh::VertexDescription*
+AsciiModelLoader::loadVertexDescription()
+{
+    if (tok.nextToken() != Tokenizer::TokenName ||
+        tok.getNameValue() != "vertexdesc")
+    {
+        reportError("Vertex description expected");
+        return NULL;
+    }
+
+    int maxAttributes = 16;
+    int nAttributes = 0;
+    uint32 offset = 0;
+    Mesh::VertexAttribute* attributes = new Mesh::VertexAttribute[maxAttributes];
+
+    while (tok.nextToken() == Tokenizer::TokenName &&
+           tok.getNameValue() != "end")
+    {
+        string semanticName;
+        string formatName;
+        bool valid = false;
+
+        if (nAttributes == maxAttributes)
+        {
+            // TODO: Should eliminate the attribute limit, though no real vertex
+            // will ever exceed it.
+            reportError("Attribute limit exceeded in vertex description");
+            delete[] attributes;
+            return NULL;
+        }
+
+        semanticName = tok.getNameValue();
+
+        if (tok.nextToken() == Tokenizer::TokenName)
+        {
+            formatName = tok.getNameValue();
+            valid = true;
+        }
+
+        if (!valid)
+        {
+            reportError("Invalid vertex description");
+            delete[] attributes;
+            return NULL;
+        }
+
+        Mesh::VertexAttributeSemantic semantic =
+            Mesh::parseVertexAttributeSemantic(semanticName);
+        if (semantic == Mesh::InvalidSemantic)
+        {
+            reportError(string("Invalid vertex attribute semantic '") +
+                        semanticName + "'");
+            delete[] attributes;
+            return NULL;
+        }
+
+        Mesh::VertexAttributeFormat format = 
+            Mesh::parseVertexAttributeFormat(formatName);
+        if (format == Mesh::InvalidFormat)
+        {
+            reportError(string("Invalid vertex attribute format '") +
+                        formatName + "'");
+            delete[] attributes;
+            return NULL;
+        }
+
+        attributes[nAttributes].semantic = semantic;
+        attributes[nAttributes].format = format;
+        attributes[nAttributes].offset = offset;
+
+        offset += Mesh::getVertexAttributeSize(format);
+        nAttributes++;
+    }
+
+    if (tok.getTokenType() != Tokenizer::TokenName)
+    {
+        reportError("Invalid vertex description");
+        delete[] attributes;
+        return NULL;
+    }
+
+    if (nAttributes == 0)
+    {
+        reportError("Vertex definitition cannot be empty");
+        delete[] attributes;
+        return NULL;
+    }
+
+    return new Mesh::VertexDescription(offset, nAttributes, attributes);
+}
+
+
+char*
+AsciiModelLoader::loadVertices(const Mesh::VertexDescription& vertexDesc,
+                               uint32& vertexCount)
+{
+    if (tok.nextToken() != Tokenizer::TokenName ||
+        tok.getNameValue() != "vertices")
+    {
+        reportError("Vertex data expected");
+        return NULL;
+    }
+
+    if (tok.nextToken() != Tokenizer::TokenNumber)
+    {
+        reportError("Vertex count expected");
+        return NULL;
+    }
+
+    double num = tok.getNumberValue();
+    if (num != floor(num) || num <= 0.0)
+    {
+        reportError("Bad vertex count for mesh");
+        return NULL;
+    }
+
+    vertexCount = (uint32) num;
+    uint32 vertexDataSize = vertexDesc.stride * vertexCount;
+    char* vertexData = new char[vertexDataSize];
+    if (vertexData == NULL)
+    {
+        reportError("Not enough memory to hold vertex data");
+        return NULL;
+    }
+
+    uint32 offset = 0;
+    double data[4];
+    for (uint32 i = 0; i < vertexCount; i++, offset += vertexDesc.stride)
+    {
+        assert(offset < vertexDataSize);
+        for (uint32 attr = 0; attr < vertexDesc.nAttributes; attr++)
+        {
+            Mesh::VertexAttributeFormat fmt = vertexDesc.attributes[attr].format;
+            uint32 nBytes = Mesh::getVertexAttributeSize(fmt);
+            int readCount = 0;
+            switch (fmt)
+            {
+            case Mesh::Float1:
+                readCount = 1;
+                break;
+            case Mesh::Float2:
+                readCount = 2;
+                break;
+            case Mesh::Float3:
+                readCount = 3;
+                break;
+            case Mesh::Float4:
+            case Mesh::UByte4:
+                readCount = 4;
+                break;
+            default:
+                assert(0);
+                delete[] vertexData;
+                return NULL;
+            }
+
+            for (int j = 0; j < readCount; j++)
+            {
+                if (tok.nextToken() != Tokenizer::TokenNumber)
+                {
+                    reportError("Error in vertex data");
+                    delete[] vertexData;
+                }
+                data[j] = tok.getNumberValue();
+            }
+
+            uint32 base = offset + vertexDesc.attributes[attr].offset;
+            if (fmt == Mesh::UByte4)
+            {
+                for (int k = 0; k < readCount; k++)
+                {
+                    reinterpret_cast<unsigned char*>(vertexData + base)[k] =
+                        (unsigned char) (data[k]);
+                }
+            }
+            else
+            {
+                for (int k = 0; k < readCount; k++)
+                    reinterpret_cast<float*>(vertexData + base)[k] = data[k];
+            }
+        }
+    }
+
+    return vertexData;
+}
+
+                               
+Mesh*
+AsciiModelLoader::loadMesh()
+{
+    if (tok.nextToken() != Tokenizer::TokenName ||
+        tok.getNameValue() != "mesh")
+    {
+        reportError("Mesh definition expected");
+        return NULL;
+    }
+    
+    Mesh::VertexDescription* vertexDesc = loadVertexDescription();
+    if (vertexDesc == NULL)
+        return NULL;
+
+    uint32 vertexCount = 0;
+    char* vertexData = loadVertices(*vertexDesc, vertexCount);
+    if (vertexData == NULL)
+        return NULL;
+
+    Mesh* mesh = new Mesh();
+    mesh->setVertexDescription(*vertexDesc);
+    mesh->setVertices(vertexCount, vertexData);
+
+    if (tok.nextToken() != Tokenizer::TokenName ||
+        tok.getNameValue() != "groups")
+    {
+        reportError("Primitive group list expected");
+        delete mesh;
+        return NULL;
+    }
+ 
+    while (tok.nextToken() == Tokenizer::TokenName &&
+           tok.getNameValue() != "end")
+    {
+        Mesh::PrimitiveGroupType type =
+            Mesh::parsePrimitiveGroupType(tok.getNameValue());
+        if (type == Mesh::InvalidPrimitiveGroupType)
+        {
+            reportError("Bad primitive group type: " + tok.getNameValue());
+            delete mesh;
+            return NULL;
+        }
+
+        if (tok.nextToken() != Tokenizer::TokenNumber)
+        {
+            reportError("Material index expected in primitive group");
+            delete mesh;
+            return NULL;
+        }
+        uint32 materialIndex = (uint32) tok.getNumberValue();
+
+        if (tok.nextToken() != Tokenizer::TokenNumber)
+        {
+            reportError("Index count expected in primitive group");
+            delete mesh;
+            return NULL;
+        }
+        uint32 indexCount = (uint32) tok.getNumberValue();
+        
+        uint32* indices = new uint32[indexCount];
+        if (indices == NULL)
+        {
+            reportError("Not enough memory to hold indices");
+            delete mesh;
+            return NULL;
+        }
+
+        for (uint32 i = 0; i < indexCount; i++)
+        {
+            if (tok.nextToken() != Tokenizer::TokenNumber)
+            {
+                reportError("Incomplete index list in primitive group");
+                delete indices;
+                delete mesh;
+                return NULL;
+            }
+        }
+
+        mesh->addGroup(type, materialIndex, indexCount, indices);
+    }
+
+    return mesh;
+}
+
+
+Model*
+AsciiModelLoader::load()
+{
+    Model* model = new Model();
+    bool seenMeshes = false;
+
+    if (model == NULL)
+    {
+        reportError("Unable to allocate memory for model");
+        return NULL;
+    }
+
+    // Parse material and mesh definitions
+    for (;;)
+    {
+        Tokenizer::TokenType ttype = tok.nextToken();
+
+        if (ttype == Tokenizer::TokenEnd)
+        {
+            break;
+        }
+        else if (ttype == Tokenizer::TokenName)
+        {
+            string name = tok.getNameValue();
+            tok.pushBack();
+
+            cout << "token " << name << '\n';
+
+            if (name == "material")
+            {
+                if (seenMeshes)
+                {
+                    reportError("Materials must be defined before meshes");
+                    delete model;
+                    return NULL;
+                }
+
+                Mesh::Material* material = loadMaterial();
+                if (material == NULL)
+                {
+                    delete model;
+                    return NULL;
+                }
+
+                model->addMaterial(material);
+            }
+            else if (name == "mesh")
+            {
+                seenMeshes = true;
+
+                Mesh* mesh = loadMesh();
+                if (mesh == NULL)
+                {
+                    delete model;
+                    return NULL;
+                }
+
+                model->addMesh(mesh);
+            }
+            else
+            {
+                reportError(string("Error: Unknown block type ") + name);
+                delete model;
+                return NULL;
+            }
+        }
+        else
+        {
+            reportError("Block name expected");
+            return NULL;
+        }
+    }
+     
+    return model;
+}
+
+
+
+BinaryModelLoader::BinaryModelLoader(istream& _in) :
+    in(_in)
+{
+}
+
+
+BinaryModelLoader::~BinaryModelLoader()
+{
+}
+
+
+Model*
+BinaryModelLoader::load()
+{
+    // TODO: Binary model loading not yet implemented
+    return NULL;
+}
---
+++ src/celengine/modelfile.h
@@ -0,0 +1,42 @@
+// modelfile.h
+//
+// Copyright (C) 2004, Chris Laurel <claurel@shatters.net>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _CELMESH_MODELFILE_H_
+#define _CELMESH_MODELFILE_H_
+
+#include "model.h"
+#include <iostream>
+#include <string>
+
+#define CEL_MODEL_HEADER_LENGTH 16
+#define CEL_MODEL_HEADER_ASCII "#celmodel__ascii"
+#define CEL_MODEL_HEADER_BINARY "#celmodel_binary"
+
+class ModelLoader
+{
+ public:
+    ModelLoader();
+    virtual ~ModelLoader();
+
+    virtual Model* load() = 0;
+
+    const std::string& getErrorMessage() const;
+
+ protected:
+    void reportError(const std::string&);
+
+ private:
+    std::string errorMessage;
+};
+
+
+
+Model* LoadModel(std::istream&);
+
+#endif // !_CELMESH_MODEL_H_
