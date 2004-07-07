8a692bc773a16d734fe833ad7886609e080c7c33
Chris Laurel <claurel@gmail.com>
2004-07-07 09:53:09 +0000

    Mesh and rendering cleanup: - Add semantic-indexed attribute list to VertexDescription class - Abstracted RenderContext so that it's an interface with implementations for various render paths. - Moved all GL code from Mesh class into RenderContext This work will enable normal and specular maps for meshes (soon)

--- src/celengine/rendcontext.cpp
+++ src/celengine/rendcontext.cpp
@@ -16,11 +16,50 @@
 
 static Mesh::Material defaultMaterial;
 
+static GLenum GLPrimitiveModes[Mesh::PrimitiveTypeMax] = 
+{
+    GL_TRIANGLES,
+    GL_TRIANGLE_STRIP,
+    GL_TRIANGLE_FAN,
+    GL_LINES,
+    GL_LINE_STRIP,
+    GL_POINTS
+};
+
+static GLenum GLComponentTypes[Mesh::FormatMax] = 
+{
+     GL_FLOAT,          // Float1
+     GL_FLOAT,          // Float2
+     GL_FLOAT,          // Float3
+     GL_FLOAT,          // Float4,
+     GL_UNSIGNED_BYTE,  // UByte4
+};
+
+static int GLComponentCounts[Mesh::FormatMax] =
+{
+     1,  // Float1
+     2,  // Float2
+     3,  // Float3
+     4,  // Float4,
+     4,  // UByte4
+};
+
+
+enum {
+    TangentAttributeIndex = 6,
+};
+
+
+static void
+setStandardVertexArrays(const Mesh::VertexDescription& desc,
+                        void* vertexData);
+static void
+setExtendedVertexArrays(const Mesh::VertexDescription& desc,
+                        const void* vertexData);
+
 
 RenderContext::RenderContext() :
     material(&defaultMaterial),
-    specularOn(false),
-    blendOn(false),
     locked(false)
 {
 }
@@ -35,12 +74,60 @@ RenderContext::RenderContext(const Mesh::Material* _material)
 }
 
 
+static void setVertexArrays(const Mesh::VertexDescription& desc)
+{
+}
+
+
 void
-RenderContext::makeCurrent()
+RenderContext::setMaterial(const Mesh::Material* newMaterial)
+{
+    if (!locked)
+    {
+        if (newMaterial == NULL)
+            newMaterial = &defaultMaterial;
+
+        if (newMaterial != material)
+        {
+            material = newMaterial;
+            makeCurrent(*material);
+        }
+    }
+}
+
+
+void
+RenderContext::drawGroup(const Mesh::PrimitiveGroup& group)
+{
+    glDrawElements(GLPrimitiveModes[(int) group.prim],
+                   group.nIndices,
+                   GL_UNSIGNED_INT,
+                   group.indices);
+}
+
+
+FixedFunctionRenderContext::FixedFunctionRenderContext() :
+    RenderContext(),
+    blendOn(false),
+    specularOn(false)
+{
+}
+
+
+FixedFunctionRenderContext::FixedFunctionRenderContext(const Mesh::Material* _material) :
+    RenderContext(_material),
+    blendOn(false),
+    specularOn(false)
+{
+}
+
+
+void
+FixedFunctionRenderContext::makeCurrent(const Mesh::Material& m)
 {
     Texture* t = NULL;
-    if (material->tex0 != InvalidResource)
-        t = GetTextureManager()->find(material->tex0);
+    if (m.tex0 != InvalidResource)
+        t = GetTextureManager()->find(m.tex0);
 
     if (t == NULL)
     {
@@ -53,7 +140,7 @@ RenderContext::makeCurrent()
     }
 
     bool blendOnNow = false;
-    if (material->opacity != 1.0f || (t != NULL && t->hasAlpha()))
+    if (m.opacity != 1.0f || (t != NULL && t->hasAlpha()))
         blendOnNow = true;
 
     if (blendOnNow != blendOn)
@@ -70,12 +157,12 @@ RenderContext::makeCurrent()
         }
     }
 
-    glColor4f(material->diffuse.red(),
-              material->diffuse.green(),
-              material->diffuse.blue(),
-              material->opacity);
+    glColor4f(m.diffuse.red(),
+              m.diffuse.green(),
+              m.diffuse.blue(),
+              m.opacity);
 
-    if (material->specular == Color::Black)
+    if (m.specular == Color::Black)
     {
         float matSpecular[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
         float zero = 0.0f;
@@ -85,53 +172,139 @@ RenderContext::makeCurrent()
     }
     else
     {
-        float matSpecular[4] = { material->specular.red(),
-                                 material->specular.green(),
-                                 material->specular.blue(),
+        float matSpecular[4] = { m.specular.red(),
+                                 m.specular.green(),
+                                 m.specular.blue(),
                                  1.0f };
         glMaterialfv(GL_FRONT, GL_SPECULAR, matSpecular);
-        glMaterialfv(GL_FRONT, GL_SHININESS, &material->specularPower);
+        glMaterialfv(GL_FRONT, GL_SHININESS, &m.specularPower);
         specularOn = true;
     }
 
     {
-        float matEmissive[4] = { material->emissive.red(),
-                                 material->emissive.green(),
-                                 material->emissive.blue(),
+        float matEmissive[4] = { m.emissive.red(),
+                                 m.emissive.green(),
+                                 m.emissive.blue(),
                                  1.0f };
         glMaterialfv(GL_FRONT, GL_EMISSION, matEmissive);
     }
-
-
 }
 
 
 void
-RenderContext::setMaterial(const Mesh::Material* newMaterial)
+FixedFunctionRenderContext::setVertexArrays(const Mesh::VertexDescription& desc, void* vertexData)
 {
-    if (!locked)
-    {
-        if (newMaterial == NULL)
-            newMaterial = &defaultMaterial;
+    setStandardVertexArrays(desc, vertexData);
+}
 
-        if (newMaterial != material)
-        {
-            material = newMaterial;
-            makeCurrent();
-        }
+
+void
+VP_Combiner_RenderContext::setVertexArrays(const Mesh::VertexDescription& desc, void* vertexData)
+{
+    setStandardVertexArrays(desc, vertexData);
+    setExtendedVertexArrays(desc, vertexData);
+}
+
+
+void
+VP_FP_RenderContext::setVertexArrays(const Mesh::VertexDescription& desc, void* vertexData)
+{
+    setStandardVertexArrays(desc, vertexData);
+    setExtendedVertexArrays(desc, vertexData);
+}
+
+
+
+void
+setStandardVertexArrays(const Mesh::VertexDescription& desc,
+                        void* vertexData)
+{
+    const Mesh::VertexAttribute& position  = desc.getAttribute(Mesh::Position);
+    const Mesh::VertexAttribute& normal    = desc.getAttribute(Mesh::Normal);
+    const Mesh::VertexAttribute& color0    = desc.getAttribute(Mesh::Color0);
+    const Mesh::VertexAttribute& texCoord0 = desc.getAttribute(Mesh::Texture0);
+
+    // Can't render anything unless we have positions
+    if (position.format != Mesh::Float3)
+        return;
+
+    // Set up the vertex arrays
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, desc.stride,
+                    reinterpret_cast<char*>(vertexData) + position.offset);
+
+    // Set up the normal array
+    switch (normal.format)
+    {
+    case Mesh::Float3:
+        glEnableClientState(GL_NORMAL_ARRAY);
+        glNormalPointer(GLComponentTypes[(int) normal.format],
+                        desc.stride,
+                        reinterpret_cast<char*>(vertexData) + normal.offset);
+        break;
+    default:
+        glDisableClientState(GL_NORMAL_ARRAY);
+        break;
+    }
+
+    // Set up the color array
+    switch (color0.format)
+    {
+    case Mesh::Float3:
+    case Mesh::Float4:
+    case Mesh::UByte4:
+        glEnableClientState(GL_COLOR_ARRAY);
+        glColorPointer(GLComponentCounts[color0.format],
+                       GLComponentTypes[color0.format],
+                       desc.stride,
+                       reinterpret_cast<char*>(vertexData) + color0.offset);
+        break;
+    default:
+        glDisableClientState(GL_COLOR_ARRAY);
+        break;
+    }
+
+    // Set up the texture coordinate array
+    switch (texCoord0.format)
+    {
+    case Mesh::Float1:
+    case Mesh::Float2:
+    case Mesh::Float3:
+    case Mesh::Float4:
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glTexCoordPointer(GLComponentCounts[(int) texCoord0.format],
+                          GLComponentTypes[(int) texCoord0.format],
+                          desc.stride,
+                          reinterpret_cast<char*>(vertexData) + texCoord0.offset);
+        break;
+    default:
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+        break;
     }
 }
 
 
 void
-RenderContext::lock()
+setExtendedVertexArrays(const Mesh::VertexDescription& desc,
+                        const void* vertexData)
 {
-    locked = true;
-}
+    const Mesh::VertexAttribute& tangent  = desc.getAttribute(Mesh::Tangent);
+    const char* vertices = reinterpret_cast<const char*>(vertexData);
 
-
-void
-RenderContext::unlock()
-{
-    locked = false;
+    switch (tangent.format)
+    {
+    case Mesh::Float3:
+        glx::glEnableVertexAttribArrayARB(TangentAttributeIndex);
+        glEnableClientState(GL_NORMAL_ARRAY);
+        glx::glVertexAttribPointerARB(TangentAttributeIndex,
+                                      GLComponentCounts[(int) tangent.format],
+                                      GLComponentTypes[(int) tangent.format],
+                                      GL_FALSE,
+                                      desc.stride,
+                                      vertices + tangent.offset);
+        break;
+    default:
+        glx::glDisableVertexAttribArrayARB(TangentAttributeIndex);
+        break;
+    }
 }
--- src/celengine/rendcontext.h
+++ src/celengine/rendcontext.h
@@ -18,17 +18,60 @@ class RenderContext
     RenderContext(const Mesh::Material*);
     RenderContext();
 
-    void makeCurrent();
+    virtual void makeCurrent(const Mesh::Material&) = 0;
+    virtual void setVertexArrays(const Mesh::VertexDescription& desc,
+                                 void* vertexData) = 0;
+    virtual void drawGroup(const Mesh::PrimitiveGroup& group);
+
     void setMaterial(const Mesh::Material*);
-    void lock();
-    void unlock();
+    void lock() { locked = true; }
+    void unlock() { locked = false; }
+    bool isLocked() const { return locked; }
 
  private:
     const Mesh::Material* material;
-    bool specularOn;
-    bool blendOn;
     bool locked;
 };
+
+
+class FixedFunctionRenderContext : public RenderContext
+{
+ public:
+    FixedFunctionRenderContext(const Mesh::Material*);
+    FixedFunctionRenderContext();
+
+    virtual void makeCurrent(const Mesh::Material&);
+    virtual void setVertexArrays(const Mesh::VertexDescription& desc,
+                                 void* vertexData);
+
+ private:
+    bool blendOn;
+    bool specularOn;
+};
+
+
+class VP_FP_RenderContext : public RenderContext
+{
+ public:
+    VP_FP_RenderContext();
+    VP_FP_RenderContext(const Mesh::Material*);
+
+    virtual void makeCurrent(const Mesh::Material&);
+    virtual void setVertexArrays(const Mesh::VertexDescription& desc,
+                                 void* vertexData);
+};
+
+
+class VP_Combiner_RenderContext : public RenderContext
+{
+    VP_Combiner_RenderContext();
+    VP_Combiner_RenderContext(const Mesh::Material*);
+
+    virtual void makeCurrent(const Mesh::Material&);
+    virtual void setVertexArrays(const Mesh::VertexDescription& desc,
+                                 void* vertexData);
+};
+
 
 #endif // _CELENGINE_RENDCONTEXT_H_
 
