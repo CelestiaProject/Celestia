f794052b74953c90c9cf36792c4c0f334b86e080
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2004-10-12 17:09:57 +0000

    Added missing defines for GLSL extension

--- src/celengine/glext.h
+++ src/celengine/glext.h
@@ -915,8 +915,20 @@ namespace glx
 #define GL_PROGRAM_ERROR_STRING_NV        0x8874
 #endif /* GL_NV_fragment_program */
 
+#ifndef GL_ARB_vertex_shader
+#define GL_VERTEX_SHADER_ARB              0x8B31
+#define GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB 0x8B4A
+#define GL_MAX_VARYING_FLOATS_ARB         0x8B4B
+#define GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS_ARB 0x8B4C
+#define GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS_ARB 0x8B4D
+#define GL_OBJECT_ACTIVE_ATTRIBUTES_ARB   0x8B89
+#define GL_OBJECT_ACTIVE_ATTRIBUTE_MAX_LENGTH_ARB 0x8B8A
+#endif
+
 #ifndef GL_ARB_fragment_shader
 #define GL_ARB_fragment_shader 1
+#define GL_FRAGMENT_SHADER_ARB            0x8B30
+#define GL_MAX_FRAGMENT_UNIFORM_COMPONENTS_ARB 0x8B49
 #endif
 
 #ifndef GL_ARB_shading_language_100
