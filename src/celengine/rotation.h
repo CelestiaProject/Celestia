353bff580bad49eb250e456e67a39f9623ca926d
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2004-11-09 06:18:46 +0000

    Implemented phase lock and sync follow modes for stars.

--- src/celengine/rotation.h
+++ src/celengine/rotation.h
@@ -10,6 +10,7 @@
 #ifndef _CELENGINE_ROTATION_H_
 #define _CELENGINE_ROTATION_H_
 
+#include <celmath/quaternion.h>
 #include <celengine/astro.h>
 
 
@@ -17,6 +18,10 @@ class RotationElements
 {
  public:
     inline RotationElements();
+
+    inline Quatd eclipticalToEquatorial(double t) const;
+    inline Quatd eclipticalToPlanetographic(double t) const;
+    inline Quatd equatorialToPlanetographic(double t) const;
 
     float period;        // sidereal rotation period
     float offset;        // rotation at epoch
@@ -46,6 +51,38 @@ inline bool operator==(const RotationElements& re0, const RotationElements& re1)
             re0.obliquity      == re1.obliquity     &&
             re0.ascendingNode  == re1.ascendingNode &&
             re0.precessionRate == re1.precessionRate);
+}
+
+
+Quatd RotationElements::eclipticalToEquatorial(double t) const
+{
+    double Omega = (double) ascendingNode + precessionRate * (t - astro::J2000);
+
+    return (Quatd::xrotation(-obliquity) * Quatd::yrotation(-Omega));
+}
+
+
+Quatd RotationElements::eclipticalToPlanetographic(double t) const
+{
+    return equatorialToPlanetographic(t) * eclipticalToEquatorial(t);
+}
+
+
+Quatd RotationElements::equatorialToPlanetographic(double t) const
+{
+    double rotations = (t - epoch) / (double) period;
+    double wholeRotations = floor(rotations);
+    double remainder = rotations - wholeRotations;
+
+    // Add an extra half rotation because of the convention in all
+    // planet texture maps where zero deg long. is in the middle of
+    // the texture.
+    remainder += 0.5;
+    
+    Quatd q(1);
+    q.yrotate(-remainder * 2 * PI - offset);
+
+    return q;
 }
 
 #endif // _CELENGINE_ROTATION_H_
