07ad319dc22c2ec52c5e821bcdd1c08b30e6bf68
dirkpitt2050 <dirkpitt2050@10d76133-bd44-0410-b00f-95d021917061>
2005-05-28 19:01:37 +0000

    Better feedback when selecting unsupported render paths

--- trunk/celestia/macosx/RenderPanelController.h
+++ trunk/celestia/macosx/RenderPanelController.h
@@ -1,9 +1,22 @@
-/* RenderPanelController */
+//
+//  RenderPanelController.h
+//  celestia
+//
+//  2005-05 Modified substantially by Da Woon Jung
+//  Copyright (c) 2002 Chris Laurel. All rights reserved.
+//
 
 #import <Cocoa/Cocoa.h>
 
 @interface RenderPanelController : NSWindowController
 {
+    IBOutlet NSImageView *renderPathCautionIcon;
+    IBOutlet NSTextField *renderPathWarning;
 
+@private
+    NSView *_renderPathWarningSuper;
 }
+
+- (void)displayRenderPathWarning:(NSString *)warning;
+- (void)hideRenderPathWarning;
 @end
