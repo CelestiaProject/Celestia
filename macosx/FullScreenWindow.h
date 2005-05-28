a00047598723ced05b7f8af624c85417289d6d1a
Da Woon Jung <dirkpitt2050@users.sf.net>
2005-05-28 20:06:09 +0000

    Full screen mode

---
+++ macosx/FullScreenWindow.h
@@ -0,0 +1,15 @@
+//
+//  FullScreenWindow.h
+//  celestia
+//
+//  Created by Da Woon Jung on Wed Jul 21 2004.
+//  Copyright 2005 Da Woon Jung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FullScreenWindow : NSWindow
+
+- (id) initWithScreen: (NSScreen *) screen;
+@end
