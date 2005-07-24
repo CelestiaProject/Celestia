176282e62710ae5e391bf317aee3694ab3f7125e
dirkpitt2050 <dirkpitt2050@10d76133-bd44-0410-b00f-95d021917061>
2005-07-24 02:24:00 +0000

    Fixes to compile with latest code in CVS

---
+++ macosx/Util.h
@@ -0,0 +1,11 @@
+//
+//  Util.h
+//  celestia
+//
+//  Created by Da Woon Jung on 05/07/24.
+//  Copyright 2005 Chris Laurel. All rights reserved.
+//
+
+#ifndef gettext
+#define gettext(s)  s
+#endif
