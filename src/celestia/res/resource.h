3962a4c4f7b4fdaf9c00a7e3bfd5142c651ef4b8
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2005-12-22 05:57:53 +0000

    Added label nebulae checkbox to Windows view options dialog. Bumped version in about box to 1.4.0.

--- src/celestia/res/resource.h
+++ src/celestia/res/resource.h
@@ -114,6 +114,7 @@
 #define IDC_ECLIPSES_LIST               1071
 #define IDC_LABELCOMETS                 1072
 #define IDC_COMETORBITS                 1073
+#define IDC_LABELNEBULAE                1074
 #define IDCOMPUTE                       1078
 #define IDC_DATEFROM                    1079
 #define IDC_ECLIPSEFROM                 1079
