c8bdf02f1f4d7828f96eee73d52e2d0a5436b2ca
christey <christey@10d76133-bd44-0410-b00f-95d021917061>
2006-01-18 20:51:07 +0000

    Synched About box with AUTHORS and README files.

--- src/celestia/kde/kdemain.cpp
+++ src/celestia/kde/kdemain.cpp
@@ -46,21 +46,27 @@ int main(int argc, char *argv[])
     aboutData.addAuthor("Chris Laurel",0, "claurel@shatters.net");
     aboutData.addAuthor("Clint Weisbrod",0, "cweisbrod@adelphia.net");
     aboutData.addAuthor("Fridger Schrempp",0, "t00fri@mail.desy.de");
-    aboutData.addAuthor("Bob Ippolito", "Mac OS X version", "bob@redivi.com");
-    aboutData.addAuthor("Hank Ramsey", "Mac OS X version");
-    aboutData.addAuthor("Christophe Teyssier", "KDE interface", "chris@teyssier.org");
+    aboutData.addAuthor("Bob Ippolito", 0, "bob@redivi.com");
+    aboutData.addAuthor("Christophe Teyssier", 0, "chris@teyssier.org");
+    aboutData.addAuthor("Hank Ramsey", 0, "hramsey@users.sourceforge.net");
+    aboutData.addAuthor("Grant Hutchison", 0, "grantcelestia@xemaps.com");
+    aboutData.addAuthor("Pat Suwalski", 0, "pat@suwalski.net");
+    aboutData.addAuthor("Toti", 0);
+    aboutData.addAuthor("Da Woon Jung", 0, "dirkpitt2050@users.sf.net");
 
-    aboutData.addCredit("Frank Gregorio", "Celestia User's Guide");
-    aboutData.addCredit("Hitoshi Suzuki", "Japanese README translation");
-    aboutData.addCredit("Christophe Teyssier", "DocBook and HTML conversion of User's Guide", "chris@teyssier.org");
-    aboutData.addCredit("Diego Rodriguez", "Acrobat conversion of User's Guide");
-
-    aboutData.addCredit("Deon Ramsey", "Unix installer, GTK interface");
-    aboutData.addCredit("Christophe André", "Eclipse finder and rendering of orbits", "kendrix@wanadoo.fr");
+    aboutData.addCredit("Deon Ramsey", "UNIX installer, GTK1 interface");
+    aboutData.addCredit("Christophe André", "Eclipse finder");
     aboutData.addCredit("Colin Walters", "Endianness fixes");
     aboutData.addCredit("Peter Chapman", "Orbit path rendering changes");
     aboutData.addCredit("James Holmes");
     aboutData.addCredit("Harald Schmidt", "Lua scripting enhancements, bug fixes");
+
+    aboutData.addCredit("Frank Gregorio", "Celestia User's Guide");
+    aboutData.addCredit("Hitoshi Suzuki", "Japanese README translation");
+    aboutData.addCredit("Christophe Teyssier", "DocBook and HTML conversion of User's Guide");
+    aboutData.addCredit("Diego Rodriguez", "Acrobat conversion of User's Guide");
+    aboutData.addCredit("Don Goyette", "CEL Scripting Guide");
+    aboutData.addCredit("Harald Schmidt", "Celx/Lua Scripting Guide");
   
     KCmdLineArgs::init( argc, argv, &aboutData );
     KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
