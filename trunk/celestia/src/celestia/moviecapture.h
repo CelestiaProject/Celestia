cc8a11e1bc012384ab90ad54c67053f0ff4da6b5
toti1258 <toti1258@10d76133-bd44-0410-b00f-95d021917061>
2006-09-13 08:21:29 +0000

    Removed dummy assignments that caused compiler warnings

--- trunk/celestia/src/celestia/moviecapture.h
+++ trunk/celestia/src/celestia/moviecapture.h
@@ -29,8 +29,9 @@ class MovieCapture
     virtual int getWidth() const = 0;
     virtual int getHeight() const = 0;
     virtual float getFrameRate() const = 0;
-    virtual void setAspectRatio(int aspect_numerator, int aspect_denominator) {aspect_numerator/aspect_denominator;};
-    virtual void setQuality(float quality) {quality;};
+
+    virtual void setAspectRatio(int aspect_numerator, int aspect_denominator) {};
+    virtual void setQuality(float quality) {};
     virtual void recordingStatus(bool started) {}; /* to update UI recording status indicator */
 };
 
