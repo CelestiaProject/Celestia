af33e93e41c085ec48a3ccc9cc77631b182daf40
Da Woon Jung <dirkpitt2050@users.sf.net>
2006-09-17 02:33:17 +0000

    Updated for latest CVS changes (eg png galaxies)

--- macosx/Astro.h
+++ macosx/Astro.h
@@ -9,12 +9,6 @@
 #import "CelestiaUniversalCoord.h"
 #import "CelestiaVector.h"
 
-#define SOLAR_ABSMAG  4.83f
-#define LN_MAG        1.085736
-#define LY_PER_PARSEC 3.26167
-#define KM_PER_LY     9466411842000.000
-#define KM_PER_AU     149597870.7
-#define AU_PER_LY     (KM_PER_LY / KM_PER_AU)
 @interface NSDate(AstroAPI)
 +(NSDate*)dateWithJulian:(NSNumber*)jd;
 @end
