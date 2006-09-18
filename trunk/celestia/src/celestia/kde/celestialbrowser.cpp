0ec13265a345a45c43d3efc37d82b178be11de92
toti1258 <toti1258@10d76133-bd44-0410-b00f-95d021917061>
2006-09-18 22:22:54 +0000

    Fixed unused parameter/variable and control flow warnings

--- trunk/celestia/src/celestia/kde/celestialbrowser.cpp
+++ trunk/celestia/src/celestia/kde/celestialbrowser.cpp
@@ -17,9 +17,9 @@
 #include "selectionpopup.h"
 #include "celutil/utf8.h"
 
-/* 
- *  Constructs a CelestialBrowser which is a child of 'parent', with the 
- *  name 'name' and widget flags set to 'f' 
+/*
+ *  Constructs a CelestialBrowser which is a child of 'parent', with the
+ *  name 'name' and widget flags set to 'f'
  *
  *  The dialog will by default be modeless, unless you set 'modal' to
  *  TRUE to construct a modal dialog.
@@ -36,13 +36,13 @@ CelestialBrowser::CelestialBrowser( QWidget* parent, CelestiaCore* appCore)
     listStars->setColumnAlignment(2, Qt::AlignRight);
     listStars->setColumnAlignment(3, Qt::AlignRight);
     listStars->setShowSortIndicator(true);
-    
+
     sbrowser.setSimulation(appSim);
     radioNearest->setChecked(true);
     statusBar()->hide();
 }
 
-/*  
+/*
  *  Destroys the object and frees any allocated resources
  */
 CelestialBrowser::~CelestialBrowser()
@@ -97,10 +97,10 @@ void CelestialBrowser::slotRefresh()
 {
     StarDatabase* stardb = appSim->getUniverse()->getStarCatalog();
     sbrowser.refresh();
-    
+
     std::vector<const Star*> *stars = sbrowser.listStars(100);
-    
-    listStars->clear();  
+
+    listStars->clear();
     browserSel.obj = const_cast<Star*>((*stars)[0]);
     browserSel.type = Selection::Type_Star;
 
@@ -109,19 +109,19 @@ void CelestialBrowser::slotRefresh()
     UniversalCoord ucPos = appSim->getObserver().getPosition();
     Point3f obsPos( (double)ucPos.x * 1e-6,
                     (double)ucPos.y * 1e-6,
-                    (double)ucPos.z * 1e-6);    
+                    (double)ucPos.z * 1e-6);
 
     setlocale(LC_NUMERIC, "");
 
-    for (std::vector<const Star*>::iterator i = stars->begin(); 
-         i != stars->end() ; 
+    for (std::vector<const Star*>::iterator i = stars->begin();
+         i != stars->end() ;
          i++ )
     {
         const Star *star=(Star *)(*i);
 
         Point3f starPos = star->getPosition();
-        Vec3d v(starPos.x - obsPos.x, 
-                starPos.y - obsPos.y, 
+        Vec3d v(starPos.x - obsPos.x,
+                starPos.y - obsPos.y,
                 starPos.z - obsPos.z);
         float dist = v.length();
 
@@ -129,36 +129,36 @@ void CelestialBrowser::slotRefresh()
         CelListViewItem *starItem = new CelListViewItem(listStars, stardb->getStarName(*star), dist, _("ly"),
                 astro::absToAppMag(star->getAbsoluteMagnitude(), dist),
                 star->getAbsoluteMagnitude(), starClass);
-        
+
         SolarSystemCatalog::iterator iter = solarSystemCatalog->find(star->getCatalogNumber());
         if (iter != solarSystemCatalog->end())
-        { 
+        {
             const PlanetarySystem* planets = 0;
             SolarSystem* solarSys = iter->second;
             planets = solarSys->getPlanets();
-            
+
             for (int i = 0; i < solarSys->getPlanets()->getSystemSize(); i++)
             {
                 Body* body = solarSys->getPlanets()->getBody(i);
-                
+
                 Point3d bodyPos = body->getHeliocentricPosition(appSim->getTime());
                 double starBodyDist = bodyPos.distanceFromOrigin();
 
-                CelListViewItem *planetItem = new CelListViewItem(starItem, body->getName(true), 
+                CelListViewItem *planetItem = new CelListViewItem(starItem, body->getName(true),
                                             starBodyDist / KM_PER_AU, _("au"),
                                             0, 0, getClassification(body->getClassification()));
-                
+
                 const PlanetarySystem* satellites = body->getSatellites();
                 if (satellites != NULL && satellites->getSystemSize() != 0)
                 {
                         for (int i = 0; i < satellites->getSystemSize(); i++)
                         {
                                 Body* sat = satellites->getBody(i);
-                                
+
                                 Point3d satPos = sat->getHeliocentricPosition(appSim->getTime());
                                 Vec3d bodySatVec(bodyPos.x - satPos.x, bodyPos.y - satPos.y, bodyPos.z - satPos.z);
                                 double bodySatDist = bodySatVec.length();
-                                
+
                                 new CelListViewItem(planetItem, sat->getName(true),
                                     bodySatDist, "km", 0, 0, getClassification(sat->getClassification()));
 
@@ -174,7 +174,7 @@ void CelestialBrowser::slotRefresh()
 }
 
 
-QString CelestialBrowser::getClassification(int c) const{    
+QString CelestialBrowser::getClassification(int c) const{
     QString cl;
     switch(c)
         {
@@ -202,7 +202,7 @@ QString CelestialBrowser::getClassification(int c) const{
 }
 
 
-void CelestialBrowser::slotRightClickOnStar(QListViewItem* item, const QPoint& p, int col) {
+void CelestialBrowser::slotRightClickOnStar(QListViewItem* item, const QPoint& p, int /*col*/) {
     CelListViewItem *i = dynamic_cast<CelListViewItem*>(item);
     std::string name = i->getName();
     while ( (i = dynamic_cast<CelListViewItem*>(i->parent())) ) {
@@ -293,10 +293,6 @@ int CelListViewItem::compare ( QListViewItem * i, int col, bool ascending ) cons
         if (col == 1) return (dist > dynamic_cast<CelListViewItem*>(i)->getDist()) * 2 - 1;
         if (col == 2) return (app_mag > dynamic_cast<CelListViewItem*>(i)->getAppMag()) * 2 - 1;
         if (col == 3) return (abs_mag > dynamic_cast<CelListViewItem*>(i)->getAbsMag()) * 2 - 1;
+        else return -1; // NOTE: Control should never reach here
     }
 }
-
-
-
-
-
