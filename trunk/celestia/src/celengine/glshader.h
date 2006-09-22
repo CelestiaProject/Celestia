b8b327167c4fba82efe52e66fcd8f19c2d034ac0
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2006-09-22 04:58:02 +0000

    Fixed small memory leak in glshader.cpp.

--- trunk/celestia/src/celengine/glshader.h
+++ trunk/celestia/src/celengine/glshader.h
@@ -1,6 +1,6 @@
 // glshader.h
 //
-// Copyright (C) 2001-2004, Chris Laurel <claurel@shatters.net>
+// Copyright (C) 2001-2006, Chris Laurel <claurel@shatters.net>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
