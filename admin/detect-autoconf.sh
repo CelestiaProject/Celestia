8c4e32c89df39670339aa82c93c4d35c1f322868
christey <christey@10d76133-bd44-0410-b00f-95d021917061>
2006-10-03 21:32:58 +0000

    Added support for autoconf 2.60

--- admin/detect-autoconf.sh
+++ admin/detect-autoconf.sh
@@ -10,7 +10,7 @@ ACLOCAL="aclocal"
 # Please add higher versions first. The last version number is the minimum
 # needed to compile KDE. Do not forget to include the name/version #
 # separator if one is present, e.g. -1.2 where - is the separator.
-KDE_AUTOCONF_VERS="-2.58 -2.57 257 -2.54 -2.53a -2.53 -2.52 -2.5x"
+KDE_AUTOCONF_VERS="-2.60 -2.58 -2.57 257 -2.54 -2.53a -2.53 -2.52 -2.5x"
 KDE_AUTOMAKE_VERS="-1.7 17 -1.6"
 
 # We don't use variable here for remembering the type ... strings. Local 
