f55ed1dd3c4e6ae955e8fbff78a9248fe0cdbd6c
Toti <toti1258@users.sf.net>
2007-01-08 00:03:07 +0000

    Clarified variable names

--- src/celengine/deepskyobj.h
+++ src/celengine/deepskyobj.h
@@ -58,7 +58,7 @@ class DeepSkyObject
 
     virtual bool pick(const Ray3d& ray,
                       double& distanceToPicker,
-                      double& cosToBoundCenter) const = 0;
+                      double& cosAngleToBoundCenter) const = 0;
     virtual bool load(AssociativeArray*, const std::string& resPath);
     virtual void render(const GLContext& context,
                         const Vec3f& offset,
