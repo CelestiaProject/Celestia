470c3fce9b92177a235543366118d63165222831
Chris Laurel <claurel@gmail.com>
2008-01-06 05:45:49 +0000

    Checked in Qt4 front-end for Celestia.

---
+++ src/celestia/qt/qtcelestialbrowser.cpp
@@ -0,0 +1,24 @@
+// qtcelestialbrowser.cpp
+//
+// Copyright (C) 2007-2008, Celestia Development Team
+// celestia-developers@lists.sourceforge.net
+//
+// Dockable celestial browser widget.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#include "qtcelestialbrowser.h"
+
+
+CelestialBrowser::CelestialBrowser(QWidget* parent) :
+    QWidget(parent)
+{
+}
+
+
+CelestialBrowser::~CelestialBrowser()
+{
+}
