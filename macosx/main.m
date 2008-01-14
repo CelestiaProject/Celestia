347c3f3357e8481053f5a5844bb9e281238857a4
Da Woon Jung <dirkpitt2050@users.sf.net>
2008-01-14 08:36:20 +0000

    Make posix locales work by programmatically setting LANG at startup (works best on OS X 10.4 and later)

--- macosx/main.m
+++ macosx/main.m
@@ -8,6 +8,22 @@
 
 int main(int argc, const char *argv[])
 {
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_3
+    CFLocaleRef curLocale = CFLocaleCopyCurrent();
+    if (curLocale)
+    {
+        CFStringRef localeName = CFLocaleGetIdentifier(curLocale);
+        if (localeName)
+        {
+            char localeBuf[32];
+            if (CFStringGetCString(localeName, localeBuf, 32, kCFStringEncodingUTF8))
+            {
+                setenv("LANG", localeBuf, 1);
+            }
+        }
+        CFRelease(curLocale);
+    }
+#endif
     setlocale(LC_ALL, "");
     return NSApplicationMain(argc,argv);
 }
