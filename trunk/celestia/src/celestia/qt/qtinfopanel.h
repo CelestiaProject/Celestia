ba9ab6c8cdca5f07f3825f50738c5a717eecab7a
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2008-01-22 09:35:49 +0000

    Added a rudimentary info panel to the Qt4 interface.

---
+++ trunk/celestia/src/celestia/qt/qtinfopanel.h
@@ -0,0 +1,41 @@
+// qtcelestiaactions.h
+//
+// Copyright (C) 2008, Celestia Development Team
+// celestia-developers@lists.sourceforge.net
+//
+// Dockable information panel for Qt4 UI.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _INFOPANEL_H_
+#define _INFOPANEL_H_
+
+#include <QDockWidget>
+#include <QTextStream>
+#include "celengine/selection.h"
+
+class QTextBrowser;
+
+class InfoPanel : public QDockWidget
+{
+ public:
+    InfoPanel(const QString& title, QWidget* parent);
+    ~InfoPanel();
+
+    void buildInfoPage(Selection sel, double t);
+
+ private:
+    void pageHeader(QTextStream&);
+    void pageFooter(QTextStream&);
+    void buildSolarSystemBodyPage(const Body* body, double t, QTextStream&);
+    void buildStarPage(const Star* star, QTextStream&);
+    void buildDSOPage(const DeepSkyObject* dso, QTextStream&);
+
+ public:
+    QTextBrowser* textBrowser;
+};
+
+#endif // _INFOPANEL_H_
