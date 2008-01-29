f436fe6a47de74d2612d91889fb493f58498382f
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2008-01-29 22:57:29 +0000

    Implemented eclipse finder for Qt4 front-end.

---
+++ src/celestia/qt/qteventfinder.h
@@ -0,0 +1,73 @@
+// qteventfinder.h
+//
+// Copyright (C) 2008, Celestia Development Team
+// celestia-developers@lists.sourceforge.net
+//
+// Qt4 interface for the celestial event finder.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _QTEVENTFINDER_H_
+#define _QTEVENTFINDER_H_
+
+#include <QDockWidget>
+
+class QTreeView;
+class QRadioButton;
+class QDateEdit;
+class QComboBox;
+class QProgressDialog;
+class EventTableModel;
+class Universe;
+
+
+class EclipseFinderWatcher
+{
+public:
+    enum Status
+    {
+        ContinueOperation = 0,
+        AbortOperation = 1,
+    };
+
+    virtual Status eclipseFinderProgressUpdate(double t) = 0;
+};
+
+
+class EventFinder : public QDockWidget, EclipseFinderWatcher
+{
+Q_OBJECT
+
+ public:
+    EventFinder(Universe* u, const QString& title, QWidget* parent);
+    ~EventFinder();
+
+    EclipseFinderWatcher::Status eclipseFinderProgressUpdate(double t);
+    
+ public slots:
+    void slotFindEclipses();
+
+ private:
+    Universe* universe;
+
+    QRadioButton* solarOnlyButton;
+    QRadioButton* lunarOnlyButton;
+    QRadioButton* allEclipsesButton;
+
+    QDateEdit* startDateEdit;
+    QDateEdit* endDateEdit;
+
+    QComboBox* planetSelect;
+
+    EventTableModel* model;
+    QTreeView* eventTable;
+
+    QProgressDialog* progress;
+    double searchSpan;
+    double lastProgressUpdate;
+};
+
+#endif // _QTEVENTFINDER_H_
