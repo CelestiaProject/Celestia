fd04079eb1ab81e5365acb1d47bba0b48b961ea7
Chris Laurel <claurel@gmail.com>
2008-02-18 23:07:58 +0000

    Improved SPICE interface: - Permit no or multiple kernels to be specified for a SPICE orbit - Automatically determine valid range of SPICE orbit if no Beginning/Ending is specified. - Reorganized code for upcoming addition of SPICE C Kernel support

--- src/celengine/spiceinterface.cpp
+++ src/celengine/spiceinterface.cpp
@@ -11,8 +11,20 @@
 
 #include "SpiceUsr.h"
 #include "spiceinterface.h"
+#include <iostream>
+#include <set>
+
+using namespace std;
 
 
+// Track loaded SPICE kernels in order to avoid loading the same kernel
+// multiple times. This is a global variable because SPICE uses a global
+// kernel pool.
+static set<string> ResidentSpiceKernels;
+
+
+/*! Perform one-time initialization of SPICE.
+ */
 bool
 InitializeSpice()
 {
@@ -21,4 +33,82 @@ InitializeSpice()
     erract_c("SET", 0, (SpiceChar*)"RETURN");
 
     return true;
+}
+
+
+/*! Convert an object name to a NAIF integer ID. Return true if the name
+ *  refers to a known object, false if not. Both names and numeric IDs are
+ *  accepted in the string.
+ */
+bool GetNaifId(const string& name, int* id)
+{
+    SpiceInt spiceID = 0;
+    SpiceBoolean found = SPICEFALSE;
+
+    // Don't call bodn2c on an empty string because SPICE generates
+    // an error if we do.
+    if (!name.empty())
+    {
+        bodn2c_c(name.c_str(), &spiceID, &found);
+        if (found)
+        {
+            *id = (int) spiceID;
+        }
+        else   // Is it a numeric ID?
+        {
+            // SpiceInt maps to an int on those architectures where sizeof(long) != sizeof(double)/2.
+            // Otherwise it maps to a long. This avoids GCC complaints about type mismatches:
+            long spiceIDlng;
+            if (sscanf(name.c_str(), " %ld", &spiceIDlng) == 1)
+            {
+                *id = (int) spiceIDlng;
+                found = SPICETRUE;
+            }
+        }
+    }
+
+    return found == SPICETRUE;
+}
+
+
+/*! Return true if a SPICE kernel has already been loaded, false if not.
+ */
+bool IsSpiceKernelLoaded(const string& filepath)
+{
+	return ResidentSpiceKernels.find(filepath) != ResidentSpiceKernels.end();
+}
+
+
+/*! Load a SPICE kernel file of any type into the kernel pool. If the kernel
+ *  is already resident, it will not be reloaded.
+ */
+bool LoadSpiceKernel(const string& filepath)
+{
+	// Only load the kernel if it is not already resident. Note that this detection
+    // of duplicate kernels will not work if a file was originally loaded through
+    // a metakernel.
+	if (IsSpiceKernelLoaded(filepath))
+		return true;
+
+	ResidentSpiceKernels.insert(filepath);
+	furnsh_c(filepath.c_str());
+
+	// If there was an error loading the kernel, dump the error message.
+	if (failed_c())
+	{
+		char errMsg[1024];
+		getmsg_c("long", sizeof(errMsg), errMsg);
+		clog << errMsg << "\n";
+
+		// Reset the SPICE error state so that future calls to
+		// SPICE can still succeed.
+		reset_c();
+
+		return false;
+	}
+	else
+	{
+		clog << "Loaded SPK file " << filepath << "\n";
+		return true;
+	}
 }
--- src/celengine/spiceorbit.cpp
+++ src/celengine/spiceorbit.cpp
@@ -14,27 +14,53 @@
 #include "SpiceUsr.h"
 #include "astro.h"
 #include "spiceorbit.h"
+#include "spiceinterface.h"
 
 using namespace std;
 
 
-static bool getNAIFID(const string& name, int* id);
+static const double MILLISEC = astro::secsToDays(0.001);
 
-SpiceOrbit::SpiceOrbit(const std::string& _kernelFile,
-                       const std::string& _targetBodyName,
+/*! Create a new SPICE orbit using with a valid interval specified
+ *  by beginning and ending.
+ */
+SpiceOrbit::SpiceOrbit(const std::string& _targetBodyName,
                        const std::string& _originName,
                        double _period,
                        double _boundingRadius,
                        double _beginning,
                        double _ending) :
-    kernelFile(_kernelFile),
     targetBodyName(_targetBodyName),
     originName(_originName),
     period(_period),
     boundingRadius(_boundingRadius),
     spiceErr(false),
     validIntervalBegin(_beginning),
-    validIntervalEnd(_ending)
+    validIntervalEnd(_ending),
+	useDefaultTimeInterval(false)
+{
+}
+
+
+/*! Create a new SPICE orbit. The valid time interval is the first
+ *  window over which there is trajectory information for the target
+ *  object. All currently loaded kernels are considered when computing
+ *  the window. If there's noncontiguous coverage and a time interval
+ *  other than the first coverage span is desired, the SPICE orbit must
+ *  be constructed with an explicitly specified time range.
+ */
+SpiceOrbit::SpiceOrbit(const std::string& _targetBodyName,
+	                   const std::string& _originName,
+                       double _period,
+                       double _boundingRadius) :
+	targetBodyName(_targetBodyName),
+	originName(_originName),
+	period(_period),
+	boundingRadius(_boundingRadius),
+	spiceErr(false),
+	validIntervalBegin(0.0),
+	validIntervalEnd(0.0),
+	useDefaultTimeInterval(true)
 {
 }
 
@@ -66,137 +92,137 @@ SpiceOrbit::getPeriod() const
 
 
 bool
-SpiceOrbit::init(const std::string& path)
+SpiceOrbit::init(const string& path,
+                 const list<string>* requiredKernels)
 {
-    string filepath = path + string("/data/") + kernelFile;
-    furnsh_c(filepath.c_str());
-
-    // If there was an error loading the kernel, flag an error
-    // and don't bother calling SPICE to compute the orbit.
-    if (failed_c())
+    // Load required kernel files
+    if (requiredKernels != NULL)
     {
-        spiceErr = true;
-        char errMsg[1024];
-        getmsg_c("long", sizeof(errMsg), errMsg);
-        clog << errMsg << "\n";
-
-        // Reset the SPICE error state so that future calls to
-        // SPICE can still succeed.
-        reset_c();
+        for (list<string>::const_iterator iter = requiredKernels->begin(); iter != requiredKernels->end(); iter++)
+        {
+            string filepath = path + string("/data/") + *iter;
+	        if (!LoadSpiceKernel(filepath))
+            {    
+		        spiceErr = true;
+                break;
+            }
+        }
     }
 
-    clog << "Loaded SPK file " << filepath << "\n";
-
     // Get the ID codes for the target
-    if (!getNAIFID(targetBodyName, &targetID))
+    if (!GetNaifId(targetBodyName, &targetID))
     {
         clog << "Couldn't find SPICE ID for " << targetBodyName << "\n";
         spiceErr = true;
         return false;
     }
 
-    if (!getNAIFID(originName, &originID))
+    if (!GetNaifId(originName, &originID))
     {
         clog << "Couldn't find SPICE ID for " << originName << "\n";
         spiceErr = true;
         return false;
     }
 
-#if 0
-    for (handles)
-    {
-        dafbfs();
-        daffna();
-    }
-#endif
+	SpiceInt spkCount = 0;
+	ktotal_c("spk", &spkCount);
 
-#if 0
     // Get coverage window for target and origin object
-    const int MaxIntervals = 10;
-    SPICEDOUBLE_CELL ( cover, MaxIntervals * 2 );
+	const int MaxIntervals = 10;
+	SPICEDOUBLE_CELL ( targetCoverage, MaxIntervals * 2 );
 
-    // First check the coverage window of the target. No interval
-    // is required for ID 0 (the solar system barycenter) which is
-    // always at (0, 0, 0).
-    SpiceDouble targetBeginning = -1.0e50;
-    SpiceDouble targetEnding    = +1.0e50;
-    if (targetID != 0)
-    {
-        spkcov_c(filepath.c_str(), targetID, &cover);
+    // Clear the coverage window.
+    scard_c(0, &targetCoverage);
 
-        SpiceInt nIntervals = 0;
-        if (!failed_c())
-            nIntervals = card_c(&cover) / 2;
+	for (SpiceInt i = 0; i < spkCount; i++)
+	{
+		SpiceChar filename[512];
+		SpiceChar filetype[32];
+		SpiceChar source[256];
+		SpiceInt handle;
+		SpiceBoolean found;
 
-        clog << "nIntervals = " << nIntervals << "\n";
+		kdata_c(i, "spk",
+				sizeof(filename), sizeof(filetype), sizeof(source),
+				filename, filetype, source, &handle, &found);
 
-        // Only consider the first interval in the window.
-        // TODO: consider supporting noncontiguous coverage
-        if (nIntervals <= 0)
-        {
-            clog << "Couldn't find object " << targetBodyName << " in SPICE kernel " << kernelFile << "\n";
-            spiceErr = true;
-            if (failed_c())
-            {
-                reset_c();
-            }
-            return false;
-        }
-
-        wnfetd_c(&cover, 0, &targetBeginning, &targetEnding);
-        clog << astro::secsToDays(targetBeginning) << ", " <<
-            astro::secsToDays(targetEnding) << "\n";
-    }
-
-    // Check the coverage for the origin
-    SpiceDouble originBeginning = -1.0e50;
-    SpiceDouble originEnding    = +1.0e50;
-    if (originID != 0)
-    {
-        spkcov_c(filepath.c_str(), originID, &cover);
-
-        SpiceInt nIntervals = 0;
-        if (!failed_c())
-            nIntervals = card_c(&cover) / 2;
-
-        if (nIntervals <= 0)
-        {
-            clog << "Couldn't find object " << originName << " in SPICE kernel " << kernelFile << "\n";
-            spiceErr = true;
-            if (failed_c())
-            {
-                reset_c();
-            }
-            return false;
-        }
-
-        wnfetd_c(&cover, 0, &originBeginning, &originEnding);
-    }
-
-    SpiceDouble beginning = max(targetBeginning, originBeginning);
-    SpiceDouble ending    = min(targetEnding,    originEnding);
-    if (beginning >= ending)
-    {
-        clog << "No overlapping coverage for SPICE target and origin objects\n";
+		// First check the coverage window of the target. No interval
+		// is required for ID 0 (the solar system barycenter) which is
+		// always at (0, 0, 0).
+		if (targetID != 0)
+		{
+            clog << filename << ", " << targetID << endl;
+			spkcov_c(filename, targetID, &targetCoverage);
+		}
+	}
+   
+	SpiceInt nIntervals = card_c(&targetCoverage) / 2;
+	if (nIntervals <= 0)
+	{
+        clog << "Couldn't find object " << targetBodyName << " in SPICE kernel pool.\n";
         spiceErr = true;
-        return false;
-    }
-#else
-    double beginning = astro::daysToSecs(validIntervalBegin - astro::J2000) + 0.001;
-#endif
+	    if (failed_c())
+		{
+			reset_c();
+		}
+		return false;
+	}
 
-#if 0
-    // Add a very small offset to the beginning and ending of the interval
-    // so that roundoff error won't cause us to sample slightly outside the
-    // valid time interval.
-    validIntervalBegin = astro::secsToDays(beginning + 0.001) + astro::J2000;
-    validIntervalEnd = astro::secsToDays(ending - 0.001) + astro::J2000;
-#endif
+	// TODO: need to consider the origin object as well as the target
+	if (useDefaultTimeInterval)
+	{
+		// Set the valid time interval for this orbit to the first interval
+		// in the coverage window for the target.
+		SpiceDouble targetBeginning = -1.0e50;
+		SpiceDouble targetEnding    = +1.0e50;
+
+		if (targetID == 0)
+		{
+			// Time range for solar system barycenter is infinite
+			validIntervalBegin = targetBeginning;
+			validIntervalEnd = targetEnding;
+		}
+		else
+		{
+			wnfetd_c(&targetCoverage, 0, &targetBeginning, &targetEnding);
+
+			// SPICE times are seconds since J2000.0
+			validIntervalBegin = astro::secsToDays(targetBeginning) + astro::J2000;
+			validIntervalEnd = astro::secsToDays(targetEnding) + astro::J2000;
+
+            // Reduce interval by a millisecond at each end; otherwise, rounding error
+            // can cause us to get SPICE errors when computing states right at the edge
+            // of the valid window.
+            validIntervalBegin += MILLISEC;
+            validIntervalEnd -= MILLISEC;
+		}
+	}
+	else
+	{
+        // Reduce valid interval by a millisecond at each end.
+        validIntervalBegin += MILLISEC;
+        validIntervalEnd -= MILLISEC;
+
+		SpiceDouble beginningSecondsJ2000 = astro::daysToSecs(validIntervalBegin - astro::J2000);
+		SpiceDouble endingSecondsJ2000    = astro::daysToSecs(validIntervalEnd - astro::J2000);
+
+		// A time interval was specified--make sure that it's covered in the SPICE kernel.
+		if (targetID != 0 &&
+			!wnincd_c(beginningSecondsJ2000, endingSecondsJ2000, &targetCoverage))
+		{
+			clog << "Specified time interval for target " << targetBodyName << " not available.\n";
+			return false;
+		}
+	}
 
     // Test getting the position of the object to make sure that there's
-    // adequate data in the kernel.
+    // adequate data in the kernel to compute the position of the target
+    // relative to the origin. Even if both objects are present and have
+    // adequate coverage, it's possible that there might be a missing frame
+    // definition or itermediate object.
+    double beginning = astro::daysToSecs(validIntervalBegin - astro::J2000);
     double position[3];
-    double lt;
+    double lt = 0.0;
     spkgps_c(targetID, beginning, "eclipj2000", originID,
              position, &lt);
     if (failed_c())
@@ -263,35 +289,4 @@ void SpiceOrbit::getValidRange(double& begin, double& end) const
 {
     begin = validIntervalBegin;
     end = validIntervalEnd;
-}
-
-
-bool getNAIFID(const string& name, int* id)
-{
-    SpiceInt spiceID = 0;
-    SpiceBoolean found = SPICEFALSE;
-
-    // Don't call bodn2c on an empty string because SPICE generates
-    // an error if we do.
-    if (!name.empty())
-    {
-        bodn2c_c(name.c_str(), &spiceID, &found);
-        if (found)
-        {
-            *id = (int) spiceID;
-        }
-        else   // Is it a numeric ID?
-        {
-            // SpiceInt maps to an int on those architectures where sizeof(long) != sizeof(double)/2.
-            // Otherwise it maps to a long. This avoids GCC complaints about type mismatches:
-            long spiceIDlng;
-            if (sscanf(name.c_str(), " %ld", &spiceIDlng) == 1)
-            {
-                *id = (int) spiceIDlng;
-                found = SPICETRUE;
-            }
-        }
-    }
-
-    return found == SPICETRUE;
 }
