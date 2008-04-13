93660632d8ca49b94b4c454de099e8a5805c2d27
Chris Laurel <claurel@gmail.com>
2008-04-13 20:19:23 +0000

    Added support for star InfoURLs to Win32 front-end.

--- src/celestia/winmain.cpp
+++ src/celestia/winmain.cpp
@@ -1735,10 +1735,13 @@ void ShowWWWInfo(const Selection& sel)
 
     case Selection::Type_Star:
         {
-            char name[32];
-            sprintf(name, "HIP%d", sel.star()->getCatalogNumber() & ~0xf0000000);
-
-            url = string("http://simbad.u-strasbg.fr/sim-id.pl?protocol=html&Ident=") + name;
+            url = sel.star()->getInfoURL();
+            if (url.empty())
+            {
+                char name[32];
+                sprintf(name, "HIP%d", sel.star()->getCatalogNumber() & ~0xf0000000);
+                url = string("http://simbad.u-strasbg.fr/sim-id.pl?protocol=html&Ident=") + name;
+            }
         }
         break;
 
