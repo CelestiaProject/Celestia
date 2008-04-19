863a9b9cea15673091b27c2031b65763f4e2efcc
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2008-04-19 03:22:12 +0000

    Added XBEL support to bookmark manager for Qt4 front-end (bookmark manager still disabled.)

---
+++ src/celestia/qt/xbel.h
@@ -0,0 +1,41 @@
+// xbel.h
+//
+// Copyright (C) 2008, the Celestia Development Team
+// Original version by Chris Laurel <claurel@gmail.com>
+//
+// XBEL bookmarks reader and writer.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#ifndef _CELESTIA_XBEL_H_
+#define _CELESTIA_XBEL_H_
+
+#include <QXmlStreamReader>
+
+class BookmarkItem;
+
+
+class XbelReader : public QXmlStreamReader
+{
+public:
+    XbelReader(QIODevice*);
+
+    BookmarkItem* read();
+
+private:
+     void readUnknownElement();
+     void readXbel(BookmarkItem* root);
+     void readTitle(BookmarkItem *item);
+     void readDescription(BookmarkItem *item);
+     void readSeparator(BookmarkItem *item);
+     void readFolder(BookmarkItem *item);
+     void readBookmark(BookmarkItem *item);
+
+     void skipUnknownElement();
+};
+
+
+#endif // _CELESTIA_XBEL_H_
