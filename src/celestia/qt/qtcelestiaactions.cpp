915ade7cc0ae7568bb98b388be42287b4382c934
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2008-05-01 18:47:51 +0000

    Qt4 front-end improvements: - Added light time delay to Time menu - Created Display menu, with actions to toggle various rendering features and control star visibility (limiting magnitude and automag) - Added all new actions to CelestiaActions pool; these can be used in the toolbar, menu, or (with slightly more effort) in a preferences dialog.

--- src/celestia/qt/qtcelestiaactions.cpp
+++ src/celestia/qt/qtcelestiaactions.cpp
@@ -125,23 +125,86 @@ CelestiaActions::CelestiaActions(QObject* parent,
     labelLocationsAction   = createCheckableAction(tr("Locations"), labelsMenu, Renderer::LocationLabels);
     labelConstellationsAction = createCheckableAction(tr("Constellations"), labelsMenu, Renderer::ConstellationLabels);
 
-    connect(labelGalaxiesAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelNebulaeAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelOpenClustersAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelStarsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelPlanetsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelDwarfPlanetsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelMoonsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelMinorMoonsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelAsteroidsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelCometsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelSpacecraftAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
-    connect(labelLocationsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
+    connect(labelGalaxiesAction, SIGNAL(triggered()),       this, SLOT(slotToggleLabel()));
+    connect(labelNebulaeAction, SIGNAL(triggered()),        this, SLOT(slotToggleLabel()));
+    connect(labelOpenClustersAction, SIGNAL(triggered()),   this, SLOT(slotToggleLabel()));
+    connect(labelStarsAction, SIGNAL(triggered()),          this, SLOT(slotToggleLabel()));
+    connect(labelPlanetsAction, SIGNAL(triggered()),        this, SLOT(slotToggleLabel()));
+    connect(labelDwarfPlanetsAction, SIGNAL(triggered()),   this, SLOT(slotToggleLabel()));
+    connect(labelMoonsAction, SIGNAL(triggered()),          this, SLOT(slotToggleLabel()));
+    connect(labelMinorMoonsAction, SIGNAL(triggered()),     this, SLOT(slotToggleLabel()));
+    connect(labelAsteroidsAction, SIGNAL(triggered()),      this, SLOT(slotToggleLabel()));
+    connect(labelCometsAction, SIGNAL(triggered()),         this, SLOT(slotToggleLabel()));
+    connect(labelSpacecraftAction, SIGNAL(triggered()),     this, SLOT(slotToggleLabel()));
+    connect(labelLocationsAction, SIGNAL(triggered()),      this, SLOT(slotToggleLabel()));
     connect(labelConstellationsAction, SIGNAL(triggered()), this, SLOT(slotToggleLabel()));
 
     labelsAction->setMenu(labelsMenu);
 
+    galaxiesAction     = createCheckableAction(tr("Galaxies"),          Renderer::ShowGalaxies);
+    galaxiesAction->setShortcut(QString("U"));
+    nebulaeAction      = createCheckableAction(tr("Nebulae"),           Renderer::ShowNebulae);
+    nebulaeAction->setShortcut(QString("^"));
+    openClustersAction = createCheckableAction(tr("Open Clusters"),     Renderer::ShowOpenClusters);
+    connect(galaxiesAction,        SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(nebulaeAction,         SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(openClustersAction,    SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+
+    cloudsAction          = createCheckableAction(tr("Clouds"),            Renderer::ShowCloudMaps);
+    cloudsAction->setShortcut(QString("I"));
+    nightSideLightsAction = createCheckableAction(tr("Night Side Lights"), Renderer::ShowNightMaps);
+    nightSideLightsAction->setShortcut(QString("Ctrl+L"));
+    cometTailsAction      = createCheckableAction(tr("Comet Tails"),       Renderer::ShowCometTails);
+    atmospheresAction     = createCheckableAction(tr("Atmospheres"),       Renderer::ShowAtmospheres);
+    atmospheresAction->setShortcut(QString("Ctrl+A"));
+    connect(cloudsAction,          SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(nightSideLightsAction, SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(cometTailsAction,      SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(atmospheresAction,     SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+
+    ringShadowsAction     = createCheckableAction(tr("Ring Shadows"),      Renderer::ShowRingShadows);
+    eclipseShadowsAction  = createCheckableAction(tr("Eclipse Shadows"),   Renderer::ShowEclipseShadows);
+    eclipseShadowsAction->setShortcut(QKeySequence("Ctrl+E"));
+    cloudShadowsAction    = createCheckableAction(tr("Cloud Shadows"),     Renderer::ShowCloudShadows);
+    connect(ringShadowsAction,    SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(eclipseShadowsAction, SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+    connect(cloudShadowsAction,   SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+
+    lowResAction          = createCheckableAction(tr("Low"),    lores);
+    mediumResAction       = createCheckableAction(tr("Medium"), lores);
+    highResAction         = createCheckableAction(tr("High"),   lores);
+    connect(lowResAction,    SIGNAL(triggered()), this, SLOT(slotSetTextureResolution()));
+    connect(mediumResAction, SIGNAL(triggered()), this, SLOT(slotSetTextureResolution()));
+    connect(highResAction,   SIGNAL(triggered()), this, SLOT(slotSetTextureResolution()));
+
+    autoMagAction        = createCheckableAction(tr("Auto Magnitude"), Renderer::ShowAutoMag);
+    autoMagAction->setShortcut(QKeySequence("Ctrl+Y"));
+    autoMagAction->setToolTip(tr("Faintest visible magnitude based on field of view"));
+    connect(autoMagAction,        SIGNAL(triggered()), this, SLOT(slotToggleRenderFlag()));
+
+    increaseLimitingMagAction = new QAction(tr("More Stars Visible"), this);
+    increaseLimitingMagAction->setData(0.1);
+    increaseLimitingMagAction->setShortcut(QString("]"));
+    decreaseLimitingMagAction = new QAction(tr("Fewer Stars Visible"), this);
+    decreaseLimitingMagAction->setData(-0.1);
+    decreaseLimitingMagAction->setShortcut(QString("["));
+    connect(increaseLimitingMagAction, SIGNAL(triggered()), this, SLOT(slotAdjustLimitingMagnitude()));
+    connect(decreaseLimitingMagAction, SIGNAL(triggered()), this, SLOT(slotAdjustLimitingMagnitude()));
+
+    pointStarAction      = createCheckableAction(tr("Points"),         Renderer::PointStars);
+    fuzzyPointStarAction = createCheckableAction(tr("Fuzzy Points"),   Renderer::FuzzyPointStars);
+    scaledDiscStarAction = createCheckableAction(tr("Scaled Discs"),   Renderer::ScaledDiscStars);
+    connect(pointStarAction,      SIGNAL(triggered()), this, SLOT(slotSetStarStyle()));
+    connect(fuzzyPointStarAction, SIGNAL(triggered()), this, SLOT(slotSetStarStyle()));
+    connect(scaledDiscStarAction, SIGNAL(triggered()), this, SLOT(slotSetStarStyle()));
+
+    lightTimeDelayAction = new QAction(tr("Light Time Delay"), this);
+    lightTimeDelayAction->setCheckable(true);
+    lightTimeDelayAction->setToolTip("Subtract one-way light travel time to selected object");
+    connect(lightTimeDelayAction, SIGNAL(triggered()), this, SLOT(slotSetLightTimeDelay()));
+    
     syncWithRenderer(appCore->getRenderer());
+    syncWithAppCore();
     appCore->getRenderer()->addWatcher(this);
 }
 
@@ -157,6 +220,8 @@ void CelestiaActions::syncWithRenderer(const Renderer* renderer)
     int renderFlags = renderer->getRenderFlags();
     int labelMode = renderer->getLabelMode();
     int orbitMask = renderer->getOrbitMask();
+    int textureRes = renderer->getResolution();
+    Renderer::StarStyle starStyle = renderer->getStarStyle();
 
     eqGridAction->setChecked(renderFlags & Renderer::ShowCelestialSphere);
     markersAction->setChecked(renderFlags & Renderer::ShowMarkers);
@@ -186,6 +251,41 @@ void CelestiaActions::syncWithRenderer(const Renderer* renderer)
     asteroidOrbitsAction->setChecked(orbitMask & Body::Asteroid);
     cometOrbitsAction->setChecked(orbitMask & Body::Comet);
     spacecraftOrbitsAction->setChecked(orbitMask & Body::Spacecraft);
+
+    // Texture resolution
+    lowResAction->setChecked(textureRes == lores);
+    mediumResAction->setChecked(textureRes == medres);
+    highResAction->setChecked(textureRes == hires);
+
+    // Star style
+    pointStarAction->setChecked(starStyle == Renderer::PointStars);
+    fuzzyPointStarAction->setChecked(starStyle == Renderer::FuzzyPointStars);
+    scaledDiscStarAction->setChecked(starStyle == Renderer::ScaledDiscStars);
+
+    // Features
+    cloudsAction->setChecked(renderFlags & Renderer::ShowCloudMaps);
+    cometTailsAction->setChecked(renderFlags & Renderer::ShowCometTails);
+    atmospheresAction->setChecked(renderFlags & Renderer::ShowAtmospheres);
+    nightSideLightsAction->setChecked(renderFlags & Renderer::ShowNightMaps);
+
+    // Deep sky object visibility
+    galaxiesAction->setChecked(renderFlags & Renderer::ShowGalaxies);
+    nebulaeAction->setChecked(renderFlags & Renderer::ShowNebulae);
+    openClustersAction->setChecked(renderFlags & Renderer::ShowOpenClusters);
+
+    // Shadows
+    ringShadowsAction->setChecked(renderFlags & Renderer::ShowRingShadows);
+    eclipseShadowsAction->setChecked(renderFlags & Renderer::ShowEclipseShadows);
+    cloudShadowsAction->setChecked(renderFlags & Renderer::ShowCloudShadows);
+
+    // Star visibility
+    autoMagAction->setChecked(renderFlags & Renderer::ShowAutoMag);
+}
+
+
+void CelestiaActions::syncWithAppCore()
+{
+    lightTimeDelayAction->setChecked(appCore->getLightDelayActive());
 }
 
 
@@ -228,6 +328,66 @@ void CelestiaActions::slotToggleOrbit()
 }
 
 
+void CelestiaActions::slotSetStarStyle()
+{
+    QAction* act = qobject_cast<QAction*>(sender());
+    if (act != NULL)
+    {
+        Renderer::StarStyle starStyle = (Renderer::StarStyle) act->data().toInt();
+        appCore->getRenderer()->setStarStyle(starStyle);
+    }
+}
+
+
+void CelestiaActions::slotSetTextureResolution()
+{
+    QAction* act = qobject_cast<QAction*>(sender());
+    if (act != NULL)
+    {
+        int textureResolution = act->data().toInt();
+        appCore->getRenderer()->setResolution(textureResolution);
+    }
+}
+
+
+void CelestiaActions::slotAdjustLimitingMagnitude()
+{
+    QAction* act = qobject_cast<QAction*>(sender());
+    if (act != NULL)
+    {
+        Renderer* renderer = appCore->getRenderer();
+        float change = (float) act->data().toDouble();
+
+        QString notification;
+        if (renderer->getRenderFlags() & Renderer::ShowAutoMag)
+        {
+            float newLimitingMag = qBound(6.0f, renderer->getFaintestAM45deg() + change, 12.0f);
+            renderer->setFaintestAM45deg(newLimitingMag);
+            appCore->setFaintestAutoMag();
+
+            notification = tr("Auto magnitude limit at 45 degrees: %L1").arg(newLimitingMag, 0, 'f', 2);
+        }
+        else
+        {
+            float newLimitingMag = qBound(1.0f, appCore->getSimulation()->getFaintestVisible() + change * 2, 15.0f);
+            appCore->setFaintest(newLimitingMag);
+
+            notification = tr("Magnitude limit: %L1").arg(newLimitingMag, 0, 'f', 2);
+        }
+
+        appCore->flash(notification.toUtf8().data());
+    }
+}
+
+
+void CelestiaActions::slotSetLightTimeDelay()
+{
+    // TODO: CelestiaCore class should offer an API for enabling/disabling light
+    // time delay.
+    appCore->charEntered('-');
+}
+
+
 // Convenience method to create a checkable action for a menu and set the data
 // to the specified integer value.
 QAction* CelestiaActions::createCheckableAction(const QString& text, QMenu* menu, int data)
@@ -236,6 +396,18 @@ QAction* CelestiaActions::createCheckableAction(const QString& text, QMenu* menu
     act->setCheckable(true);
     act->setData(data);
     menu->addAction(act);
+
+    return act;
+}
+
+
+// Convenience method to create a checkable action and set the data
+// to the specified integer value.
+QAction* CelestiaActions::createCheckableAction(const QString& text, int data)
+{
+    QAction* act = new QAction(text, this);
+    act->setCheckable(true);
+    act->setData(data);
 
     return act;
 }
