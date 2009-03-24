05e2f74b45f35b1f17cdd4116a6e9abd94348fed
cjlaurel <cjlaurel@10d76133-bd44-0410-b00f-95d021917061>
2009-03-24 20:24:16 +0000

    Added GL_MULTISAMPLE to list of extensions in glext.h.

--- src/celengine/glext.h
+++ src/celengine/glext.h
@@ -40,6 +40,19 @@ typedef unsigned int GLhandleARB;	/* shader object handle */
 #define GL_BGRA_EXT                         0x80E1
 #endif
 
+/* ARB_multisample defines from <GL/gl.h> */
+#ifndef GL_ARB_multisample
+#define GL_MULTISAMPLE_ARB                0x809D
+#define GL_SAMPLE_ALPHA_TO_COVERAGE_ARB   0x809E
+#define GL_SAMPLE_ALPHA_TO_ONE_ARB        0x809F
+#define GL_SAMPLE_COVERAGE_ARB            0x80A0
+#define GL_SAMPLE_BUFFERS_ARB             0x80A8
+#define GL_SAMPLES_ARB                    0x80A9
+#define GL_SAMPLE_COVERAGE_VALUE_ARB      0x80AA
+#define GL_SAMPLE_COVERAGE_INVERT_ARB     0x80AB
+#define GL_MULTISAMPLE_BIT_ARB            0x20000000
+#endif
+
 /* GL_ARB_texture_compression */
 #ifndef GL_ARB_texture_compression
 #define GL_ARB_texture_compression 1
