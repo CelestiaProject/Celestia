d256d8c7444d7e36490b0e91262398d16541e569
Chris Laurel <claurel@gmail.com>
2009-04-09 21:34:49 +0000

    Added ifdefs so that shaders.log is only written by debug builds.

--- src/celengine/shadermanager.cpp
+++ src/celengine/shadermanager.cpp
@@ -184,11 +184,16 @@ bool operator<(const ShaderProperties& p0, const ShaderProperties& p1)
 
 ShaderManager::ShaderManager()
 {
+#if defined(_DEBUG) || defined(DEBUG)
+    // Only write to shader log file if this is a debug build
+
     if (g_shaderLogFile == NULL)
 #ifdef _WIN32
         g_shaderLogFile = new ofstream("shaders.log");
 #else
         g_shaderLogFile = new ofstream("/tmp/celestia-shaders.log");
+#endif
+
 #endif
 }
 
