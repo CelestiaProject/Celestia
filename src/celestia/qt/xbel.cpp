051f7034ee424b50ebd1ae623e384c19387d2871
Vincent Giangiulio <vince.gian@free.fr>
2010-04-18 14:13:42 +0000

    Fixed compilation error: added util.h include to identify the gettext macro

--- src/celestia/qt/xbel.cpp
+++ src/celestia/qt/xbel.cpp
@@ -10,6 +10,7 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
+#include "celutil/util.h"
 #include "xbel.h"
 #include "qtbookmark.h"
 #include <QBuffer>
