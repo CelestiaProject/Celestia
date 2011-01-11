2667961f7cc68c38e980e41900531411ae9ee198
suwalski <suwalski@10d76133-bd44-0410-b00f-95d021917061>
2011-01-11 22:57:52 +0000

    Add missing <cstdio> include.

--- src/celephem/scriptobject.cpp
+++ src/celephem/scriptobject.cpp
@@ -9,6 +9,8 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
+#include <cstdio>
+
 #include "scriptobject.h"
 
 using namespace std;
