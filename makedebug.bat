5ead3de04db4870c2f303aafa1d70acf973d6a17
ajtribick <ajtribick@10d76133-bd44-0410-b00f-95d021917061>
2012-03-22 20:09:01 +0000

    Clean-up: removed bat files for VC++6.

--- makedebug.bat
+++
@@ -1,5 +0,0 @@
-cd src
-nmake /f winbuild.mak CFG=Debug %1
-cd ..
-copy src\celestia\Debug\celestia.exe .
-
--- makerelease.bat
+++
@@ -1,5 +0,0 @@
-cd src
-nmake /f winbuild.mak CFG=Release %1
-cd ..
-copy src\celestia\Release\celestia.exe .
-
