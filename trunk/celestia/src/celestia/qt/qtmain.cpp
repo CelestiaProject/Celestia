9787f28f83ff5496e84a012c2710e2e3db468b6e
suwalski <suwalski@10d76133-bd44-0410-b00f-95d021917061>
2012-03-28 20:04:49 +0000

    Fix resource loading in symbol-optimized build (non-qmake).

--- trunk/celestia/src/celestia/qt/qtmain.cpp
+++ trunk/celestia/src/celestia/qt/qtmain.cpp
@@ -47,6 +47,8 @@ int main(int argc, char *argv[])
 {  
     QApplication app(argc, argv);
 
+    Q_INIT_RESOURCE(icons);
+
     QCoreApplication::setOrganizationName("Celestia Development Team");
     QCoreApplication::setApplicationName("Celestia");
 
