4a605aa898af9ff4176d8a2d14272ef6bb66e309
Hleb Valoshka <375gnu@gmail.com>
2018-12-01 13:53:06 +0300

    Update pot creation infrastructure

--- po/update_pot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=$(dirname $0)
-
-$dir/extractrc $(find src -name \*.ui -o -name \*.rc -o -name \*.kcfg) > testrc.cpp
-xgettext -ki18n -k_ -F -j --qt \
-         -d celestia --package-name=celestia --package-version=1.7.0 --msgid-bugs-address=chris@teyssier.org --copyright-holder="Chris Laurel" \
-         $(find . -name \*.c -o -name \*.cpp -o -name \*.cc -o -name \*.h -o -name \*.hpp -o -name \*.qml) -o po/celestia.pot
-rm testrc.cpp
