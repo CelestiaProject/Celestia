a6b50bb25ac3f647903ac5ba72fa9f038c7025bb
Hleb Valoshka <375gnu@gmail.com>
2019-01-12 21:57:18 +0300

    Binary XYZV format implementation (Closes: #168)

---
+++ src/celephem/xyzvbinary.h
@@ -0,0 +1,17 @@
+#pragma once
+
+struct XYZVBinaryHeader
+{
+    char magic[8];
+    uint16_t byteOrder;
+    uint16_t digits;
+    uint32_t reserved;
+    uint64_t count;
+};
+
+struct XYZVBinaryData
+{
+    double tdb;
+    double position[3];
+    double velocity[3];
+};
