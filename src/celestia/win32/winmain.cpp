96537b126ddb2425749f6029f723aa1b3eacbb3e
Hleb Valoshka <375gnu@gmail.com>
2019-08-13 11:57:19 +0300

    wip

--- src/celestia/win32/winmain.cpp
+++ src/celestia/win32/winmain.cpp
@@ -500,10 +500,10 @@ static bool ToggleMenuItem(HMENU menu, int id)
 
 bool LoadItemTextFromFile(HWND hWnd,
                           int item,
-                          char* filename)
+                          const fs::path& filename)
 {
     // ifstream textFile(filename, ios::in | ios::binary);
-    ifstream textFile(filename, ios::in);
+    ifstream textFile(filename.string(), ios::in);
     string s;
 
     if (!textFile.good())
@@ -586,7 +586,7 @@ BOOL APIENTRY ControlsHelpProc(HWND hDlg,
     switch (message)
     {
     case WM_INITDIALOG:
-        LoadItemTextFromFile(hDlg, IDC_TEXT_CONTROLSHELP, const_cast<char*>(LocaleFilename("controls.txt").c_str()));
+        LoadItemTextFromFile(hDlg, IDC_TEXT_CONTROLSHELP, LocaleFilename("controls.txt"));
         return(TRUE);
 
     case WM_COMMAND:
@@ -610,7 +610,7 @@ BOOL APIENTRY LicenseProc(HWND hDlg,
     switch (message)
     {
     case WM_INITDIALOG:
-        LoadItemTextFromFile(hDlg, IDC_LICENSE_TEXT, const_cast<char*>(LocaleFilename("COPYING").c_str()));
+        LoadItemTextFromFile(hDlg, IDC_LICENSE_TEXT, LocaleFilename("COPYING"));
         return(TRUE);
 
     case WM_COMMAND:
@@ -3098,7 +3098,7 @@ static bool runOnce = false;
 static string startURL;
 static string startDirectory;
 static string startScript;
-static vector<string> extrasDirectories;
+static vector<fs::path> extrasDirectories;
 static string configFileName;
 static bool useAlternateConfigFile = false;
 static bool skipSplashScreen = false;
