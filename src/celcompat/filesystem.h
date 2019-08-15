ed1c7df73d8f90b8b98591cd01ed733b330e2b3d
Hleb Valoshka <375gnu@gmail.com>
2019-08-15 22:14:34 +0300

    Add a minimal C++17-like filesystem library

---
+++ src/celcompat/filesystem.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <config.h>
+
+#if __cplusplus >= 201703L
+#include <filesystem>
+namespace fs = std::filesystem;
+#elif defined(HAVE_EXPERIMENTAL_FILESYSTEM)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
+#include "fs.h"
+namespace fs = celestia::filesystem;
+#endif
