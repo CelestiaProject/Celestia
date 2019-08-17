ca39d82b09bf343ca8e85f47ee1d94a4936da6bc
Hleb Valoshka <375gnu@gmail.com>
2019-08-17 09:56:21 +0300

    Reimplement DebugPrint using variadic templates (Closes: #367)

--- src/celutil/debug.cpp
+++ src/celutil/debug.cpp
@@ -7,44 +7,7 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
-#ifdef _MSC_VER
-#include <windows.h>
-#endif
-#include <cstdio>
-#include <cstdarg>
-#include <config.h>
-#include <fmt/printf.h>
-
-static int debugVerbosity = 0;
-
-#if defined(DEBUG) || defined(_DEBUG)
-void DebugPrint(int level, const char *format, ...)
-{
-    va_list args;
-    va_start(args, format);
-
-    if (level <= debugVerbosity)
-    {
-#ifdef _MSC_VER
-        if (IsDebuggerPresent())
-        {
-            char buf[1024];
-            fmt::vsprintf(buf, format, args);
-            OutputDebugStringA(buf);
-        }
-        else
-        {
-            fmt::vfprintf(stdout, format, args);
-        }
-#else
-        fmt::vfprintf(stderr, format, args);
-#endif
-    }
-
-    va_end(args);
-}
-#endif /* DEBUG */
-
+int debugVerbosity = 0;
 
 void SetDebugVerbosity(int dv)
 {
