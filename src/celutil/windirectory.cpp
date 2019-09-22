063f217b3a701aa5601264048ba54be139cba368
Hleb Valoshka <375gnu@gmail.com>
2019-09-22 23:26:22 +0300

    Add methods to save data in user's home dir

--- src/celutil/windirectory.cpp
+++ src/celutil/windirectory.cpp
@@ -9,7 +9,15 @@
 
 #include <iostream>
 #include <windows.h>
+#include <shlobj.h>
+#ifdef _MSC_VER
+#include <direct.h>
+#else
+#include <sys/stat.h>
+#endif
+#include <cstdlib>
 #include "directory.h"
+#include "util.h"
 
 using namespace std;
 
@@ -106,6 +114,76 @@ bool IsDirectory(const std::string& filename)
         return ((attr & FILE_ATTRIBUTE_DIRECTORY) != 0);
 }
 
-std::string WordExp(const std::string& filename) {
+
+static string ToWinSeparators(const string& filename)
+{
+    string::size_type pos = filename.find('/');
+    if (pos == string::npos)
+        return filename;
+
+    return filename.substr(0, pos) + '\\' + ToWinSeparators(filename.substr(pos + 1));
+}
+
+
+string WordExp(const string& filename)
+{
+    if (filename[0] == '~')
+    {
+        if (filename.size() == 1)
+            return HomeDir();
+        if (filename[1] == '\\' || filename[1] == '/')
+            return HomeDir() + '\\' + ToWinSeparators(filename.substr(2));
+    }
     return filename;
+}
+
+
+string HomeDir()
+{
+    char p[MAX_PATH + 1];
+    if (SUCCEEDED(SHGetFolderPathA(NULL, CSIDL_PROFILE, NULL, 0, &p[0])))
+        return p;
+
+    // fallback to environment variables
+    const char *s = getenv("USERPROFILE");
+    if (s != NULL)
+        return s;
+
+    const char *s1 = getenv("HOMEDRIVE");
+    const char *s2 = getenv("HOMEPATH");
+    if (s1 != NULL && s2 != NULL)
+    {
+        return string(s1) + '/' + s2;
+    }
+
+    // unlikely this is defined in woe but let's check
+    s = getenv("HOME");
+    if (s != NULL)
+        return s;
+
+    return "";
+}
+
+
+bool IsAbsolutePath(const std::string &p)
+{
+    return    p[0] == '/'
+           || p[0] == '\\'
+           || (((p[0] >= 'A' && p[0] <= 'Z') || (p[0] >= 'a' && p[0] <= 'z')) && p[1] == ':');
+}
+
+string WriteableDataPath()
+{
+    const char *p = getenv("APPDATA");
+    p = p != NULL ? p : "~\\AppData\\Roaming";
+    return WordExp(p) + "\\Celestia";
+}
+
+bool MkDir(const std::string &p)
+{
+#ifdef _MSC_VER
+    return _mkdir(p.c_str()) == 0;
+#else
+    return mkdir(p.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == 0;
+#endif
 }
