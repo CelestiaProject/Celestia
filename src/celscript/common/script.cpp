d93a1cdf032e08d3e006af9c6acfe4c2a8084093
Hleb Valoshka <375gnu@gmail.com>
2019-10-17 23:51:26 +0300

    Common new script interface

---
+++ src/celscript/common/script.cpp
@@ -0,0 +1,38 @@
+// script.cpp
+//
+// Copyright (C) 2019, the Celestia Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#include "script.h"
+
+namespace celestia
+{
+namespace scripts
+{
+
+bool IScript::handleMouseButtonEvent(float x, float y, int button, bool down)
+{
+    return false;
+}
+
+bool IScript::charEntered(const char*)
+{
+    return false;
+}
+
+bool IScript::handleKeyEvent(const char* key)
+{
+    return false;
+}
+
+bool IScript::handleTickEvent(double dt)
+{
+    return false;
+}
+
+}
+}
