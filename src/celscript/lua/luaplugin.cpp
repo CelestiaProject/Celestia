414d7824da10989985f60688b089d696487b537b
Hleb Valoshka <375gnu@gmail.com>
2019-10-24 17:57:02 +0300

    [skip ci] wip

---
+++ src/celscript/lua/luaplugin.cpp
@@ -0,0 +1,44 @@
+// luaplugin.cpp
+//
+// Copyright (C) 2019, the Celestia Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#include <string>
+#include "luascript.h"
+
+class CelestiaCore;
+class CelestiaConfig;
+class ProgressNotifier;
+class Hash;
+class CachingOrbit;
+class RotationModel;
+
+CELESTIA_PLUGIN_ENTRYPOINT()
+{
+    static celestia::plugin::PluginInfo pinf(celestia::plugin::Scripting, "LUA");
+    return &pinf;
+}
+
+CELESTIA_PLUGIN_EXPORTABLE bool CreateScriptEnvironment(CelestiaCore *appCore, const CelestiaConfig *config, ProgressNotifier *progressNotifier)
+{
+    return celestia::scripts::CreateLuaEnvironment(appCore, config, progressNotifier);
+}
+
+CELESTIA_PLUGIN_EXPORTABLE LuaScript* CreateScript(CelestiaCore *appCore)
+{
+    return 
+}
+
+ScriptedModel* CreateScriptedRotation(const std::string& moduleName, const std::string& funcName, Hash* parameters)
+{
+    return nullptr;
+}
+
+ScriptedOrbit* CreateScriptedOrbit(const std::string& moduleName, const std::string& funcName, Hash* parameters)
+{
+    return nullptr;
+}
