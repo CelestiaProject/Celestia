28451c94160bf66c0c25d196c2837f4402ffa17d
Hleb Valoshka <375gnu@gmail.com>
2019-10-29 15:57:58 +0300

    test osx

--- src/celcompat/filesystem.h
+++ src/celcompat/filesystem.h
@@ -2,10 +2,10 @@
 
 #include <config.h>
 
-#if __cplusplus >= 201703L
-#include <filesystem>
-namespace fs = std::filesystem;
-#elif defined(HAVE_EXPERIMENTAL_FILESYSTEM)
+//#if __cplusplus >= 201703L
+//#include <filesystem>
+//namespace fs = std::filesystem;
+#if defined(HAVE_EXPERIMENTAL_FILESYSTEM)
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 #else
