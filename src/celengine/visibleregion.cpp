055ea6626485f9360613309a8f87a19de49fd39b
Hleb Valoshka <375gnu@gmail.com>
2019-12-12 22:54:23 +0300

    Fix VBO data type
    GL_DYNAMIC_DRAW == update occasionally
    GL_STREAM_DRAW == update on every draw

--- src/celengine/visibleregion.cpp
+++ src/celengine/visibleregion.cpp
@@ -92,7 +92,7 @@ renderTerminator(Renderer* renderer, const vector<Vector3f>& pos, const Vector4f
      * Because of this we make calculations on a CPU and stream results to GPU.
      */
 
-    static celgl::VertexObject vo{ GL_ARRAY_BUFFER, maxSections * sizeof(Vector3f), GL_DYNAMIC_DRAW };
+    static celgl::VertexObject vo{ GL_ARRAY_BUFFER, maxSections * sizeof(Vector3f), GL_STREAM_DRAW };
 
     auto *prog = renderer->getShaderManager().getShader("uniform_color");
     if (prog == nullptr)
