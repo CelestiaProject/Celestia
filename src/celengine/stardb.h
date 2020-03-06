e8d0e4d1d7284977aec258170eca81d47764a2e2
pirogronian <pirogronian0@gmail.com>
2020-03-06 19:57:51 +0100

    Move blockarray to celutil

--- src/celengine/stardb.h
+++ src/celengine/stardb.h
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <celutil/blockarray.h>
 #include <celengine/constellation.h>
 #include <celengine/starname.h>
 #include <celengine/star.h>
@@ -22,82 +23,6 @@
 
 
 static const unsigned int MAX_STAR_NAMES = 10;
-
-// TODO: Move BlockArray to celutil; consider making it a full STL
-// style container with iterator support.
-
-/*! BlockArray is a container class that is similar to an STL vector
- *  except for two very important differences:
- *  - The elements of a BlockArray are not necessarily in one
- *    contiguous block of memory.
- *  - The address of a BlockArray element is guaranteed not to
- *    change over the lifetime of the BlockArray (or until the
- *    BlockArray is cleared.)
- */
-template<class T> class BlockArray
-{
-public:
-    BlockArray() :
-        m_blockSize(1000),
-        m_elementCount(0)
-    {
-    }
-
-    ~BlockArray()
-    {
-        clear();
-    }
-
-    unsigned int size() const
-    {
-        return m_elementCount;
-    }
-
-    /*! Append an item to the BlockArray. */
-    void add(T& element)
-    {
-        unsigned int blockIndex = m_elementCount / m_blockSize;
-        if (blockIndex == m_blocks.size())
-        {
-            T* newBlock = new T[m_blockSize];
-            m_blocks.push_back(newBlock);
-        }
-
-        unsigned int elementIndex = m_elementCount % m_blockSize;
-        m_blocks.back()[elementIndex] = element;
-
-        ++m_elementCount;
-    }
-
-    void clear()
-    {
-        for (typename std::vector<T*>::const_iterator iter = m_blocks.begin(); iter != m_blocks.end(); ++iter)
-        {
-            delete[] *iter;
-        }
-        m_elementCount = 0;
-        m_blocks.clear();
-    }
-
-    T& operator[](int index)
-    {
-        unsigned int blockNumber = index / m_blockSize;
-        unsigned int elementNumber = index % m_blockSize;
-        return m_blocks[blockNumber][elementNumber];
-    }
-
-    const T& operator[](int index) const
-    {
-        unsigned int blockNumber = index / m_blockSize;
-        unsigned int elementNumber = index % m_blockSize;
-        return m_blocks[blockNumber][elementNumber];
-    }
-
-private:
-    unsigned int m_blockSize;
-    unsigned int m_elementCount;
-    std::vector<T*> m_blocks;
-};
 
 
 class StarDatabase
