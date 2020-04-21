794e833a42ac25787b2a9838a8249af76b60132d
Hleb Valoshka <375gnu@gmail.com>
2020-04-21 18:43:56 +0300

    Remove legacy build files

--- src/tools/cmod/cmod.pro
+++
@@ -1,7 +0,0 @@
-TEMPLATE = subdirs
-SUBDIRS = \
-    3dstocmod \
-    cmodfix \
-    cmodsphere \
-    cmodview \
-    itokawa
--- src/tools/spice2xyzv/build.bat
+++
@@ -1,3 +0,0 @@
-:: Build file for Windows systems
-
-cl /Ox /MD /EHsc spice2xyzv.cpp /I ../../../windows/inc/spice ../../../windows/lib/x86/cspice.lib
