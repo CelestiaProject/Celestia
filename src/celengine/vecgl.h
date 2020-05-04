e5e0f14905cd7087524717c97436eec267194de4
Hleb Valoshka <375gnu@gmail.com>
2020-05-04 11:51:41 +0300

    Remove unneeded fixed pipeline wrappers

--- src/celengine/vecgl.h
+++ src/celengine/vecgl.h
@@ -19,52 +19,6 @@
 #include <Eigen/Geometry>
 
 
-inline void glColor(const Color& c)
-{
-    glColor4f(c.red(), c.green(), c.blue(), c.alpha());
-}
-
-inline void glColor(const Color& c, float a)
-{
-    glColor4f(c.red(), c.green(), c.blue(), c.alpha() * a);
-}
-
-
-#if 0
-inline void glLightDirection(GLenum light, const Vec3f& dir)
-{
-    glLightfv(light, GL_POSITION, &(Vec4f(dir.x, dir.y, dir.z, 0.0f).x));
-}
-
-inline void glLightPosition(GLenum light, const Point3f& pos)
-{
-    glLightfv(light, GL_POSITION, &(Vec4f(pos.x, pos.y, pos.z, 1.0f).x));
-}
-
-inline void glLightColor(GLenum light, GLenum which, const Vec3f& color)
-{
-    glLightfv(light, which, &(Vec4f(color.x, color.y, color.z, 1.0f).x));
-}
-
-inline void glLightColor(GLenum light, GLenum which, const Vec4f& color)
-{
-    glLightfv(light, which, &color.x);
-}
-#endif
-
-inline void glLightColor(GLenum light, GLenum which, const Color& color)
-{
-    Eigen::Vector4f v(color.red(), color.green(), color.blue(), color.alpha());
-    glLightfv(light, which, v.data());
-}
-
-inline void glAmbientLightColor(const Color& color)
-{
-    Eigen::Vector4f v(color.red(), color.green(), color.blue(), color.alpha());
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, v.data());
-}
-
-
 /**** Eigen helpers for OpenGL ****/
 
 inline void glMatrix(const Eigen::Matrix4f& m)
@@ -127,27 +81,6 @@ inline void glVertex(const Eigen::Vector3d& v)
     glVertex3dv(v.data());
 }
 #endif
-
-inline void glLightDirection(GLenum light, const Eigen::Vector3f& dir)
-{
-    glLightfv(light, GL_POSITION, Eigen::Vector4f(dir.x(), dir.y(), dir.z(), 0.0f).data());
-}
-
-inline void glLightPosition(GLenum light, const Eigen::Vector3f& pos)
-{
-    glLightfv(light, GL_POSITION, Eigen::Vector4f(pos.x(), pos.y(), pos.z(), 1.0f).data());
-}
-
-template<typename DERIVED>
-void glLightColor(GLenum light, GLenum which, const Eigen::MatrixBase<DERIVED>& color)
-{
-    glLightfv(light, which, Eigen::Vector4f(color.x(), color.y(), color.z(), 1.0f).data());
-}
-
-inline void glLightColor(GLenum light, GLenum which, const Eigen::Vector4f& color)
-{
-    glLightfv(light, which, color.data());
-}
 
 inline void glVertexAttrib(GLuint index, const Color &color)
 {
