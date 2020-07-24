9c17d20b1ace7b9b7df648e9810afb70e183fd59
Hleb Valoshka <375gnu@gmail.com>
2020-07-24 13:00:59 +0300

    Replace fmt::fprintf with DPRINTF in albedo warning
    Will restore in 1.8

--- src/celengine/solarsys.cpp
+++ src/celengine/solarsys.cpp
@@ -809,7 +809,7 @@ static Body* CreateBody(const string& name,
     double t;
     if (planetData->getNumber("Albedo", t))
     {
-        fmt::fprintf(cerr, "Deprecated parameter Albedo used in %s definition.\nUse GeomAlbedo instead.\n", name);
+        DPRINTF("Deprecated parameter Albedo used in %s definition.\nUse GeomAlbedo & BondAlbedo instead.\n", name);
         body->setGeomAlbedo((float) t);
     }
 
