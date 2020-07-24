ff9984129ad02f43dca12ed507516731cb3f6b3f
Hleb Valoshka <375gnu@gmail.com>
2020-07-24 18:56:09 +0300

    Natural order sorting for typed text completions

---
+++ src/celutil/strnatcmp.h
@@ -0,0 +1,29 @@
+// This file is based on strnatcmp.h by Martin Pool
+// Copyright (C) 2000, 2004 by Martin Pool <mbp sourcefrog net>
+// Adaptations
+// Copyright (C) 2020, the Celestia Development Team
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+#pragma once
+
+#include <string>
+
+int strnatcmp(const std::string &a, const std::string &b);
+int strnatcmp(const std::wstring &wa, const std::wstring &wb);
+int strnatcasecmp(const std::string &a, const std::string &b);
+int strnatcasecmp(const std::wstring &wa, const std::wstring &wb);
