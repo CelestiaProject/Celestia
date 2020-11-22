a0186828eb6e4c48de18e6cb98cbd17814e007ef
Levin Li <lilinfeng303@outlook.com>
2020-11-22 11:57:10 +0800

    Fix largestar shader on some platforms

--- shaders/largestar_vert.glsl
+++ shaders/largestar_vert.glsl
@@ -11,6 +11,6 @@ void main(void)
 {
     texCoord = in_TexCoord0.st;
     set_vp(vec4(center, 1.0));
-    vec2 transformed = vec2((texCoord.x - 0.5) * pointWidth, (texCoord.y - 0.5) * pointHeight);
+    vec2 transformed = vec2((in_TexCoord0.x - 0.5) * pointWidth, (in_TexCoord0.y - 0.5) * pointHeight);
     gl_Position.xy += transformed * gl_Position.w;
 }
