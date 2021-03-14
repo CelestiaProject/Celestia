df192875ce84495cbe03e781787fdaa6ba05c559
Levin Li <lilinfeng303@outlook.com>
2021-03-14 22:17:43 +0800

    Add cctype include for toupper

--- src/celutil/stringutils.cpp
+++ src/celutil/stringutils.cpp
@@ -10,6 +10,7 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
+#include <cctype>
 #include "stringutils.h"
 
 using namespace std;
