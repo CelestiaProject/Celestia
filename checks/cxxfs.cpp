481f81c66403b17d7904403f48d82a01e87bc1bc
Hleb Valoshka <375gnu@gmail.com>
2021-10-20 18:44:02 +0400

    [cmake] Use FindFilesystem instead of own code

--- checks/cxxfs.cpp
+++
@@ -1,7 +0,0 @@
-#include <filesystem>
-
-int main()
-{
-    std::error_code ec;
-    throw std::filesystem::filesystem_error("test", ec);
-}
--- checks/cxxfsexp.cpp
+++
@@ -1,7 +0,0 @@
-#include <experimental/filesystem>
-
-int main()
-{
-    std::error_code ec;
-    throw std::experimental::filesystem::filesystem_error("test", ec);
-}
