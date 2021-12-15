ec0202007daace1da7637f1eb7d026516be5761a
Andrew Tribick <ajtribick@googlemail.com>
2021-12-15 15:07:12 +0100

    Use inline constexpr for constants in header files

--- src/celengine/deepskyobj.h
+++ src/celengine/deepskyobj.h
@@ -26,7 +26,7 @@ class Selection;
 class Renderer;
 struct Matrices;
 
-constexpr const float DSO_DEFAULT_ABS_MAGNITUDE = -1000.0f;
+constexpr inline float DSO_DEFAULT_ABS_MAGNITUDE = -1000.0f;
 
 class Nebula;
 class Galaxy;
--- src/celengine/dsodb.h
+++ src/celengine/dsodb.h
@@ -21,10 +21,10 @@
 #include <celengine/parser.h>
 
 
-constexpr const unsigned int MAX_DSO_NAMES = 10;
+constexpr inline unsigned int MAX_DSO_NAMES = 10;
 
 // 100 Gly - on the order of the current size of the universe
-constexpr const float DSO_OCTREE_ROOT_SIZE = 1.0e11f;
+constexpr inline float DSO_OCTREE_ROOT_SIZE = 1.0e11f;
 
 //NOTE: this one and starDatabase should be derived from a common base class since they share lots of code and functionality.
 class DSODatabase
