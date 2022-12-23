3554c00921e9f7c34d64baac4a6092feedf9792c
Hleb Valoshka <375gnu@gmail.com>
2022-12-23 23:57:51 +0200

    w

--- po/translate_resources.pl
+++ po/translate_resources.pl
@@ -3,17 +3,12 @@
 ################################################################
 #           Utility for i18n on Win32
 #
-#   Author: Christophe Teyssier <chris@teyssier.org>
-#   date: July 2006
+#   Copyright: 2006-2021, Celestia Development Team
+#   Original author: Christophe Teyssier <chris@teyssier.org>
+#   Original date: July 2006
 #
-# - Loads translations from the po files and produces translated .rc files in src/celestia/res
-# - Produces a dll with the translated resources for each po file in the locale/ dir
-# - Produces a list of unicode codepoints for each language in the current dir
-#   (to generate txf font textures)
-# - Compiles .po files and installs catalogs under locale/
-#
-# Requirements:
-# - rc.exe link.exe and msgfmt.exe must be in the PATH
+# - Loads translations from the po files and produces translated .rc
+#   files in src/celestia/win32/res
 ################################################################
 
 use Encode;
@@ -21,18 +16,14 @@ use File::Basename;
 use File::Path qw(make_path);
 use Cwd qw(getcwd);
 
-my $platform = $#ARGV == -1 ? 'win32' : $ARGV[0];
-my $machine = $platform == 'win32' ? 'X86' : $platform;
+my $lang = $ARGV[0];
+my $machine = $ARGV[1];
 
 my $po_dir = dirname $0;
 my $resource_file = "$po_dir/../src/celestia/win32/res/celestia.rc";
-my $rc_dir = "$po_dir/../src/celestia/win32/res";
-#my $build_dir = getcwd;
+my $rc_dir = dirname $resource_file;
+my $build_dir = getcwd;
 my $res_dir = "$po_dir/../build/src/celestia/win32/res";
-
-opendir(DIR, $po_dir);
-my @po_files = sort (grep( /\.po$/, readdir(DIR) ));
-closedir DIR;
 
 open RES, "< $resource_file";
 my $resource;
@@ -45,63 +36,118 @@ close RES;
 # LangID and code page
 # http://msdn.microsoft.com/en-us/library/dd318693
 my %lang = (
-ar => [ '401', 1256 ],
-be => [ '423', 1251 ],
-bg => [ '402', 1251 ],
-de => [ '407', 1252 ],
-el => [ '408', 1253 ],
-en => [ '409', 1252 ],
-es => [ '40a', 1252 ],
-fr => [ '40c', 1252 ],
-gl => [ '456', 1252 ],
-hu => [ '40e', 1250 ],
-it => [ '410', 1252 ],
-ja => [ '411',  932 ],
-ko => [ '412',  949 ],
-lt => [ '427', 1257 ],
-lv => [ '426', 1257 ],
-nb => [ '414', 1252 ],
-nl => [ '413', 1252 ],
-nn => [ '814', 1252 ],
-pl => [ '415', 1250 ],
-pt_BR => [ '416', 1252 ],
-pt => [ '816', 1252 ],
-ro => [ '418', 1250 ],
-ru => [ '419', 1251 ],
-sk => [ '41b', 1250 ],
-sv => [ '41d', 1252 ],
-tr => [ '41f', 1254 ],
-uk => [ '422', 1251 ],
-zh_CN => [ '804', 936 ],
-zh_TW => [ '404', 950 ],
+ar => [ '401', 1256, 'LANG_ARABIC, SUBLANG_DEFAULT'],
+be => [ '423', 1251, 'LANG_BELARUSIAN, SUBLANG_DEFAULT'],
+bg => [ '402', 1251, 'LANG_BULGARIAN, SUBLANG_DEFAULT'],
+de => [ '407', 1252, 'LANG_GERMAN, SUBLANG_DEFAULT'],
+el => [ '408', 1253, 'LANG_GREEK, SUBLANG_DEFAULT'],
+en => [ '409', 1252, 'LANG_ENGLISH, SUBLANG_DEFAULT'],
+es => [ '40a', 1252, 'LANG_SPANISH, SUBLANG_DEFAULT'],
+fr => [ '40c', 1252, 'LANG_FRENCH, SUBLANG_DEFAULT'],
+gl => [ '456', 1252, 'LANG_GALICIAN, SUBLANG_DEFAULT' ],
+hu => [ '40e', 1250, 'LANG_HUNGARIAN, SUBLANG_DEFAULT'],
+it => [ '410', 1252, 'LANG_ITALIAN, SUBLANG_DEFAULT'],
+ja => [ '411',  932, 'LANG_JAPANESE, SUBLANG_DEFAULT'],
+ko => [ '412',  949, 'LANG_KOREAN, SUBLANG_DEFAULT'],
+lt => [ '427', 1257, 'LANG_LITHUANIAN, SUBLANG_DEFAULT'],
+lv => [ '426', 1257, 'LANG_LATVIAN, SUBLANG_DEFAULT'],
+nb => [ '414', 1252, 'LANG_NORWEGIAN, SUBLANG_NORWEGIAN_BOKMAL'],
+nl => [ '413', 1252, 'LANG_DUTCH, SUBLANG_DEFAULT'],
+nn => [ '814', 1252, 'LANG_NORWEGIAN, SUBLANG_NORWEGIAN_NYNORSK'],
+pl => [ '415', 1250, 'LANG_POLISH, SUBLANG_DEFAULT'],
+pt_BR => [ '416', 1252, 'LANG_PORTUGUESE, SUBLANG_PORTUGUESE_BRAZILIAN'],
+pt => [ '816', 1252, 'LANG_PORTUGUESE, SUBLANG_PORTUGUESE_PORTUGAL'],
+ro => [ '418', 1250, 'LANG_ROMANIAN, SUBLANG_DEFAULT'],
+ru => [ '419', 1251, 'LANG_RUSSIAN, SUBLANG_DEFAULT'],
+sk => [ '41b', 1250, 'LANG_SLOVAK, SUBLANG_DEFAULT'],
+sv => [ '41d', 1252, 'LANG_SWEDISH, SUBLANG_DEFAULT'],
+tr => [ '41f', 1254, 'LANG_TURKISH, SUBLANG_DEFAULT'],
+uk => [ '422', 1251, 'LANG_UKRAINIAN, SUBLANG_DEFAULT'],
+zh_CN => [ '804', 936, 'LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED'],
+zh_TW => [ '404', 950, 'LANG_CHINESE, SUBLANG_CHINESE_TRADITIONAL'],
 );
 
-make_path($res_dir) if ! -d $res_dir;
+my @keywords = qw{
+    POPUP
+    MENUITEM
+    DEFPUSHBUTTON
+    LTEXT
+    CAPTION
+    PUSHBUTTON
+    RTEXT
+    CTEXT
+    LTEXT
+    GROUPBOX
+    CONTROL
+    GROUPBOX
+    AUTOCHECKBOX
+    AUTORADIOBUTTON
+};
 
-foreach my $po (@po_files) {
-    my $strings = load_po_strings("$po_dir/$po");
-    my $res = $resource;
-    my $lang = basename $po;
-    $lang =~ s/\..*//o;
+my $keys = join('|', @keywords);
 
-    while (my ($k, $v) = each %$strings) {
-        Encode::from_to($v, 'UTF-8', "CP$lang{$lang}[1]");
-        $res =~ s/"\Q$k\E"/"$v"/g;
+my $strings = load_po_strings("$po_dir/$lang.po");
+my @res = split(/\n|\r\n/, $resource);
+my $lang_id = $lang{$lang}[0];
+my $codepade = $lang{$lang}[1];
+my $lang_name =  $lang{$lang}[2];
+
+while (my ($k, $v) = each %$strings) {
+    Encode::from_to($v, 'UTF-8', "CP$codepade");
+    my $msgctxt;
+    my $msgid;
+    my $context = 0;
+
+    if ($k =~ /^(.+)\004(.+)$/) {
+        $msgctxt = $1;
+        $msgid = $2;
+        $context = 1;
     }
-
-    if ($lang{$lang}) {
-        $res=~ s/\Q#pragma code_page(1252)\E/#pragma code_page($lang{$lang}[1])/;
-        $res=~ s/VALUE "Translation", 0x409, 1252/VALUE "Translation", 0x$lang{$lang}[0], $lang{$lang}[1]/;
-        open OUT, "> celestia_$lang.rc";
-        print OUT $res;
-        close OUT;
-        system qq{echo rc /l $lang{$lang}[0] /d NDEBUG /fo $res_dir\\celestia_$lang.res /i $rc_dir celestia_$lang.rc};
-        system qq{echo link /nologo /noentry /dll /machine:$machine /out:$res_dir\\res_$lang.dll $res_dir\\celestia_$lang.res};
-
-        system qq{rc /l $lang{$lang}[0] /d NDEBUG /fo $res_dir\\celestia_$lang.res /i $rc_dir celestia_$lang.rc};
-        system qq{link /nologo /noentry /dll /machine:$machine /out:$res_dir\\res_$lang.dll $res_dir\\celestia_$lang.res};
+    foreach my $line (@res) {
+        if ($line =~ /^\s*(?:$keys)/) {
+            if ($context) {
+                $line =~ s/\bNC_\(\s*"\Q$msgctxt\E"\s*,\s*"\Q$msgid\E"\s*\)/"$v"/g;
+            } else {
+                $line =~ s/"\Q$k\E"/"$v"/g;
+            }
+        }
     }
 }
+
+#while (my ($k, $v) = each %$strings) {
+#    next if $k !~ /^(.+)\004(.+)$/;
+#    Encode::from_to($v, 'UTF-8', "CP$codepade");
+#    my $msgctxt = $1;
+#    my $msgid = $2;
+#
+#    foreach my $line (@res) {
+#        if ($line =~ /^\s*(?:$keys)/) {
+#            $line =~ s/\bNC_\(\s*"\Q$msgctxt\E"\s*,\s*"\Q$msgid\E"\s*\)/"$v"/g;
+#        }
+#    }
+#}
+#
+#while (my ($k, $v) = each %$strings) {
+#    next if $k =~ /^(.+)\004(.+)$/;
+#    Encode::from_to($v, 'UTF-8', "CP$codepade");
+#    foreach my $line (@res) {
+#        if ($line =~ /^\s*(?:$keys)/) {
+#            $line =~ s/"\Q$k\E"/"$v"/g;
+#        }
+#    }
+#}
+
+foreach my $line (@res) {
+    $line =~ s/LANGUAGE LANG_ENGLISH, SUBLANG_ENGLISH_US/\/\/LANGUAGE $lang_name/;
+    $line =~ s/\Q#pragma code_page(1252)\E/#pragma code_page($codepade)/;
+    $line =~ s/VALUE "Translation", 0x0?409, (?:1252|0x04B0)/VALUE "Translation", 0x$lang_id, $codepade/;
+}
+open OUT, "> celestia_$lang.rc";
+print OUT join("\r\n", @res);
+close OUT;
+
+system qq{rc /fo celestia_$lang.res /i $rc_dir celestia_$lang.rc};
+system qq{link /nologo /noentry /dll /machine:$machine /out:res_$lang.dll celestia_$lang.res};
 
 sub load_po_strings {
     my $po = shift;
@@ -111,18 +157,28 @@ sub load_po_strings {
     my $l1;
     my $l2;
     my $l3;
-    while ($l3 = <PO>) {
+    my $l4;
+    while ($l4 = <PO>) {
         # The po file is read by groups of three lines.
         # we can safely ignore multiline msgids since resource files
         # use only single line strings
-        if ($l2 =~ /^msgid\s+"(.*)"/) {
+        if ($l3 =~ /^msgid\s+"(.*)"/) {
             my $msgid = $1;
-            if ($l3 =~ /^msgstr\s+"(.+)"/ && $l1 !~ /fuzzy/ ) {
-                $strings{$msgid} = $1;
+            if ($l4 =~ /^msgstr\s+"(.+)"/) {
+                my $translation = $1;
+                if ($l2 =~ /^msgctxt\s+"(.+)"/) {
+                    my $context = $1;
+                    if ($l1 !~ /fuzzy/) {
+                        $strings{"$context\004$msgid"} = $translation;
+                    }
+                } elsif ($l2 !~ /fuzzy/) {
+                    $strings{$msgid} = $translation;
+                }
             }
         }
         $l1 = $l2;
         $l2 = $l3;
+        $l3 = $l4;
     }
     close PO;
     return \%strings;
