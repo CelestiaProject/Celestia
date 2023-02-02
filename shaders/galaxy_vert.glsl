15c4301b229c622917aa26edc3257202b867fac4
Hleb Valoshka <375gnu@gmail.com>
2023-02-02 23:13:44 +0200

    Reduce memory required for galaxy rendering
    GalaxyVertex: 24 -> 16
    Blob: 24 -> 16

--- shaders/galaxy_vert.glsl
+++ shaders/galaxy_vert.glsl
@@ -1,7 +1,5 @@
 attribute vec4 in_Position;
 attribute vec4 in_TexCoord0;
-//attribute float in_ColorIndex;
-//attribute float in_Alpha;
 
 uniform sampler2D colorTex;
 
@@ -10,14 +8,10 @@ varying vec2 texCoord;
 
 void main(void)
 {
-    // we pass color index as short int
-    // reusing gl_MultiTexCoord0.z
-    // we use 255 only because we have 256 color indices
-    float t = in_TexCoord0.z / 255.0; // [0, 255] -> [0, 1]
-    // we pass alpha values as as short int
-    // reusing gl_MultiTexCoord0.w
-    // we use 65535 for better precision
-    float a = in_TexCoord0.w / 65535.0; // [0, 65535] -> [0, 1]
+    // we pass color index as a normalized byte reusing in_TexCoord0.z
+    float t = in_TexCoord0.z;
+    // we pass alpha values as a normalized byte reusing in_TexCoord0.w
+    float a = in_TexCoord0.w;
     color = vec4(texture2D(colorTex, vec2(t, 0.0)).rgb, a);
     texCoord = in_TexCoord0.st;
     set_vp(in_Position);
