3b0512bda9b90427934a94567b687a7bb1527181
Hleb Valoshka <375gnu@gmail.com>
2023-02-19 18:55:11 +0200

    Fix and refactor globular rendering
     * fix sprites sizes
     * precompute relStarDensity
     * remove some global variables
     * Decrease GPU memory usage

--- shaders/tidal_vert.glsl
+++ shaders/tidal_vert.glsl
@@ -1,14 +1,20 @@
 attribute vec3 in_Position;
-attribute vec2 in_TexCoord0;
+attribute vec3 in_TexCoord0; // reuse [3] as colorIndex
 
+uniform sampler2D colorTex;
 uniform mat3 viewMat;
 uniform float tidalSize;
-varying vec2 texCoord;
+uniform float brightness;
+uniform float pixelWeight;
 
+varying vec2 texCoord;
+varying vec4 color;
 
 void main(void)
 {
     vec3 p = viewMat * in_Position.xyz * tidalSize;
     texCoord = in_TexCoord0.st;
+    float colorIndex = in_TexCoord0.p;
+    color = vec4(texture2D(colorTex, vec2(colorIndex, 0.0)).rgb, min(1.0, 2.0 * brightness * pixelWeight));
     set_vp(vec4(p, 1.0));
 }
