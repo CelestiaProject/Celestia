c8c0d14a959dcd4055b3098194a592eca08ea7dc
Levin Li <lilinfeng303@outlook.com>
2023-03-14 16:54:29 +0800

    Use icu for utf8 decoding in textlayout

---
+++ src/celutil/unicode.h
@@ -0,0 +1,19 @@
+// unicode.h
+//
+// Copyright (C) 2023-present, the Celestia Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+#pragma once
+
+#include <unicode/unistr.h>
+
+namespace celestia::util
+{
+
+bool UnicodeStringToWString(const icu::UnicodeString &input, std::wstring &output);
+
+}
