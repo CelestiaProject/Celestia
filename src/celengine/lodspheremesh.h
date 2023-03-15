9688c74bb9f51a682f1cf0c3a7d34d23cde99af0
Andrew Tribick <ajtribick@googlemail.com>
2023-03-15 23:40:59 +0100

    Updates to lodspheremesh
    - Ensure trigonometry arrays have exact values at multiples of 90°
    - Use symmetry of sin/cos functions when populating trig arrays
    - Use ArrayVector to handle texture arrays
    - Use glBufferSubData to transfer index data
    - Clean up buffers in LODSphereMesh destructor
    - Reduce MaxVertexSize since normals just re-use the position data

--- src/celengine/lodspheremesh.h
+++ src/celengine/lodspheremesh.h
@@ -10,20 +10,34 @@
 
 #pragma once
 
-#include <Eigen/Geometry>
-#include <celengine/texture.h>
+#include <array>
+#include <cstddef>
+#include <vector>
+
+#include <Eigen/Core>
+
 #include <celengine/glsupport.h>
-#include <celmath/frustum.h>
 
+class Texture;
 
-#define MAX_SPHERE_MESH_TEXTURES 6
-#define NUM_SPHERE_VERTEX_BUFFERS 2
+namespace celmath
+{
+class Frustum;
+}
 
 class LODSphereMesh
 {
 public:
-    LODSphereMesh();
+    static constexpr std::size_t MAX_SPHERE_MESH_TEXTURES = 6;
+    static constexpr std::size_t NUM_SPHERE_VERTEX_BUFFERS = 2;
+
+    LODSphereMesh() = default;
     ~LODSphereMesh();
+
+    LODSphereMesh(const LODSphereMesh&) = delete;
+    LODSphereMesh& operator=(const LODSphereMesh&) = delete;
+    LODSphereMesh(LODSphereMesh&&) = delete;
+    LODSphereMesh& operator=(LODSphereMesh&&) = delete;
 
     void render(unsigned int attributes, const celmath::Frustum&, float pixWidth,
                 Texture** tex, int nTextures);
@@ -53,8 +67,8 @@ public:
         int step;
         unsigned int attributes;  // vertex attributes
         const celmath::Frustum& frustum;   // frustum, for culling
-        Eigen::Vector3f fp[8];    // frustum points, for culling
-        int texLOD[MAX_SPHERE_MESH_TEXTURES];
+        std::array<Eigen::Vector3f, 8> fp{};    // frustum points, for culling
+        std::array<int, MAX_SPHERE_MESH_TEXTURES> texLOD{};
     };
 
     void renderPatches(int phi0, int theta0,
@@ -64,20 +78,17 @@ public:
 
     void renderSection(int phi0, int theta0, int extent, const RenderInfo&);
 
-    float* vertices{ nullptr };
-
-    int maxVertices{ 0 };
     int vertexSize{ 0 };
 
-    int nIndices{ 0 };
-    unsigned short* indices{ nullptr };
+    std::vector<float> vertices{};
+    std::vector<unsigned short> indices{};
 
     int nTexturesUsed{ 0 };
-    Texture* textures[MAX_SPHERE_MESH_TEXTURES]{};
-    unsigned int subtextures[MAX_SPHERE_MESH_TEXTURES]{};
+    std::array<Texture*, MAX_SPHERE_MESH_TEXTURES> textures{};
+    std::array<unsigned int, MAX_SPHERE_MESH_TEXTURES> subtextures{};
 
     bool vertexBuffersInitialized{ false };
     GLuint currentVB{ 0 };
-    GLuint vertexBuffers[NUM_SPHERE_VERTEX_BUFFERS];
+    std::array<GLuint, NUM_SPHERE_VERTEX_BUFFERS> vertexBuffers{};
     GLuint indexBuffer{ 0 };
 };
