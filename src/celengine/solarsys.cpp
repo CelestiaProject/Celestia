af8b33dd85ab42be65e9b964b7b7a3aeb33352a2
Hleb Valoshka <375gnu@gmail.com>
2023-11-19 13:05:07 +0200

    Disallow zero SemiAxes and Oblateness values

--- src/celengine/solarsys.cpp
+++ src/celengine/solarsys.cpp
@@ -828,6 +828,20 @@ Body* CreateBody(const std::string& name,
 
     bool semiAxesSpecified = false;
     auto semiAxes = planetData->getVector3<double>("SemiAxes");
+
+    if (semiAxes.has_value())
+    {
+        if ((*semiAxes).x() <= 0.0 || (*semiAxes).y() <= 0.0 || (*semiAxes).z() <= 0.0)
+        {
+            GetLogger()->error(_("Invalid SemiAxes value for object {}: [{}, {}, {}]\n"),
+                               name,
+                               (*semiAxes).x(),
+                               (*semiAxes).y(),
+                               (*semiAxes).z());
+            semiAxes.reset();
+        }
+    }
+
     if (radiusSpecified && semiAxes.has_value())
     {
         // If the radius has been specified, treat SemiAxes as dimensionless
@@ -847,7 +861,16 @@ Body* CreateBody(const std::string& name,
     {
         auto oblateness = planetData->getNumber<float>("Oblateness");
         if (oblateness.has_value())
-            body->setSemiAxes(body->getRadius() * Eigen::Vector3f(1.0f, 1.0f - *oblateness, 1.0f));
+        {
+            if (*oblateness >= 0.0f && *oblateness < 1.0f)
+            {
+                body->setSemiAxes(body->getRadius() * Eigen::Vector3f(1.0f, 1.0f - *oblateness, 1.0f));
+            }
+            else
+            {
+                GetLogger()->error(_("Invalid Oblateness value for object {}: {}\n"), name, *oblateness);
+            }
+        }
     }
 
     int classification = body->getClassification();
