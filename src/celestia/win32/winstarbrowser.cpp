34a1d2a92aff8cd678abaf0ce00cf2020d2975a5
Andrew Tribick <ajtribick@googlemail.com>
2023-12-09 09:03:19 +0100

    Consolidate star browser implementations across the front-ends

--- src/celestia/win32/winstarbrowser.cpp
+++ src/celestia/win32/winstarbrowser.cpp
@@ -17,6 +17,7 @@
 #include <windows.h>
 #include <commctrl.h>
 #include <cstring>
+#include <celestia/celestiacore.h>
 #include <celutil/gettext.h>
 #include <celutil/winutil.h>
 #include "winuiutils.h"
@@ -29,22 +30,9 @@ using namespace std;
 using namespace celestia::win32;
 
 namespace astro = celestia::astro;
+namespace engine = celestia::engine;
 namespace math = celestia::math;
 namespace util = celestia::util;
-
-static const int MinListStars = 10;
-static const int MaxListStars = 500;
-static const int DefaultListStars = 100;
-
-// TODO: More of the functions in this module should be converted to
-// methods of the StarBrowser class.
-
-enum {
-    BrightestStars = 0,
-    NearestStars = 1,
-    StarsWithPlanets = 2,
-};
-
 
 bool InitStarBrowserColumns(HWND listView)
 {
@@ -91,118 +79,7 @@ bool InitStarBrowserColumns(HWND listView)
     return true;
 }
 
-
-struct CloserStarPredicate
-{
-    Vector3f pos;
-    bool operator()(const Star* star0, const Star* star1) const
-    {
-        return ((pos - star0->getPosition()).squaredNorm() <
-                (pos - star1->getPosition()).squaredNorm());
-
-    }
-};
-
-struct BrighterStarPredicate
-{
-    Vector3f pos;
-    UniversalCoord ucPos;
-    bool operator()(const Star* star0, const Star* star1) const
-    {
-        float d0 = (pos - star0->getPosition()).norm();
-        float d1 = (pos - star1->getPosition()).norm();
-
-        // If the stars are closer than one light year, use
-        // a more precise distance estimate.
-        if (d0 < 1.0f)
-            d0 = ucPos.offsetFromLy(star0->getPosition()).norm();
-        if (d1 < 1.0f)
-            d1 = ucPos.offsetFromLy(star1->getPosition()).norm();
-
-        return (star0->getApparentMagnitude(d0) <
-                star1->getApparentMagnitude(d1));
-    }
-};
-
-struct SolarSystemPredicate
-{
-    Vector3f pos;
-    SolarSystemCatalog* solarSystems;
-
-    bool operator()(const Star* star0, const Star* star1) const
-    {
-        SolarSystemCatalog::iterator iter;
-
-        iter = solarSystems->find(star0->getIndex());
-        bool hasPlanets0 = (iter != solarSystems->end());
-        iter = solarSystems->find(star1->getIndex());
-        bool hasPlanets1 = (iter != solarSystems->end());
-        if (hasPlanets1 == hasPlanets0)
-        {
-            return ((pos - star0->getPosition()).squaredNorm() <
-                    (pos - star1->getPosition()).squaredNorm());
-        }
-        else
-        {
-            return hasPlanets0;
-        }
-    }
-};
-
-
-// Find the nearest/brightest/X-est N stars in a database.  The
-// supplied predicate determines which of two stars is a better match.
-template<class Pred> vector<const Star*>*
-FindStars(const StarDatabase& stardb, Pred pred, int nStars)
-{
-    vector<const Star*>* finalStars = new vector<const Star*>();
-    if (nStars == 0)
-        return finalStars;
-
-    typedef multiset<const Star*, Pred> StarSet;
-    StarSet firstStars(pred);
-
-    int totalStars = stardb.size();
-    if (totalStars < nStars)
-        nStars = totalStars;
-
-    // We'll need at least nStars in the set, so first fill
-    // up the list indiscriminately.
-    int i = 0;
-    for (i = 0; i < nStars; i++)
-    {
-        Star* star = stardb.getStar(i);
-        if (star->getVisibility())
-            firstStars.insert(star);
-    }
-
-    // From here on, only add a star to the set if it's
-    // a better match than the worst matching star already
-    // in the set.
-    const Star* lastStar = *--firstStars.end();
-    for (; i < totalStars; i++)
-    {
-        Star* star = stardb.getStar(i);
-        if (star->getVisibility() && pred(star, lastStar))
-        {
-            firstStars.insert(star);
-            firstStars.erase(--firstStars.end());
-            lastStar = *--firstStars.end();
-        }
-    }
-
-    // Move the best matching stars into the vector
-    finalStars->reserve(nStars);
-    for (const auto& star : firstStars)
-    {
-        finalStars->insert(finalStars->end(), star);
-    }
-
-    return finalStars;
-}
-
-
-bool InitStarBrowserLVItems(HWND listView, vector<const Star*>& stars)
+bool InitStarBrowserLVItems(HWND listView, std::vector<engine::StarBrowserRecord>& records)
 {
     LVITEM lvi;
 
@@ -211,127 +88,92 @@ bool InitStarBrowserLVItems(HWND listView, vector<const Star*>& stars)
     lvi.stateMask = 0;
     lvi.pszText = LPSTR_TEXTCALLBACK;
 
-    for (unsigned int i = 0; i < stars.size(); i++)
+    int index = 0;
+    for (const auto& record : records)
     {
-        lvi.iItem = i;
+        lvi.iItem = index;
         lvi.iSubItem = 0;
-        lvi.lParam = (LPARAM) stars[i];
+        lvi.lParam = (LPARAM) &record;
         ListView_InsertItem(listView, &lvi);
+        ++index;
     }
 
     return true;
 }
 
-
 bool InitStarBrowserItems(HWND listView, StarBrowser* browser)
 {
-    Universe* univ = browser->appCore->getSimulation()->getUniverse();
-    StarDatabase* stardb = univ->getStarCatalog();
-    SolarSystemCatalog* solarSystems = univ->getSolarSystemCatalog();
-
-    vector<const Star*>* stars = NULL;
-    switch (browser->predicate)
-    {
-    case BrightestStars:
-        {
-            BrighterStarPredicate brighterPred;
-            brighterPred.pos = browser->pos;
-            brighterPred.ucPos = browser->ucPos;
-            stars = FindStars(*stardb, brighterPred, browser->nStars);
-        }
-        break;
-
-    case NearestStars:
-        {
-            CloserStarPredicate closerPred;
-            closerPred.pos = browser->pos;
-            stars = FindStars(*stardb, closerPred, browser->nStars);
-        }
-        break;
-
-    case StarsWithPlanets:
-        {
-            if (solarSystems == NULL)
-                return false;
-            SolarSystemPredicate solarSysPred;
-            solarSysPred.pos = browser->pos;
-            solarSysPred.solarSystems = solarSystems;
-            stars = FindStars(*stardb, solarSysPred,
-                              min((unsigned int) browser->nStars, (unsigned int) solarSystems->size()));
-        }
-        break;
-
-    default:
-        return false;
-    }
-
-    bool succeeded = InitStarBrowserLVItems(listView, *stars);
-    delete stars;
+    const Observer& obs = browser->appCore->getSimulation()->getObserver();
+    browser->starBrowser.setPosition(obs.getPosition());
+    browser->starBrowser.setTime(obs.getTime());
+    browser->starBrowser.populate(browser->stars);
+    bool succeeded = InitStarBrowserLVItems(listView, browser->stars);
 
     return succeeded;
 }
 
-
 // Crud used for the list item display callbacks
-static string starNameString("");
+static string starNameString;
+static string starNameString2;
 static char callbackScratch[256];
 
-struct StarBrowserSortInfo
+static int CALLBACK StarBrowserCompareName(LPARAM lParam0, LPARAM lParam1, LPARAM lParamInfo)
 {
-    int subItem;
-    Vector3f pos;
-    UniversalCoord ucPos;
-};
+    auto record0 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam0);
+    auto record1 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam1);
+    auto stardb = reinterpret_cast<const StarDatabase*>(lParamInfo);
 
-int CALLBACK StarBrowserCompareFunc(LPARAM lParam0, LPARAM lParam1,
-                                    LPARAM lParamSort)
-{
-    StarBrowserSortInfo* sortInfo = reinterpret_cast<StarBrowserSortInfo*>(lParamSort);
-    Star* star0 = reinterpret_cast<Star*>(lParam0);
-    Star* star1 = reinterpret_cast<Star*>(lParam1);
-
-    switch (sortInfo->subItem)
-    {
-    case 0:
-        return 0;
-
-    case 1:
-        {
-            float d0 = (sortInfo->pos - star0->getPosition()).norm();
-            float d1 = (sortInfo->pos - star1->getPosition()).norm();
-            return (int) math::sign(d0 - d1);
-        }
-
-    case 2:
-        {
-            float d0 = (sortInfo->pos - star0->getPosition()).norm();
-            float d1 = (sortInfo->pos - star1->getPosition()).norm();
-            if (d0 < 1.0f)
-                d0 = sortInfo->ucPos.offsetFromLy(star0->getPosition()).norm();
-            if (d1 < 1.0f)
-                d1 = sortInfo->ucPos.offsetFromLy(star1->getPosition()).norm();
-            return (int) math::sign(star0->getApparentMagnitude(d0) -
-                                    star1->getApparentMagnitude(d1));
-        }
-
-    case 3:
-        return (int) math::sign(star0->getAbsoluteMagnitude() - star1->getAbsoluteMagnitude());
-
-    case 4:
-        return strcmp(star0->getSpectralType(), star1->getSpectralType());
-
-    default:
-        return 0;
-    }
+    starNameString = util::UTF8ToCurrentCP(stardb->getStarName(*record0->star, true));
+    starNameString2 = util::UTF8ToCurrentCP(stardb->getStarName(*record1->star, true));
+    return CompareString(LOCALE_USER_DEFAULT, NORM_LINGUISTIC_CASING | SORT_DIGITSASNUMBERS,
+                         starNameString.data(), static_cast<int>(starNameString.size()),
+                         starNameString2.data(), static_cast<int>(starNameString2.size()));
 }
 
+static int CALLBACK StarBrowserCompareDistance(LPARAM lParam0, LPARAM lParam1, LPARAM)
+{
+    auto record0 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam0);
+    auto record1 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam1);
+
+    return static_cast<int>(math::sign(record0->distance - record1->distance));
+}
+
+static int CALLBACK StarBrowserCompareAppMag(LPARAM lParam0, LPARAM lParam1, LPARAM)
+{
+    auto record0 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam0);
+    auto record1 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam1);
+    return static_cast<int>(math::sign(record0->appMag - record1->appMag));
+}
+
+static int CALLBACK StarBrowserCompareAbsMag(LPARAM lParam0, LPARAM lParam1, LPARAM)
+{
+    auto record0 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam0);
+    auto record1 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam1);
+    return static_cast<int>(math::sign(record0->star->getAbsoluteMagnitude() - record1->star->getAbsoluteMagnitude()));
+}
+
+static int CALLBACK StarBrowserCompareSpectralType(LPARAM lParam0, LPARAM lParam1, LPARAM)
+{
+    auto record0 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam0);
+    auto record1 = reinterpret_cast<const engine::StarBrowserRecord*>(lParam1);
+    return std::strcmp(record0->star->getSpectralType(), record1->star->getSpectralType());
+}
+
+static constexpr std::array<PFNLVCOMPARE, 5> compareFuncs
+{
+    nullptr,
+    &StarBrowserCompareDistance,
+    &StarBrowserCompareAppMag,
+    &StarBrowserCompareAbsMag,
+    &StarBrowserCompareSpectralType,
+};
 
 void StarBrowserDisplayItem(LPNMLVDISPINFOA nm, StarBrowser* browser)
 {
     double tdb = browser->appCore->getSimulation()->getTime();
 
-    Star* star = reinterpret_cast<Star*>(nm->item.lParam);
-    if (star == NULL)
+    auto record = reinterpret_cast<const engine::StarBrowserRecord*>(nm->item.lParam);
+    if (record == nullptr)
     {
         nm->item.pszText = const_cast<char*>("");
         return;
@@ -342,14 +184,14 @@ void StarBrowserDisplayItem(LPNMLVDISPINFOA nm, StarBrowser* browser)
     case 0:
         {
             Universe* u = browser->appCore->getSimulation()->getUniverse();
-            starNameString = util::UTF8ToCurrentCP(u->getStarCatalog()->getStarName(*star));
+            starNameString = util::UTF8ToCurrentCP(u->getStarCatalog()->getStarName(*record->star, true));
             nm->item.pszText = const_cast<char*>(starNameString.c_str());
         }
         break;
 
     case 1:
         {
-            Vector3d r = star->getPosition(tdb).offsetFromKm(browser->ucPos);
+            Vector3d r = record->star->getPosition(browser->starBrowser.time()).offsetFromKm(browser->starBrowser.position());
             sprintf(callbackScratch, "%.4g", astro::kilometersToLightYears(r.norm()));
             nm->item.pszText = callbackScratch;
         }
@@ -357,21 +199,20 @@ void StarBrowserDisplayItem(LPNMLVDISPINFOA nm, StarBrowser* browser)
 
     case 2:
         {
-            Vector3d r = star->getPosition(tdb).offsetFromKm(browser->ucPos);
-            float appMag = star->getApparentMagnitude(astro::kilometersToLightYears(r.norm()));
+            Vector3d r = record->star->getPosition(browser->starBrowser.time()).offsetFromKm(browser->starBrowser.position());
+            float appMag = record->star->getApparentMagnitude(astro::kilometersToLightYears(r.norm()));
             sprintf(callbackScratch, "%.2f", appMag);
             nm->item.pszText = callbackScratch;
         }
         break;
 
     case 3:
-        sprintf(callbackScratch, "%.2f", star->getAbsoluteMagnitude());
+        sprintf(callbackScratch, "%.2f", record->star->getAbsoluteMagnitude());
         nm->item.pszText = callbackScratch;
         break;
 
     case 4:
-        strncpy(callbackScratch, star->getSpectralType(),
-                sizeof(callbackScratch));
+        strncpy(callbackScratch, record->star->getSpectralType(), sizeof(callbackScratch)); /* Flawfinder: ignore */
         callbackScratch[sizeof(callbackScratch) - 1] = '\0';
         nm->item.pszText = callbackScratch;
         break;
@@ -382,9 +223,6 @@ void RefreshItems(HWND hDlg, StarBrowser* browser)
 {
     SetMouseCursor(IDC_WAIT);
 
-    Simulation* sim = browser->appCore->getSimulation();
-    browser->ucPos = sim->getObserver().getPosition();
-    browser->pos = browser->ucPos.toLy().cast<float>();
     HWND hwnd = GetDlgItem(hDlg, IDC_STARBROWSER_LIST);
     if (hwnd != 0)
     {
@@ -419,15 +257,15 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
             //Initialize Max Stars edit box
             char val[16];
             hwnd = GetDlgItem(hDlg, IDC_MAXSTARS_EDIT);
-            sprintf(val, "%d", DefaultListStars);
+            sprintf(val, "%d", browser->starBrowser.size());
             SetWindowText(hwnd, val);
             SendMessage(hwnd, EM_LIMITTEXT, 3, 0);
 
             //Initialize Max Stars Slider control
             SendDlgItemMessage(hDlg, IDC_MAXSTARS_SLIDER, TBM_SETRANGE,
-                (WPARAM)TRUE, (LPARAM)MAKELONG(MinListStars, MaxListStars));
+                (WPARAM)TRUE, (LPARAM)MAKELONG(engine::StarBrowser::MinListStars, engine::StarBrowser::MaxListStars));
             SendDlgItemMessage(hDlg, IDC_MAXSTARS_SLIDER, TBM_SETPOS,
-                (WPARAM)TRUE, (LPARAM)DefaultListStars);
+                (WPARAM)TRUE, (LPARAM)browser->starBrowser.size());
 
             return(TRUE);
         }
@@ -462,17 +300,20 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
             break;
 
         case IDC_RADIO_BRIGHTEST:
-            browser->predicate = BrightestStars;
+            browser->starBrowser.setComparison(engine::StarBrowser::Comparison::ApparentMagnitude);
+            browser->starBrowser.setFilter(engine::StarBrowser::Filter::Visible);
             RefreshItems(hDlg, browser);
             break;
 
         case IDC_RADIO_NEAREST:
-            browser->predicate = NearestStars;
+            browser->starBrowser.setComparison(engine::StarBrowser::Comparison::Nearest);
+            browser->starBrowser.setFilter(engine::StarBrowser::Filter::Visible);
             RefreshItems(hDlg, browser);
             break;
 
         case IDC_RADIO_WITHPLANETS:
-            browser->predicate = StarsWithPlanets;
+            browser->starBrowser.setComparison(engine::StarBrowser::Comparison::Nearest);
+            browser->starBrowser.setFilter(engine::StarBrowser::Filter::WithPlanets);
             RefreshItems(hDlg, browser);
             break;
 
@@ -492,7 +333,7 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
 
                 // Check if new value is different from old. Don't want to
                 // cause a refresh to occur if not necessary.
-                if (nNewStars != browser->nStars)
+                if (nNewStars != browser->starBrowser.size())
                 {
                     minRange = SendDlgItemMessage(hDlg, IDC_MAXSTARS_SLIDER, TBM_GETRANGEMIN, 0, 0);
                     maxRange = SendDlgItemMessage(hDlg, IDC_MAXSTARS_SLIDER, TBM_GETRANGEMAX, 0, 0);
@@ -510,14 +351,14 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
                     }
 
                     // Recheck value if different from original.
-                    if (nNewStars != browser->nStars)
+                    if (nNewStars != browser->starBrowser.size())
                     {
-                        browser->nStars = nNewStars;
+                        browser->starBrowser.setSize(nNewStars);
                         SendDlgItemMessage(hDlg,
                                            IDC_MAXSTARS_SLIDER,
                                            TBM_SETPOS,
                                            (WPARAM) TRUE,
-                                           (LPARAM) browser->nStars);
+                                           (LPARAM) browser->starBrowser.size());
                         RefreshItems(hDlg, browser);
                     }
                 }
@@ -543,9 +384,9 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
                         if ((nm->uNewState & LVIS_SELECTED) != 0)
                         {
                             Simulation* sim = browser->appCore->getSimulation();
-                            Star* star = reinterpret_cast<Star*>(nm->lParam);
-                            if (star != NULL)
-                                sim->setSelection(Selection(star));
+                            auto record = reinterpret_cast<const engine::StarBrowserRecord*>(nm->lParam);
+                            if (record != nullptr)
+                                sim->setSelection(Selection(const_cast<Star*>(record->star)));
                         }
                         break;
                     }
@@ -555,12 +396,12 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
                         if (hwnd != 0)
                         {
                             LPNMLISTVIEW nm = (LPNMLISTVIEW) lParam;
-                            StarBrowserSortInfo sortInfo;
-                            sortInfo.subItem = nm->iSubItem;
-                            sortInfo.ucPos = browser->ucPos;
-                            sortInfo.pos = browser->pos;
-                            ListView_SortItems(hwnd, StarBrowserCompareFunc,
-                                               reinterpret_cast<LPARAM>(&sortInfo));
+                            if (auto subItem = nm->iSubItem;
+                                subItem >= 0 && subItem < static_cast<int>(compareFuncs.size()))
+                            {
+                                const StarDatabase* stardb = browser->starBrowser.universe()->getStarCatalog();
+                                ListView_SortItems(hwnd, compareFuncs[subItem], reinterpret_cast<LPARAM>(stardb));
+                            }
                         }
                     }
 
@@ -584,7 +425,7 @@ BOOL APIENTRY StarBrowserProc(HWND hDlg,
                 }
                 case SB_THUMBPOSITION:
                 {
-                    browser->nStars = (int)HIWORD(wParam);
+                    browser->starBrowser.setSize(HIWORD(wParam));
                     RefreshItems(hDlg, browser);
                     break;
                 }
@@ -601,14 +442,9 @@ StarBrowser::StarBrowser(HINSTANCE appInstance,
                          HWND _parent,
                          CelestiaCore* _appCore) :
     appCore(_appCore),
-    parent(_parent)
+    parent(_parent),
+    starBrowser(_appCore->getSimulation()->getUniverse())
 {
-    ucPos = appCore->getSimulation()->getObserver().getPosition();
-    pos = ucPos.toLy().cast<float>();
-
-    predicate = NearestStars;
-    nStars = DefaultListStars;
-
     hwnd = CreateDialogParam(appInstance,
                              MAKEINTRESOURCE(IDD_STARBROWSER),
                              parent,
