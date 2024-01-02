4f49a2a6af707a1891b28e33a54b186f1baffe46
Andrew Tribick <ajtribick@googlemail.com>
2024-01-02 20:40:28 +0100

    Set _WIN32_WINNT and NTDDI_VERSION when using Windows ICU functions

--- cmake/winicu.cmake
+++ cmake/winicu.cmake
@@ -4,10 +4,16 @@ function(EnableWinICU)
     HAVE_WIN_ICU_1903
     ${CMAKE_BINARY_DIR}
     "${CMAKE_SOURCE_DIR}/checks/winicunew.cpp"
+    COMPILE_DEFINITIONS
+      -D_WIN32_WINNT=_WIN32_WINNT_WIN10
+      -DNTDDI_VERSION=NTDDI_WIN10_19H1
     LINK_LIBRARIES icu
   )
+  # _WIN32_WINNT and NTDDI_VERSION need to be set to enable the icu header to work properly.
+  # https://learn.microsoft.com/en-gb/windows/win32/winprog/using-the-windows-headers
+  add_definitions(-D_WIN32_WINNT=_WIN32_WINNT_WIN10)
   if(HAVE_WIN_ICU_1903)
-    add_definitions(-DHAVE_WIN_ICU_COMBINED_HEADER)
+    add_definitions(-DHAVE_WIN_ICU_COMBINED_HEADER -DNTDDI_VERSION=NTDDI_WIN10_19H1)
     link_libraries("icu")
     message(STATUS "Found ICU with combined header and combined lib")
   else()
@@ -15,10 +21,13 @@ function(EnableWinICU)
       HAVE_WIN_ICU_1709
       ${CMAKE_BINARY_DIR}
       "${CMAKE_SOURCE_DIR}/checks/winicunew.cpp"
+      COMPILE_DEFINITIONS
+        -D_WIN32_WINNT=_WIN32_WINNT_WIN10
+        -DNTDDI_VERSION=NTDDI_WIN10_RS3
       LINK_LIBRARIES icuuc icuin
     )
     if(HAVE_WIN_ICU_1709)
-      add_definitions(-DHAVE_WIN_ICU_COMBINED_HEADER)
+      add_definitions(-DHAVE_WIN_ICU_COMBINED_HEADER -DNTDDI_VERSION=NTDDI_WIN10_RS3)
       link_libraries("icuuc" "icuin")
       message(STATUS "Found ICU with combined header and separate libs")
     else()
@@ -26,10 +35,13 @@ function(EnableWinICU)
         HAVE_WIN_ICU_1703
         ${CMAKE_BINARY_DIR}
         "${CMAKE_SOURCE_DIR}/checks/winicuold.cpp"
+        COMPILE_DEFINITIONS
+          -D_WIN32_WINNT=_WIN32_WINNT_WIN10
+          -DNTDDI_VERSION=NTDDI_WIN10_RS2
         LINK_LIBRARIES icuuc icuin
       )
       if(HAVE_WIN_ICU_1703)
-        add_definitions(-DHAVE_WIN_ICU_SEPARATE_HEADERS)
+        add_definitions(-DHAVE_WIN_ICU_SEPARATE_HEADERS -DNTDDI_VERSION=NTDDI_WIN10_RS2)
         link_libraries("icuuc" "icuin")
         message(STATUS "Found ICU with separate headers and separate libs")
       else()
