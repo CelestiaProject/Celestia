a75e3d23d4b98d8d712a46e799972e5d60bef49c
Levin Li <lilinfeng303@outlook.com>
2024-03-19 09:10:47 +0800

    Use pointer

--- src/celastro/date.cpp
+++ src/celastro/date.cpp
@@ -16,6 +16,7 @@
 #include <cstdint>
 #include <ctime>
 #include <locale>
+#include <memory>
 #include <string_view>
 
 #include <fmt/format.h>
@@ -85,7 +86,7 @@ celestia::util::array_view<LeapSecondRecord> g_leapSeconds = LeapSeconds; //NOSO
 class MonthAbbreviations
 {
 public:
-    MonthAbbreviations(const std::locale& loc);
+    explicit MonthAbbreviations(const std::locale& loc);
     std::string_view operator[](int i) const { return abbreviations[static_cast<std::size_t>(i)]; }
 
 private:
@@ -217,20 +218,22 @@ Date::toString(const std::locale& loc, Format format) const
         return fmt::format(loc, "{:%Y %b %d %H:%M:%S %z}"sv, cal_time);
     }
 #else
-    static auto monthAbbreviations = MonthAbbreviations(loc);
+    static const MonthAbbreviations* monthAbbreviations = nullptr;
+    if (monthAbbreviations == nullptr)
+        monthAbbreviations = std::make_unique<MonthAbbreviations>(loc).release(); //NOSONAR
     switch(format)
     {
     case Locale:
     case TZName:
         return fmt::format("{:04} {} {:02} {:02}:{:02}:{:02} {}"sv,
-                           year, monthAbbreviations[month - 1], day,
+                           year, (*monthAbbreviations)[month - 1], day,
                            hour, minute, static_cast<int>(seconds), tzname);
     default:
         {
             char sign = utc_offset < 0 ? '-' : '+';
             auto offsets = std::div(std::abs(utc_offset), 3600);
             return fmt::format("{:04} {} {:02} {:02}:{:02}:{:02} {}{:02}{:02}"sv,
-                               year, monthAbbreviations[month - 1], day,
+                               year, (*monthAbbreviations)[month - 1], day,
                                hour, minute, static_cast<int>(seconds),
                                sign, offsets.quot, offsets.rem / 60);
         }
