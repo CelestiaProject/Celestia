0eebce0e29e134a7eef25c1275afb1d5dd5d5f5e
Andrew Tribick <ajtribick@googlemail.com>
2026-02-15 10:49:30 +0100

    Handle out-of-range catalog numbers in .stc, fix line number counting

--- src/celutil/buffile.h
+++ src/celutil/buffile.h
@@ -8,6 +8,8 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
+#pragma once
+
 #include <cstddef>
 #include <cstdint>
 #include <iosfwd>
