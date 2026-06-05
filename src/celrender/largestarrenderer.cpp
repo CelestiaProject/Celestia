f1c139bbadcde12fb9f9975cf83a64b0cc6e8833
Levin Li <lilinfeng303@outlook.com>
2026-06-04 21:35:54 -0700

    Add copyright header to LargeStarRenderer

--- src/celrender/largestarrenderer.cpp
+++ src/celrender/largestarrenderer.cpp
@@ -1,4 +1,11 @@
 // largestarrenderer.cpp
+//
+// Copyright (C) 2026-present, the Celestia Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 
 #include "largestarrenderer.h"
 
