bd4566c4fa9a07f0844e80e271ba0d675b91c97a
Levin Li <lilinfeng303@outlook.com>
2026-06-26 23:16:14 -0700

    Use [ and ] keys to regulate star exposure in PSF star mode

--- src/celestia/celestiacore.cpp
+++ src/celestia/celestiacore.cpp
@@ -1512,7 +1512,13 @@ void CelestiaCore::charEntered(const char *c_p, int modifiers)
         break;
 
     case '[':
-        if (!util::is_set(renderer->getRenderFlags(), RenderFlags::ShowAutoMag))
+        if (renderer->getStarStyle() == StarStyle::PointSpreadFunction)
+        {
+            renderer->setStarExposure(renderer->getStarExposure() / 1.1f);
+            auto buf = fmt::format(loc, fmt::runtime(_("Star exposure: {:.3f}")), renderer->getStarExposure());
+            flash(buf);
+        }
+        else if (!util::is_set(renderer->getRenderFlags(), RenderFlags::ShowAutoMag))
         {
             if (sim->getFaintestVisible() > 1.0f)
             {
@@ -1536,7 +1542,13 @@ void CelestiaCore::charEntered(const char *c_p, int modifiers)
         break;
 
     case ']':
-        if (!util::is_set(renderer->getRenderFlags(), RenderFlags::ShowAutoMag))
+        if (renderer->getStarStyle() == StarStyle::PointSpreadFunction)
+        {
+            renderer->setStarExposure(renderer->getStarExposure() * 1.1f);
+            auto buf = fmt::format(loc, fmt::runtime(_("Star exposure: {:.3f}")), renderer->getStarExposure());
+            flash(buf);
+        }
+        else if (!util::is_set(renderer->getRenderFlags(), RenderFlags::ShowAutoMag))
         {
             if (sim->getFaintestVisible() < 15.0f)
             {
